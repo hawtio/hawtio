80b022346f44e1b9a4dbd6fe8d4939df0448dd16
James Strachan <james.strachan@gmail.com>
2012-11-23 14:46:55 +0000

    tweaked layout

--- web-console/src/main/webapp/js/app.js
+++ web-console/src/main/webapp/js/app.js
@@ -1032,7 +1032,6 @@ function SendMessageController($scope, $location, workspace) {
     $scope.$watch('sourceFormat', function () {
         var format = $scope.sourceFormat;
         var workspace = $scope.workspace;
-        console.log("source format is now: " + format);
         if(format && workspace) {
             workspace.setLocalStorage(languageFormatPreference, format);
         }
