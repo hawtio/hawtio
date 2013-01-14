3a081aec3d65d75b5d909754c5c3c922417b0136
Stan Lewis <slewis@fusesource.com>
2013-01-14 16:48:57 -0500

    Fix health tab

--- hawtio-web/src/main/webapp/app/health/js/health-app.ts
+++ hawtio-web/src/main/webapp/app/health/js/health-app.ts
@@ -2,7 +2,7 @@ module Health {
   var pluginName = 'health';
   angular.module(pluginName, ['bootstrap', 'ngResource', 'hawtioCore']).config(($routeProvider) => {
     $routeProvider.
-            when('/health', {templateUrl: 'app/core/html/health.html', controller: HealthController})
+            when('/health', {templateUrl: 'app/health/html/health.html', controller: HealthController})
   }).
           run(($location: ng.ILocationService, workspace:Workspace) => {
             // now lets register the nav bar stuff!
