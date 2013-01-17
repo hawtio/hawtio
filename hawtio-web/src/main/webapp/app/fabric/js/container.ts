ef5d63e8b2c92793b9f9a930f80b68a31dbdb7b6
Stan Lewis <slewis@fusesource.com>
2013-01-17 11:30:07 -0500

    Show icons when known services are present on a container

--- hawtio-web/src/main/webapp/app/fabric/js/container.ts
+++ hawtio-web/src/main/webapp/app/fabric/js/container.ts
@@ -16,6 +16,62 @@ module Fabric {
       return "";
     }
 
+    $scope.hasServices = () => {
+      $scope.getServices().length > 0;
+    }
+
+    $scope.getServices = () => {
+      var answer = [];
+      if ($scope.row) {
+
+        $scope.row.jmxDomains.forEach((domain) => {
+          if (domain === "org.apache.activemq") {
+            answer.push({
+              title: "Apache ActiveMQ",
+              type: "img",
+              src: "app/fabric/img/message_broker.png"
+            });
+          }
+          if (domain === "org.apache.camel") {
+            answer.push({
+              title: "Apache Camel",
+              type: "img",
+              src: "app/fabric/img/camel.png"
+            });
+          }
+          if (domain === "org.fusesource.fabric") {
+            answer.push({
+              title: "Fuse Fabric",
+              type: "img",
+              src: "app/fabric/img/fabric.png"
+            });
+          }
+          if (domain === "hawtio") {
+            answer.push({
+              title: "hawtio",
+              type: "img",
+              src: "app/fabric/img/hawtio.png"
+            });
+          }
+          if (domain === "org.apache.karaf") {
+            answer.push({
+              title: "Apache Karaf",
+              type: "icon",
+              src: "icon-beaker"
+            })
+          }
+          if (domain === "org.apache.zookeeper") {
+            answer.push({
+              title: "Apache Zookeeper",
+              type: "icon",
+              src: "icon-group"
+            })
+          }
+        });
+      }
+      return answer;
+    }
+
     jolokia.request(
             {type: 'exec', mbean: managerMBean,
               operation: 'getContainer(java.lang.String)',
@@ -25,6 +81,7 @@ module Fabric {
 
     function populateTable(response) {
       $scope.row = response.value;
+      $scope.row.services = $scope.getServices();
       Fabric.defaultContainerValues(workspace, $scope, [$scope.row]);
       $scope.$apply();
     }
