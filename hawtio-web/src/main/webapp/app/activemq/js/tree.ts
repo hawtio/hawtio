47c90f483470951049e98a8ce4181e3a6676b9c0
James Strachan <james.strachan@gmail.com>
2013-01-18 15:03:45 +0000

    avoid the unnecessary Broker root folder on the AMQ 5.8 or later JMX

--- hawtio-web/src/main/webapp/app/activemq/js/tree.ts
+++ hawtio-web/src/main/webapp/app/activemq/js/tree.ts
@@ -20,6 +20,19 @@ module ActiveMQ {
         if (folder) {
           children = folder.children;
         }
+        if (children.length) {
+          var firstChild = children[0];
+          // the children could be AMQ 5.7 style broker name folder with the actual MBean in the children
+          // along with folders for the Queues etc...
+          if (!firstChild.typeName && firstChild.children.length < 4) {
+            // lets avoid the top level folder
+            var answer = [];
+            angular.forEach(children, (child) => {
+              answer = answer.concat(child.children);
+            });
+            children = answer;
+          }
+        }
         var treeElement = $("#activemqtree");
         Jmx.enableTree($scope, $location, workspace, treeElement, children, true);
 /*
