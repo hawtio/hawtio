31f5bdf0dea13cecc0cb5e24265837e93d7ffc59
Stan Lewis <slewis@fusesource.com>
2013-01-21 18:18:00 -0500

    Get this implementation checked in...

--- hawtio-web/src/main/webapp/app/fabric/js/containers.ts
+++ hawtio-web/src/main/webapp/app/fabric/js/containers.ts
@@ -2,40 +2,84 @@ module Fabric {
 
   export function ContainerRow($scope, workspace:Workspace, jolokia) {
 
-    Core.register(jolokia, $scope, {
-      type: 'exec', mbean: managerMBean,
-      operation: 'getContainer(java.lang.String)',
-      arguments: [$scope.row.id]
-    }, onSuccess(render));
+    $scope.statusIcon = () => {
+      if ($scope.row) {
+        if ($scope.row.alive) {
+          switch($scope.row.provisionResult) {
+            case 'success': 
+              return "icon-thumbs-up";
+            case 'downloading':
+              return "icon-download-alt";
+            case 'installing':
+              return "icon-hdd";
+            case 'analyzing':
+            case 'finalizing':
+              return "icon-refresh icon-spin";
+            case 'resolving':
+              return "icon-sitemap";
+            case 'error':
+              return "red icon-warning-sign";
+          }
+        } else {
+          return "icon-off";
+        }
+      }
+      return "icon-refresh icon-spin";
+    }
+
+    if (angular.isDefined($scope.containerId)) {
+      Core.register(jolokia, $scope, {
+        type: 'exec', mbean: managerMBean,
+        operation: 'getContainer(java.lang.String)',
+        arguments: [$scope.containerId]
+      }, onSuccess(render, {
+        error: function(response) => {
+          // most likely the container has been deleted
+          $scope.$destroy();
+          $scope.$apply();
+        }
+      }));
+    }
     
     function render(response) {
-      if (!Object.equal($scope.row, response.value)) {
+      if (!Object.equal($scope.row, response.value)) { 
         $scope.row = response.value
+        $scope.$parent.setProfilesFor($scope.row);
         $scope.$apply();
       }
     }
+    
   }
 
 
   export function ContainersController($scope, $location:ng.ILocationService, workspace:Workspace, jolokia) {
     $scope.profileId = '';
     
-    $scope.foo = (row) => {
+    $scope.profileMap = {};
+
+    $scope.profileIds = () => {
+      var answer = [""];
+      angular.forEach($scope.profileMap, (value, key) => {
+        answer = answer.union(value)
+      });
+      return answer;
+    }
+    
+    $scope.setProfilesFor = function (row) {
+      $scope.profileMap[row.id] = row.profileIds;
+    }
+
+    $scope.show = (row) => {
       if (!angular.isDefined($scope.profileId) || $scope.profileId === '') {
         return true;
       }
-      if (row.profileIds.find($scope.profileId)) {
+
+      if (angular.isDefined($scope.profileMap[row].find( function (id) { return $scope.profileId === id })) ) {
         return true;
       }
+
       return false;      
     }
-
-    $scope.profileIds = () => {
-      // TODO this should be a generic function?
-      var answer = [""];
-      angular.forEach($scope.profileMap, (value, key) => answer.push(key));
-      return answer;
-    };
 
     $scope.$on("$routeChangeSuccess", function (event, current, previous) {
       // lets update the profileId from the URL if its available
@@ -45,18 +89,18 @@ module Fabric {
       }
     });
 
-    $scope.$watch('workspace.selection', function () {
-      if (workspace.moveIfViewInvalid()) return;
-
-      function populateTable(response) {
-        var values = response.value;
-        $scope.containers = defaultContainerValues(workspace, $scope, values);
+    Core.register(jolokia, $scope, {
+      type: 'exec', mbean: managerMBean,
+      operation: 'containerIds()',
+      arguments: []
+    }, onSuccess(render));
+    
+    function render(response) {
+      if (!Object.equal($scope.containers, response.value)) {
+        $scope.containers = response.value;
         $scope.$apply();
       }
-
-      jolokia.request(
-              {type: 'exec', mbean: managerMBean, operation: 'containers'},
-              onSuccess(populateTable));
-    });
+    }
+    
   }
 }
