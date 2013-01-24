00c789ec591cd6c3593d0c206a66dd5bc03b0afc
James Strachan <james.strachan@gmail.com>
2013-01-24 16:04:23 +0000

    default to help page first

--- hawtio-web/src/main/webapp/app/core/js/corePlugin.ts
+++ hawtio-web/src/main/webapp/app/core/js/corePlugin.ts
@@ -1,4 +1,3 @@
-
 // bootstrap plugin loader
 var jolokiaUrl = hawtioPluginLoader.parseQueryString()['url'] || "/hawtio/jolokia";
 
@@ -26,28 +25,28 @@ myApp.config(($routeProvider) => {
           }).
           when('/help/:tabName', {templateUrl: 'app/core/html/help.html', controller: Core.NavBarController}).
 
-          otherwise({redirectTo: '/jmx/help/overview'});
+          otherwise({redirectTo: '/help/overview'});
 });
 
 myApp.service('localStorage', function () {
-      // TODO Create correct implementation of windowLocalStorage
-      var storage:WindowLocalStorage = window.localStorage || <any> (function () {
-        return {};
-      })();
-      return storage;
-    });
+  // TODO Create correct implementation of windowLocalStorage
+  var storage:WindowLocalStorage = window.localStorage || <any> (function () {
+    return {};
+  })();
+  return storage;
+});
 
 myApp.factory('jolokia', ($location:ng.ILocationService, localStorage) => {
-    var jolokiaUrl = $location.search()['url'] || url("/jolokia");
-    // console.log("Jolokia URL is " + jolokiaUrl);
-    var jolokia = new Jolokia(jolokiaUrl);
-    localStorage['url'] = jolokiaUrl;
-    return jolokia;
-  });
+  var jolokiaUrl = $location.search()['url'] || url("/jolokia");
+  // console.log("Jolokia URL is " + jolokiaUrl);
+  var jolokia = new Jolokia(jolokiaUrl);
+  localStorage['url'] = jolokiaUrl;
+  return jolokia;
+});
 
-myApp.factory('workspace',($location:ng.ILocationService, $compile:ng.ICompileService, $templateCache:ng.ITemplateCacheService, localStorage:WindowLocalStorage, jolokia) => {
-      return new Workspace(jolokia, $location, $compile, $templateCache, localStorage);
-    });
+myApp.factory('workspace', ($location:ng.ILocationService, $compile:ng.ICompileService, $templateCache:ng.ITemplateCacheService, localStorage:WindowLocalStorage, jolokia) => {
+  return new Workspace(jolokia, $location, $compile, $templateCache, localStorage);
+});
 
 
 myApp.filter('humanize', () => humanizeValue)
@@ -55,73 +54,72 @@ myApp.filter('humanize', () => humanizeValue)
 
 myApp.run(($rootScope, $routeParams, jolokia, workspace, localStorage) => {
 
-        $.support.cors = true;
+  $.support.cors = true;
 
-        /**
-         * Count the number of lines in the given text
-         */
-        $rootScope.lineCount = lineCount;
+  /**
+   * Count the number of lines in the given text
+   */
+  $rootScope.lineCount = lineCount;
 
-        /**
-         * Easy access to route params
-         */
-        $rootScope.params = $routeParams;
+  /**
+   * Easy access to route params
+   */
+  $rootScope.params = $routeParams;
 
-        /**
-         * Wrapper for angular.isArray, isObject, etc checks for use in the view
-         *
-         * @param type {string} the name of the check (casing sensitive)
-         * @param value {string} value to check
-         */
-        $rootScope.is = function (type:any, value:any):bool {
-          return angular['is' + type](value);
-        };
+  /**
+   * Wrapper for angular.isArray, isObject, etc checks for use in the view
+   *
+   * @param type {string} the name of the check (casing sensitive)
+   * @param value {string} value to check
+   */
+  $rootScope.is = function (type:any, value:any):bool {
+    return angular['is' + type](value);
+  };
 
-        /**
-         * Wrapper for $.isEmptyObject()
-         *
-         * @param value  {mixed} Value to be tested
-         * @return boolean
-         */
-        $rootScope.empty = function (value:any):bool {
-          return $.isEmptyObject(value);
-        };
+  /**
+   * Wrapper for $.isEmptyObject()
+   *
+   * @param value  {mixed} Value to be tested
+   * @return boolean
+   */
+  $rootScope.empty = function (value:any):bool {
+    return $.isEmptyObject(value);
+  };
 
-        /**
-         * Initialize jolokia polling and add handler to change poll
-         * frequency
-         */
-        // only reset the update rate if its not defined
-        var updateRate = localStorage['updateRate'];
-        if (angular.isUndefined(updateRate)) {
-          localStorage['updateRate'] = 5000;
-        }
+  /**
+   * Initialize jolokia polling and add handler to change poll
+   * frequency
+   */
+  // only reset the update rate if its not defined
+  var updateRate = localStorage['updateRate'];
+  if (angular.isUndefined(updateRate)) {
+    localStorage['updateRate'] = 5000;
+  }
 
-        $rootScope.$on('UpdateRate', (event, rate) => {
-          jolokia.stop();
-          if (rate > 0) {
-            jolokia.start(rate);
-          }
-          console.log("Set update rate to: " + rate);
-        });
+  $rootScope.$on('UpdateRate', (event, rate) => {
+    jolokia.stop();
+    if (rate > 0) {
+      jolokia.start(rate);
+    }
+    console.log("Set update rate to: " + rate);
+  });
 
-        $rootScope.$emit('UpdateRate', localStorage['updateRate']);
+  $rootScope.$emit('UpdateRate', localStorage['updateRate']);
 
-        /**
-         * Debugging Tools
-         *
-         * Allows you to execute debug functions from the view
-         */
-          // TODO Doesn't support vargs like it should
-        $rootScope.log = function (variable:any):void {
-          console.log(variable);
-        };
-        $rootScope.alert = function (text:string) {
-          alert(text);
-        };
+  /**
+   * Debugging Tools
+   *
+   * Allows you to execute debug functions from the view
+   */
+    // TODO Doesn't support vargs like it should
+  $rootScope.log = function (variable:any):void {
+    console.log(variable);
+  };
+  $rootScope.alert = function (text:string) {
+    alert(text);
+  };
 
-    });
-
+});
 
 
 myApp.directive('expandable', function () {
@@ -163,17 +161,17 @@ $(function () {
 // ---------------------------------------------------------
 var main = angular.module('main', []);
 
-main.config(function($routeProvider) {
+main.config(function ($routeProvider) {
   $routeProvider.when('/plugins', {
-      templateUrl: 'html/plugins.html',
-      controller: PluginController
-    });
+    templateUrl: 'html/plugins.html',
+    controller: PluginController
+  });
 
-    //$locationProvider.html5Mode(true);
+  //$locationProvider.html5Mode(true);
 });
 
 // service for plugins to register links
-main.factory('links', function() {
+main.factory('links', function () {
   var answer = [];
   return answer;
 });
@@ -181,11 +179,11 @@ main.factory('links', function() {
 // constant for plugin to link back to main page
 main.constant('home', '#/hawtio');
 
-main.run( function () {
-    // console.log("main app running");
+main.run(function () {
+  // console.log("main app running");
 });
 
-var PluginController = function($scope, $route, links) {
+var PluginController = function ($scope, $route, links) {
   $scope.routes = JSON.stringify($route.routes, null, 4);
   $scope.links = links;
 }
@@ -193,10 +191,10 @@ var PluginController = function($scope, $route, links) {
 hawtioPluginLoader.addModule('main');
 // ---------------------------------------------------------
 
-$(function() {
-      hawtioPluginLoader.loadPlugins(function() {
-        angular.bootstrap($(document), hawtioPluginLoader.getModules());
-      });
+$(function () {
+  hawtioPluginLoader.loadPlugins(function () {
+    angular.bootstrap($(document), hawtioPluginLoader.getModules());
+  });
 });
 
 
