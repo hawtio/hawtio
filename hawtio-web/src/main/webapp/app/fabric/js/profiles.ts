e743bed229e078825a6dd9e34a35db11d4e92234
Stan Lewis <slewis@fusesource.com>
2013-01-24 16:18:30 -0500

    Bit more work on the profile table

--- hawtio-web/src/main/webapp/app/fabric/js/profiles.ts
+++ hawtio-web/src/main/webapp/app/fabric/js/profiles.ts
@@ -57,8 +57,36 @@ module Fabric {
         { 
           field: 'id',
           displayName: 'Name',
-          cellTemplate: '<div class="ngCellText"><a href="#/fabric/profile/{{$parent.version.id}}/{{row.getProperty(col.field)}}{{hash}}">{{row.getProperty(col.field)}}</a></div>'
+          cellTemplate: '<div class="ngCellText"><a ng-href="#/fabric/profile/{{$parent.version.id}}/{{row.getProperty(col.field)}}{{hash}}">{{row.getProperty(col.field)}}</a></div>',
+          width: 300
+        },
+        {
+          field: 'attributes',
+          displayName: 'A',
+          headerCellTemplate: '<div ng-click="col.sort()" class="ngHeaderSortColumn {{col.headerClass}}" ng-style="{\'cursor\': col.cursor}" ng-class="{ \'ngSorted\': !noSortVisible }"><div class="ngHeaderText colt{{$index}} pagination-centered" title="Attributes"><i class="icon-cogs"></i></div><div class="ngSortButtonDown" ng-show="col.showSortButtonDown()"></div><div class="ngSortButtonUp" ng-show="col.showSortButtonUp()"></div></div>',
+          cellTemplate: '<div class="ngCellText"><ul class="unstyled inline"><li class="attr-column"><i ng-show="row.entity.locked" title="Locked" class="icon-lock"></i></li><li class="attr-column"><i ng-show="row.entity.abstract" title="Abstract" class="icon-font"></i></li></ul></div>',
+          width: 52
+        },
+        {
+          field: 'containerCount',
+          displayName: 'C',
+          headerCellTemplate: '<div ng-click="col.sort()" class="ngHeaderSortColumn {{col.headerClass}}" ng-style="{\'cursor\': col.cursor}" ng-class="{ \'ngSorted\': !noSortVisible }"><div class="ngHeaderText colt{{$index}} pagination-centered" title="Containers"><i class="icon-truck"></i></div><div class="ngSortButtonDown" ng-show="col.showSortButtonDown()"></div><div class="ngSortButtonUp" ng-show="col.showSortButtonUp()"></div></div>',
+          cellTemplate: '<div class="ngCellText pagination-centered"><a ng-show="row.getProperty(col.field) > 0" title="{{row.entity.containers.sortBy().join(\'\n\')}}" href="#/fabric/containers?cv={{$parent.version.id}}&cp={{row.entity.id}}{{hash}}">{{row.getProperty(col.field)}}</a></div>',
+          width: 28
+        },
+        { 
+          field: 'parentIds',
+          displayName: 'Parent Profiles',
+          cellTemplate: '<div class="ngCellText"><ul class="unstyled inline"><li ng-repeat="profile in row.entity.parentIds.sortBy()"><a href="#/fabric/profile/{{$parent.version.id}}/{{profile}}">{{profile}}</a></li></ul></div>',
+          width: 400
+        },
+        {
+          field: 'childIds',
+          displayName: 'Child Profiles',
+          cellTemplate: '<div class="ngCellText"><ul class="unstyled inline"><li ng-repeat="profile in row.entity.childIds.sortBy()"><a href="#/fabric/profile/{{$parent.version.id}}/{{profile}}">{{profile}}</a></li></ul></div>',
+          width: 800
         }
+        
       ]
     };
     
@@ -83,148 +111,18 @@ module Fabric {
           
           $scope.profilesResponse.forEach(function(profile) {
             $scope.profiles.push({
-              id: profile.id
+              id: profile.id,
+              parentIds: profile.parentIds,
+              childIds: profile.childIds,
+              containerCount: profile.containerCount,
+              containers: profile.containers,
+              locked: profile.locked,
+              abstract: profile.abstractProfile
             })
-          })
-          console.log("Profiles: ", $scope.profilesResponse);
+          });
           $scope.$apply();
         }
       }
     }
-      
-    
-
-    
-    
-    
-    
-  /*    
-    
-    $scope.versions = [];
-
-    $scope.widget = new TableWidget($scope, workspace, [
-      {
-        "mDataProp": null,
-        "sClass": "control center",
-        "mData": null,
-        "sDefaultContent": '<i class="icon-plus"></i>'
-      },
-      {
-        "mDataProp": "link",
-        "sDefaultContent": "",
-        "mData": null
-      },
-      {
-        "mDataProp": "containersCountLink",
-        "mRender": function (data, type, row) {
-          if (data) {
-            return data;
-          } else return "";
-        }
-      },
-      {
-        "mDataProp": "parentLinks",
-        "sDefaultContent": "",
-        "mData": null
-      }
-    ], {
-      rowDetailTemplateId: 'fabricProfileTemplate',
-      disableAddColumns: true
-    });
-
-    $scope.$on("$routeChangeSuccess", function (event, current, previous) {
-      // lets update the profileId from the URL if its available
-      var ao = $location.search()['ao'];
-      if (ao) {
-        $scope.activeOnly = true;
-      }
-      var key = $location.search()['v'];
-      if (key && key !== $scope.versionId) {
-        $scope.versionId = key;
-        // lets do this asynchronously to avoid Error: $digest already in progress
-        setTimeout(updateTableContents, 50);
-      }
-    });
-
-    $scope.$watch('version', function () {
-      if (workspace.moveIfViewInvalid()) return;
-
-      var versionId = $scope.versionId;
-      if ($scope.version) {
-        versionId = $scope.version.id;
-      }
-      if (!versionId) {
-        versionId = "1.0";
-      }
-
-      if (versionId !== $scope.versionId) {
-        $scope.versionId = versionId;
-        var q = $location.search();
-        q['v'] = versionId;
-        $location.search(q);
-        updateTableContents();
-      }
-    });
-
-    $scope.$watch('activeOnly', function () {
-      var q = $location.search();
-      if ($scope.activeOnly) {
-        q['ao'] = "t";
-      } else {
-        delete q['ao'];
-      }
-      $location.search(q);
-
-      // lets do this asynchronously to avoid Error: $digest already in progress
-      setTimeout(filterTable, 50);
-    });
-
-    function populateTable(response) {
-      var values = response.value;
-      $scope.allProfiles = defaultProfileValues(workspace, $scope.versionId, values);
-      filterTable();
-    }
-
-    /**
-     * After we have loaded all the profiles lets apply any specific filters
-     */
-     /*
-    function filterTable() {
-      $scope.profiles = $scope.allProfiles;
-      if ($scope.activeOnly && $scope.profiles) {
-        $scope.profiles = $scope.profiles.filter((p) => p["containerCount"] > 0);
-      }
-      // populate calls $scope.$apply()
-      $scope.widget.populateTable($scope.profiles);
-    }
-
-    function populateVersions(response) {
-      $scope.versions = response.value;
-      if (!$scope.versions.isEmpty()) {
-        if ($scope.versionId) {
-          // lets re-select the version object based on the last selection
-          $scope.version = $scope.versions.find({ id: $scope.versionId });
-        }
-        else {
-          // lets default the version
-          $scope.version = $scope.versions.find({ defaultVersion: true}) || $scope.versions[0];
-        }
-      }
-    }
-
-    function updateTableContents() {
-      var jolokia = workspace.jolokia;
-      //console.log("Requesting profiles for version " + $scope.versionId);
-      jolokia.request(
-              [
-                {type: 'exec', mbean: managerMBean, operation: 'versions'},
-                {type: 'exec', mbean: managerMBean,
-                  operation: 'getProfiles(java.lang.String)',
-                  arguments: [$scope.versionId]}
-              ],
-              onSuccess([populateVersions, populateTable]));
-    }
-  */
-    
   }
 }
