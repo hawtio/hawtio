781c28dc84ed459ff2e499636f576bd7c923ef43
Ioannis Canellos <iocanel@apache.org>
2013-01-30 17:17:17 +0200

    Add bundle links to feature details, if the bundle already exists in the container.

--- hawtio-web/src/main/webapp/app/karaf/js/feature.ts
+++ hawtio-web/src/main/webapp/app/karaf/js/feature.ts
@@ -3,30 +3,67 @@ module Karaf {
     export function FeatureController($scope, $filter:ng.IFilterService, workspace:Workspace, $routeParams) {
         $scope.name = $routeParams.name;
         $scope.version = $routeParams.version;
+        $scope.bundlesByLocation = {};
 
         updateTableContents();
 
         function populateTable(response) {
             angular.forEach(response.value["Features"], (feature) => {
-                    angular.forEach(feature, (entry) => {
+                angular.forEach(feature, (entry) => {
 
-                        if (entry["Name"] === $scope.name && entry["Version"] === $scope.version) {
-                            //$scope.row = entry;
-                            $scope.row = extractFeature(response.value, entry["Name"], entry["Version"])
-                        }
-                    });
+                    if (entry["Name"] === $scope.name && entry["Version"] === $scope.version) {
+                        //$scope.row = entry;
+                        $scope.row = extractFeature(response.value, entry["Name"], entry["Version"])
+                        addBundleDetails($scope.row);
+                    }
+                });
             });
             $scope.$apply();
         };
 
+        function setBundles(response) {
+            var bundleMap = {};
+            Osgi.defaultBundleValues(workspace, $scope, response.values);
+            angular.forEach(response.value, (bundle) => {
+                var location = bundle["Location"];
+                $scope.bundlesByLocation[location] = bundle;
+            });
+        };
+
+
         function updateTableContents() {
-            var mbean = getSelectionFeaturesMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
+            var featureMbean = getSelectionFeaturesMBean(workspace);
+            var bundleMbean = Osgi.getSelectionBundleMBean(workspace);
+            var jolokia = workspace.jolokia;
+
+            if (bundleMbean) {
+                setBundles(jolokia.request(
+                    {type: 'exec', mbean: bundleMbean, operation: 'listBundles()'}));
+            }
+
+            if (featureMbean) {
                 jolokia.request(
-                    {type: 'read', mbean: getSelectionFeaturesMBean(workspace)},
+                    {type: 'read', mbean: featureMbean},
                     onSuccess(populateTable));
             }
+        }
+
+        function addBundleDetails(feature) {
+            var bundleDetails = [];
+            angular.forEach(feature["Bundles"], (bundleLocation)=> {
+                var bundle = $scope.bundlesByLocation[bundleLocation];
+                if (bundle) {
+                    bundle["Installed"] = true;
+                    bundleDetails.push(bundle);
+                } else {
+                    bundleDetails.push({
+                        "Location": bundleLocation,
+                        "Installed": false
+                    })
+
+                }
+            });
+            feature["BundleDetails"] = bundleDetails;
         }
     }
 }
