f719832be77247ef40bf45c4fee1003df6f82c77
Stan Lewis <slewis@fusesource.com>
2013-02-04 12:59:55 -0500

    Remove selection checkboxes and column selector in attributes grid for now

--- hawtio-web/src/main/webapp/app/jmx/js/attributes.ts
+++ hawtio-web/src/main/webapp/app/jmx/js/attributes.ts
@@ -18,6 +18,9 @@ module Jmx {
     $scope.gridOptions = {
       selectedItems: $scope.selectedItems,
       showFilter: false,
+      showColumnMenu: false,
+      canSelectRows: false,
+      displaySelectionCheckbox: false,
       filterOptions: {
         filterText: "searchText"
       },
