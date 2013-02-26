255de28841d16f093debd4fafafb0de4b6f1a7df
Stan Lewis <slewis@fusesource.com>
2013-02-26 10:55:54 -0500

    First cut on swapping out table widget with ng-grid for #109

--- hawtio-web/src/main/webapp/app/osgi/js/bundles.ts
+++ hawtio-web/src/main/webapp/app/osgi/js/bundles.ts
@@ -1,54 +1,95 @@
 module Osgi {
 
-    export function BundlesController($scope, $filter:ng.IFilterService, workspace:Workspace, $templateCache:ng.ITemplateCacheService, $compile:ng.IAttributes) {
-        var dateFilter = $filter('date');
+  export function BundlesController($scope, workspace:Workspace, jolokia) {
 
-        $scope.widget = new TableWidget($scope, workspace, [
-            {
-                "mDataProp": null,
-                "sClass": "control center",
-                "sDefaultContent": '<i class="icon-plus"></i>'
-            },
-            { "mDataProp": "IdentifierLink" },
-            { "mDataProp": "SymbolicName" },
-            { "mDataProp": "stateImageLink"},
-            { "mDataProp": "Version" },
-            { "mDataProp": "LastModified",
-                "mRender": function (data, type, row) {
-                    return dateFilter(data, "short");
-                }
-            }
-        ], {
-            rowDetailTemplateId: 'osgiBundleTemplate',
-            disableAddColumns: true
+    $scope.result = {};
+    $scope.bundles = [];
+    $scope.selectedBundles = [];
+    $scope.loading = true;
+
+    var columnDefs = [
+      {
+        field: 'Identifier',
+        displayName: 'ID',
+        width: "48",
+        headerCellTemplate: '<div ng-click="col.sort()" class="ngHeaderSortColumn {{col.headerClass}}" ng-style="{\'cursor\': col.cursor}" ng-class="{ \'ngSorted\': !noSortVisible }"><div class="ngHeaderText colt{{$index}} pagination-centered" title="Identifier"><i class="icon-tag"></i></div><div class="ngSortButtonDown" ng-show="col.showSortButtonDown()"></div><div class="ngSortButtonUp" ng-show="col.showSortButtonUp()"></div></div>',
+      },
+      {
+        field: 'State',
+        displayName: 'C',
+        width: "24",
+        headerCellTemplate: '<div ng-click="col.sort()" class="ngHeaderSortColumn {{col.headerClass}}" ng-style="{\'cursor\': col.cursor}" ng-class="{ \'ngSorted\': !noSortVisible }"><div class="ngHeaderText colt{{$index}} pagination-centered" title="State"><i class="icon-tasks"></i></div><div class="ngSortButtonDown" ng-show="col.showSortButtonDown()"></div><div class="ngSortButtonUp" ng-show="col.showSortButtonUp()"></div></div>',
+        cellTemplate: '<div class="ngCellText" title="{{row.getProperty(col.field)}}"><i class="{{row.getProperty(col.field)}}"></i></div>'
+      },
+      {
+        field: 'Name',
+        displayName: 'Name',
+        width: "***",
+        cellTemplate: '<div class="ngCellText"><a href="#/osgi/bundle/{{row.entity.Identifier}}">{{row.getProperty(col.field)}}</a></div>'
+      },
+      {
+        field: 'SymbolicName',
+        displayName: 'SymbolicName',
+        width: "***",
+        cellTemplate: '<div class="ngCellText"><a href="#/osgi/bundle/{{row.entity.Identifier}}">{{row.getProperty(col.field)}}</a></div>'
+      },
+      {
+        field: 'Version',
+        displayName: 'Version',
+        width: "**"
+      },
+      {
+        field: 'LastModified',
+        displayName: 'Last Modified',
+        cellFilter: "date:'yyyy-MM-dd HH:mm:ss'",
+        width: "**"
+      },
+      {
+        field: 'Location',
+        displayName: 'Update Location',
+        width: "***"
+      }
+    ];
+
+    $scope.gridOptions = {
+      data: 'bundles',
+      showfilter: false,
+      selectedItems: $scope.selectedBundles,
+      selectWithCheckboxOnly: true,
+      columnDefs: columnDefs
+    };
+
+    function render(response) {
+      if (!Object.equal($scope.result, response.value)) {
+        console.log("Got: ", response.value);
+        console.log("First: ", response.value[1]);
+        $scope.result = response.value;
+        $scope.bundles = [];
+        angular.forEach($scope.result, function(value, key) {
+          var obj = {
+            Identifier: value.Identifier,
+            Name: "",
+            SymbolicName: value.SymbolicName,
+            State: value.State,
+            Version: value.Version,
+            LastModified: value.LastModified,
+            Location: value.Location
+          };
+          if (value.Headers['Bundle-Name']) {
+            obj.Name = value.Headers['Bundle-Name']['Value'];
+
+          }
+          $scope.bundles.push(obj);
         });
-
-
-/*        $scope.$on("$routeChangeSuccess", function (event, current, previous) {
-            // lets do this asynchronously to avoid Error: $digest already in progress
-            setTimeout(updateTableContents, 50);
-        });
-*/
-
-        $scope.$watch('workspace.selection', function () {
-            updateTableContents();
-        });
-
-        function populateTable(response) {
-            Osgi.defaultBundleValues(workspace, $scope, response.value);
-            $scope.widget.populateTable(response.value);
-            $scope.$apply();
-        }
-
-        function updateTableContents() {
-            //console.log("Loading the bundles");
-            var mbean = getSelectionBundleMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request(
-                        {type: 'exec', mbean: mbean, operation: 'listBundles()'},
-                        onSuccess(populateTable));
-            }
-        }
+        $scope.loading = false;
+        $scope.$apply();
+      }
     }
+
+    Core.register(jolokia, $scope, {
+      type: 'exec', mbean: getSelectionBundleMBean(workspace),
+      operation: 'listBundles()'
+    }, onSuccess(render));
+
+  }
 }
--- hawtio-web/src/main/webapp/app/osgi/js/osgiPlugin.ts
+++ hawtio-web/src/main/webapp/app/osgi/js/osgiPlugin.ts
@@ -1,6 +1,6 @@
 module Osgi {
   var pluginName = 'osgi';
-  angular.module(pluginName, ['bootstrap', 'ngResource', 'hawtioCore']).config(($routeProvider) => {
+  angular.module(pluginName, ['bootstrap', 'ngResource', 'ngGrid', 'hawtioCore']).config(($routeProvider) => {
     $routeProvider.
             when('/osgi/bundles', {templateUrl: 'app/osgi/html/bundles.html'}).
             when('/osgi/bundle/:bundleId', {templateUrl: 'app/osgi/html/bundle.html'}).
@@ -18,37 +18,9 @@ module Osgi {
               content: "OSGi",
               title: "Visualise and manage the bundles and services in this OSGi container",
               isValid: (workspace: Workspace) => workspace.treeContainsDomainAndProperties("osgi.core"),
-              href: () => "#/osgi/bundles?tab=osgiTab",
-              isActive: (workspace: Workspace) => workspace.isTopTabActive("osgiTab")
+              href: () => "#/osgi/bundles",
+              isActive: (workspace: Workspace) => workspace.isLinkActive("osgi")
             });
-
-            workspace.subLevelTabs.push( {
-              content: '<i class="icon-list"></i> Bundles',
-              title: "View the available bundles in this OSGi container",
-              isValid: (workspace: Workspace) => workspace.isOsgiFolder(),
-              href: () => "#/osgi/bundles"
-            });
-
-            workspace.subLevelTabs.push({
-              content: '<i class="icon-list"></i> Packages',
-              title: "View the available packages in this OSGi container",
-              isValid: (workspace: Workspace) => workspace.isOsgiFolder(),
-              href: () => "#/osgi/packages"
-            });
-
-            workspace.subLevelTabs.push({
-                  content: '<i class="icon-list"></i> Services',
-                  title: "View the available services in this OSGi container",
-                  isValid: (workspace: Workspace) => workspace.isOsgiFolder(),
-                  href: () => "#/osgi/services"
-              });
-
-          workspace.subLevelTabs.push({
-              content: '<i class="icon-list"></i> Configuration',
-              title: "View the available configuration in this OSGi container",
-              isValid: (workspace: Workspace) => workspace.isOsgiCompendiumFolder(),
-              href: () => "#/osgi/configurations"
-          });
 
       });
 
