10af32280073e94e47f9c7298567653d4cd64c7d
davsclaus <claus.ibsen@gmail.com>
2013-02-26 20:06:49 +0100

    Fixed tomcat plugin to work with catalina or tomcat mbeans

--- hawtio-web/src/main/webapp/app/tomcat/js/tomcat.ts
+++ hawtio-web/src/main/webapp/app/tomcat/js/tomcat.ts
@@ -1,30 +1,26 @@
 module Tomcat {
     export function TomcatController($scope, $location:ng.ILocationService, jolokia) {
 
-        $scope.webAppObjectNames = jolokia.search("Tomcat:j2eeType=WebModule,*");
+        $scope.webAppObjectNames = jolokia.search("*:j2eeType=WebModule,*")
 
         $scope.webAppName = function webAppName(name) {
-            console.log("get attr displayName on mbean " + name);
             return jolokia.getAttribute(name, "displayName")
         }
 
         $scope.webAppContextPath = function webAppContextPath(name) {
-            console.log("get attr path on mbean " + name);
             return jolokia.getAttribute(name, "path")
         }
 
         $scope.webAppOperation = function webAppOperation(name,operation) {
-            console.log("Executing " + operation + " on mbean " + name);
             return jolokia.execute(name,operation);
         };
 
         $scope.wepAppState = function webAppState(name) {
-            console.log("get attr stateName on mbean " + name);
-            return jolokia.getAttribute(name, "stateName");
+            return jolokia.getAttribute(name, "stateName")
         }
 
         $scope.isEmpty = function isEmpty(map) {
-            return Object.keys(map).length === 0;
+            return Object.keys(map).length === 0
         }
     }
 }
