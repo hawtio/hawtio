2464fd5ff69799d627a505811fbb2987eb3fa5ec
davsclaus <claus.ibsen@gmail.com>
2013-03-01 16:06:45 +0100

    Migrated jboss plugin to grid table

--- hawtio-web/src/main/webapp/app/jboss/js/jboss.ts
+++ hawtio-web/src/main/webapp/app/jboss/js/jboss.ts
@@ -1,24 +1,127 @@
 module JBoss {
     export function JBossController($scope, $location:ng.ILocationService, jolokia) {
 
-        $scope.webAppObjectNames = jolokia.search("jboss.as:deployment=*")
+        $scope.webapps = [];
+        $scope.selected = [];
+        $scope.search = "";
 
-        $scope.webAppName = function webAppName(name) {
-            var name = jolokia.getAttribute(name, "name")
-            // TODO: a better way to drop .war from the name
-            return name.substring(0, name.length - 4)
-        }
+        var columnDefs: any[] = [
+            {
+                field: 'name',
+                displayName: 'Name',
+                cellFilter: null,
+                width: "*",
+                resizable: true
+            },
+            {
+                field: 'contextPath',
+                displayName: 'Context-Path',
+                cellFilter: null,
+                width: "*",
+                resizable: true
+            },
+            {
+                field: 'state',
+                displayName: 'State',
+                cellFilter: null,
+                width: "*",
+                resizable: false
+            }
+        ];
 
-        $scope.webAppOperation = function webAppOperation(name,operation) {
-            return jolokia.execute(name,operation);
+        $scope.gridOptions = {
+            data: 'webapps',
+            displayFooter: false,
+            selectedItems: $scope.selected,
+            selectWithCheckboxOnly: true,
+            columnDefs: columnDefs,
+            filterOptions: {
+                filterText: 'search'
+            }
         };
 
-        $scope.wepAppState = function webAppState(name) {
-            return jolokia.getAttribute(name, "status")
+        function render(response) {
+            console.log("render -> " + response)
+            $scope.webapps = [];
+            $scope.selected.length = 0;
+
+            // create structure for each response
+            angular.forEach(response, function(value, key) {
+                var obj = {
+                    mbeanName: value,
+                    name: cleanWebAppName(jolokia.getAttribute(value, "name")),
+                    // we do not have a jmx attribute for the context-path
+                    contextPath: cleanContextPath(jolokia.getAttribute(value, "name")),
+                    state: jolokia.getAttribute(value, "status")
+                };
+                $scope.webapps.push(obj);
+            });
+            $scope.$apply();
+        };
+
+        // function to trigger reloading page
+        $scope.onResponse = function () {
+            jolokia.search("jboss.as:deployment=*",
+                {
+                    success: render,
+                    error: render
+                });
         }
 
-        $scope.isEmpty = function isEmpty(map) {
-            return Object.keys(map).length === 0
+        // function to control the web applications
+        $scope.controlWebApps = function(op) {
+            // grab id of mbean names to control
+            var ids = $scope.selected.map(function(b) { return b.mbeanName });
+            if (!angular.isArray(ids)) {
+                ids = [ids];
+            }
+
+            // execute operation on each mbean
+            ids.forEach((id) => {
+                jolokia.request({
+                        type: 'exec',
+                        mbean: id,
+                        operation: op,
+                        arguments: null
+                    },
+                    {
+                        success: $scope.onResponse,
+                        error: $scope.onResponse
+                    });
+            });
         }
+
+        $scope.start = function() {
+            $scope.controlWebApps('deploy');
+        }
+
+        $scope.stop = function() {
+            $scope.controlWebApps('undeploy');
+        }
+
+        $scope.reload = function() {
+            $scope.controlWebApps('redeploy');
+        }
+
+        $scope.uninstall = function() {
+            $scope.controlWebApps('remove');
+        }
+
+        // register to core to poll a search for the web apps so the page is dynamic updated
+        Core.registerSearch(jolokia, $scope, "jboss.as:deployment=*", onSuccess(render));
+
+        function cleanWebAppName(name: string) {
+            // JBoss may include .war as the application name, so remove that
+            if (name.lastIndexOf(".war") > -1) {
+                return name.replace(".war", "")
+            } else {
+                return name
+            }
+        }
+
+        function cleanContextPath(contextPath: string) {
+            return "/" + cleanWebAppName(contextPath)
+        }
+
     }
 }
