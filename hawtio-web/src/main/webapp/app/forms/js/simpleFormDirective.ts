1b83147747b44b6ddc43f4829af8ffbea644828c
Stan Lewis <slewis@fusesource.com>
2013-03-22 14:09:45 -0400

    Actually show some forms using 3 different configuration methods

--- hawtio-web/src/main/webapp/app/forms/js/simpleFormDirective.ts
+++ hawtio-web/src/main/webapp/app/forms/js/simpleFormDirective.ts
@@ -3,22 +3,93 @@ module Forms {
   export class SimpleForm {
 
     public restrict = 'A';
+    public replace = true;
+    public transclude = true;
+
+    // see constructor for why this is here...
+    public link: (scope, element, attrs) => any;
+
+    private method = 'post';
+    private data:any = {};
+    private json:any = undefined;
+    private args = [];
+    private url = '';
 
     constructor(private workspace) {
 
+      // necessary to ensure 'this' is this object <sigh>
+      this.link = (scope, element, attrs) => {
+        return this.doLink(scope, element, attrs);
+      }
+
     }
 
-    public link(scope, element, attrs) {
+    private sanitize(args) {
+      if (args) {
+        args.forEach( function (arg) {
+          if (angular.isDefined(arg.formType)) {
+            // user-defined input type
+            return;
+          }
+          switch (arg.type) {
+            case "int":
+            case "long":
+              arg.formType = "number";
+              break;
+            default:
+              arg.formType = "text";
+          }
+        });
+      }
 
+      return args;
+    };
+
+    private doLink(scope, element, attrs) {
       var config = scope[attrs['simpleForm']];
 
-      var method = 'post';
-      var data = {};
-      var url = '';
+      this.configure(config, attrs);
 
+      //console.log("attrs: ", attrs);
+      //console.log("config: ", config);
+      //console.log("This: ", this);
 
-      console.log("attrs: ", attrs);
-      console.log("config: ", config);
+      if (angular.isDefined(this.json)) {
+        this.data = $.parseJSON(this.json);
+      } else {
+        this.data = scope[this.data];
+      }
+      //console.log("data: ", this.data);
+      this.args = this.sanitize(this.data.args);
+
+      console.log("args:", this.args);
+
+      var form = this.createForm();
+
+      var addInput = function(arg) {
+        var input = this.getInput(arg);
+        form.append(input);
+      }
+
+      this.args.forEach(addInput, this);
+
+      $(element).append(form);
+    }
+
+    private createForm() {
+      return $('<form class="form-horizontal no-bottom-margin"><fieldset><legend>' + this.data.desc + '</legend><div class="control-group"></div></fieldset></form>');
+    }
+
+    // TODO: support more input types, i.e. checkboxes/radio/select
+    private getInput(arg) {
+      return $('<div class="control-group" title="' + arg.desc + '"><label class="control-label">' + arg.name.capitalize() + ': </label><div class="controls"><input type="' + arg.formType + '"><span class="help-block">type: ' + arg.type + '</span></div></div>');
+    }
+
+    private configure(config, attrs) {
+      if (angular.isDefined(config)) {
+        angular.extend(this, config);
+      }
+      angular.extend(this, attrs);
     }
 
   }
--- hawtio-web/src/main/webapp/app/forms/js/test.ts
+++ hawtio-web/src/main/webapp/app/forms/js/test.ts
@@ -4,9 +4,35 @@ module Forms {
     $scope.config = {
       url: "/some/url",
       method: "post",
-      data: {
+      data: 'setVMOption'
+    }
 
-      }
+    $scope.setVMOption = {
+      args: [
+        {
+          desc: 'Argument key',
+          name: 'key',
+          type: 'java.lang.String'
+        },
+        {
+          desc: 'Argument Value',
+          name: 'value',
+          type: 'java.lang.String'
+        },
+        {
+          desc: 'Long argument',
+          name: 'longArg',
+          type: 'Long'
+        },
+        {
+          desc: 'Int argument',
+          name: 'intArg',
+          type: 'Integer'
+        }
+          // TODO - add more types, above is what I remember from jolokia
+        ],
+      desc: 'Show some stuff in a form',
+      ret: 'java.lang.String'
     }
 
   }
