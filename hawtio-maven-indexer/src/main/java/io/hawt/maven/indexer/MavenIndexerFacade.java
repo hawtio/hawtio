3fc6da0750da814dad60395f91a85c61d6fd2db4
James Strachan <james.strachan@gmail.com>
2013-03-25 19:25:48 +0000

    first spike of #216 so we can compose server side resources in hawtio wars using blueprint so we don't have to keep hacking the web.xml to initialise all the various server side plugins

--- hawtio-maven-indexer/src/main/java/io/hawt/maven/indexer/MavenIndexerFacade.java
+++ hawtio-maven-indexer/src/main/java/io/hawt/maven/indexer/MavenIndexerFacade.java
@@ -1,6 +1,5 @@
 package io.hawt.maven.indexer;
 
-import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
@@ -97,54 +96,51 @@ public class MavenIndexerFacade implements MavenIndexerFacadeMXBean {
         }
 
         // now lets create all the indexers
-        for (String repository : repositories) {
-            if (StringUtils.isNotBlank(repository)) {
-                String url = repository;
-                String id = repository;
-                int idx = repository.indexOf('@');
-                if (idx > 0) {
-                    url = repository.substring(0, idx);
-                    id = repository.substring(idx + 1);
-                }
-                File repoDir = new File(cacheDirectory, id);
-                File cacheDir = new File(repoDir, "cache");
-                File indexDir = new File(repoDir, "index");
-                cacheDir.mkdirs();
-                indexDir.mkdirs();
-                String contextId = id + "-context";
+        Thread thread = new Thread("MavenIndexer index thread") {
+            @Override
+            public void run() {
+                try {
+                    for (String repository : repositories) {
+                        if (StringUtils.isNotBlank(repository)) {
+                            String url = repository;
+                            String id = repository;
+                            int idx = repository.indexOf('@');
+                            if (idx > 0) {
+                                url = repository.substring(0, idx);
+                                id = repository.substring(idx + 1);
+                            }
+                            File repoDir = new File(cacheDirectory, id);
+                            File cacheDir = new File(repoDir, "cache");
+                            File indexDir = new File(repoDir, "index");
+                            cacheDir.mkdirs();
+                            indexDir.mkdirs();
+                            String contextId = id + "-context";
 
-                IndexingContext repoContext = indexer.createIndexingContext(contextId, id, cacheDir, indexDir,
-                        url, null, true, true, indexers);
-                indexContexts.put(id, repoContext);
-            }
-            File mergedDir = new File(cacheDirectory, "all");
-            File cacheDir = new File(mergedDir, "cache");
-            File indexDir = new File(mergedDir, "index");
-            ContextMemberProvider members = new StaticContextMemberProvider(indexContexts.values());
-            mergedContext = indexer.createMergedIndexingContext("all-context", "all", cacheDir, indexDir, true, members);
-        }
-
-        if (updateIndexOnStartup) {
-            Thread thread = new Thread("MavenIndexer reindex thread") {
-                @Override
-                public void run() {
-                    try {
+                            IndexingContext repoContext = indexer.createIndexingContext(contextId, id, cacheDir, indexDir,
+                                    url, null, true, true, indexers);
+                            indexContexts.put(id, repoContext);
+                        }
+                        File mergedDir = new File(cacheDirectory, "all");
+                        File cacheDir = new File(mergedDir, "cache");
+                        File indexDir = new File(mergedDir, "index");
+                        ContextMemberProvider members = new StaticContextMemberProvider(indexContexts.values());
+                        mergedContext = indexer.createMergedIndexingContext("all-context", "all", cacheDir, indexDir, true, members);
+                    }
+                    if (updateIndexOnStartup) {
                         downloadOrUpdateIndices();
-                    } catch (IOException e) {
-                        LOG.error("Failed to update the maven repository indices: " + e, e);
                     }
-                    try {
-                        registerMBean();
-                    } catch (Exception e) {
-                        LOG.error("Failed to register MBean: " + e, e);
-                    }
-                    startedSignal.countDown();
+                } catch (IOException e) {
+                    LOG.error("Failed to update the maven repository indices: " + e, e);
                 }
-            };
-            thread.run();
-        } else {
-            registerMBean();
-        }
+                try {
+                    registerMBean();
+                } catch (Exception e) {
+                    LOG.error("Failed to register MBean: " + e, e);
+                }
+                startedSignal.countDown();
+            }
+        };
+        thread.run();
     }
 
     protected void registerMBean() throws MalformedObjectNameException, InstanceAlreadyExistsException, MBeanRegistrationException, NotCompliantMBeanException {
