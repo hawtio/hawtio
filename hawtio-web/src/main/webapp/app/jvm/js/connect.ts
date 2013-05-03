e70c2d2dacc96b841b8072a7670ac67ca0ded047
James Strachan <james.strachan@gmail.com>
2013-05-03 10:09:05 +0100

    fixes #322

--- hawtio-web/src/main/webapp/app/jvm/js/connect.ts
+++ hawtio-web/src/main/webapp/app/jvm/js/connect.ts
@@ -3,8 +3,23 @@ module JVM {
 
     JVM.configureScope($scope, $location, workspace);
 
-    $scope.gotoServer = (url) => {
-      console.log("going to server: " + url);
+    $scope.host = "localhost";
+    $scope.port = 8181;
+    $scope.path = "jolokia";
+    $scope.useProxy = true;
+    $scope.userName = null;
+    $scope.password = null;
+
+    $scope.gotoServer = () => {
+      var host = $scope.host || "localhost";
+      var port = $scope.port || 80;
+      var path = Core.trimLeading($scope.path || "jolokia", "/");
+      var url = host + ":" + port + "/" + path;
+
+      if ($scope.useProxy) {
+        url = "/hawtio/proxy/" + url;
+      }
+      console.log("going to server: " + url + " as user " + $scope.userName);
 
       var full = "?url=" + encodeURIComponent(url);
 
@@ -12,9 +27,9 @@ module JVM {
         full += "&_user=" + $scope.userName;
       }
       if ($scope.password) {
-        full += "&pwd=" + $scope.password;
+        full += "&_pwd=" + $scope.password;
       }
-      window.open();
+      window.open(full);
     }
   }
 }
