73fe1edff87cb7fc73b0fc203799cdb5e5e0d1db
James Strachan <james.strachan@gmail.com>
2013-05-03 10:54:28 +0100

    improve #322 so we store previous values of user/pwd/port so they are reused

--- hawtio-web/src/main/webapp/app/jvm/js/connect.ts
+++ hawtio-web/src/main/webapp/app/jvm/js/connect.ts
@@ -1,14 +1,37 @@
 module JVM {
-  export function ConnectController($scope, $location, workspace) {
+  export function ConnectController($scope, $location, localStorage, workspace) {
 
     JVM.configureScope($scope, $location, workspace);
 
     $scope.host = "localhost";
-    $scope.port = 8181;
     $scope.path = "jolokia";
     $scope.useProxy = true;
-    $scope.userName = null;
-    $scope.password = null;
+
+    // lets load the local storage configuration
+    var key = "jvmConnect";
+    var config = {};
+    var configJson = localStorage[key];
+    if (configJson) {
+      try {
+        config = JSON.parse(configJson);
+      } catch (e) {
+        // ignore
+      }
+    }
+    $scope.port = config["port"] || 8181;
+    $scope.userName = config["userName"];
+    $scope.password = config["password"];
+
+    // replicate changes to local storage
+    angular.forEach(["userName", "password", "port"], (name) => {
+      $scope.$watch(name, () => {
+        var value = $scope[name];
+        if (value) {
+          config[name] = value;
+          localStorage[key] = JSON.stringify(config);
+        }
+      });
+    });
 
     $scope.gotoServer = () => {
       var host = $scope.host || "localhost";
