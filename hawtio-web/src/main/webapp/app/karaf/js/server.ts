003b944cc869e685bfe896d94e3a0b55e39cba98
James Strachan <james.strachan@gmail.com>
2013-05-03 13:05:11 +0100

    added more data to the karaf server page & made it more async

--- hawtio-web/src/main/webapp/app/karaf/js/server.ts
+++ hawtio-web/src/main/webapp/app/karaf/js/server.ts
@@ -1,62 +1,68 @@
 module Karaf {
 
-    export function ServerController($scope, $location, workspace:Workspace, jolokia) {
+  export function ServerController($scope, $location, workspace:Workspace, jolokia) {
 
-      $scope.data = {
-        name: "",
-        version: "",
-        state: "",
-        root: "",
-        location: "",
-        sshPort: "",
-        rmiRegistryPort: "",
-        rmiServerPort: "",
-        pid: ""};
+    $scope.data = {
+      name: "",
+      version: "",
+      state: "",
+      root: "",
+      location: "",
+      sshPort: "",
+      rmiRegistryPort: "",
+      rmiServerPort: "",
+      pid: ""};
 
-      function render(response) {
-        console.log("Response is " + response);
+    $scope.$on('jmxTreeUpdated', reloadFunction);
+    $scope.$watch('workspace.tree', reloadFunction);
 
-        // grab the first mbean as there should ideally only be one karaf in the JVM
-        if (angular.isArray(response)) {
-          var mbean = response[0];
-          if (mbean) {
-            var instances = jolokia.getAttribute(mbean, "Instances", onSuccess(null));
-            if (instances) {
-              console.log("Instances is " + instances);
-              // the name is the first child
-                $scope.data.name = "TODO";
-                // TODO: we need to get version from another mbean
-                $scope.data.version = "TODO";
-                $scope.data.state = instances['root'].State;
-                $scope.data.root = "TODO";
-                $scope.data.location = instances['root'].Location;
-                $scope.data.sshPort = "TODO";
-                $scope.data.rmiRegistryPort = "TODO";
-                $scope.data.rmiServerPort = "TODO";
-                $scope.data.pid = instances['root'].Pid;
-            }
-          }
-        }
+    // TODO: we need the framework and version from another mbean
 
-        // ensure web page is updated
-        Core.$apply($scope);
-      }
-
-      // TODO: we need the framework and version from another mbean
-
-      function loadData() {
-        console.log("Loading Karaf data...");
-        jolokia.search("org.apache.karaf:type=admin,*", onSuccess(render));
-      }
-
-      $scope.$on('jmxTreeUpdated', reloadFunction);
-      $scope.$watch('workspace.tree', reloadFunction);
-
-      function reloadFunction() {
-        // if the JMX tree is reloaded its probably because a new MBean has been added or removed
-        // so lets reload, asynchronously just in case
-        setTimeout(loadData, 50);
-      }
+    function reloadFunction() {
+      // if the JMX tree is reloaded its probably because a new MBean has been added or removed
+      // so lets reload, asynchronously just in case
+      setTimeout(loadData, 50);
     }
 
+    function loadData() {
+      console.log("Loading Karaf data...");
+      jolokia.search("org.apache.karaf:type=admin,*", onSuccess(render));
+    }
+
+    function render(response) {
+      console.log("Response is " + response);
+
+      // grab the first mbean as there should ideally only be one karaf in the JVM
+      if (angular.isArray(response)) {
+        var mbean = response[0];
+        if (mbean) {
+          jolokia.getAttribute(mbean, "Instances", onSuccess(onInstances));
+        }
+      }
+
+      // ensure web page is updated
+      Core.$apply($scope);
+    }
+
+    function onInstances(instances) {
+      if (instances) {
+        console.log("Instances is " + JSON.stringify(instances));
+        // the name is the first child
+        var rootInstance = instances['root'];
+        $scope.data.name = rootInstance.Name;
+        $scope.data.state = rootInstance.State;
+        $scope.data.root = rootInstance["Is Root"];
+        $scope.data.location = rootInstance.Location;
+        $scope.data.sshPort = rootInstance["SSH Port"];
+        $scope.data.rmiRegistryPort = rootInstance["RMI Registry Port"];
+        $scope.data.rmiServerPort = rootInstance["RMI Server Port"];
+        $scope.data.pid = rootInstance.Pid;
+
+        // TODO: we need to get version from another mbean
+        $scope.data.version = "";
+
+        Core.$apply($scope);
+      }
+    }
+  }
 }
