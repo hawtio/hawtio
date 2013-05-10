9595c9a58c851c643d9504979b433708ee39a7ba
James Strachan <james.strachan@gmail.com>
2013-05-10 20:46:17 +0100

    #327 allow navigation to the detail page of a dependency

--- hawtio-web/src/main/webapp/app/maven/js/dependencies.ts
+++ hawtio-web/src/main/webapp/app/maven/js/dependencies.ts
@@ -1,6 +1,6 @@
 module Maven {
 
-  export function DependenciesController($scope, $routeParams, workspace:Workspace, jolokia) {
+  export function DependenciesController($scope, $routeParams, $location, workspace:Workspace, jolokia) {
     $scope.artifacts = [];
     $scope.group = $routeParams["group"] || "";
     $scope.artifact = $routeParams["artifact"] || "";
@@ -21,12 +21,26 @@ module Maven {
       updateTableContents();
     });
 
-    $scope.onSelectNode = (rootNode) => {
-       // process the rootNode
+    $scope.onSelectNode = (node) => {
+       $scope.selected = node;
     };
 
     $scope.onRootNode = (rootNode) => {
        // process the rootNode
+    };
+
+    $scope.validSelection = () => {
+      return $scope.selected && $scope.selected !== $scope.rootDependency;
+    };
+
+    $scope.viewDetails = () => {
+      var dependency = Core.pathGet($scope.selected, ["dependency"]);
+      var link = $scope.detailLink(dependency);
+      if (link) {
+        var path = Core.trimLeading(link,  "#");
+        console.log("going to view " + path);
+        $location.path(path);
+      }
     };
 
     function updateTableContents() {
@@ -49,16 +63,18 @@ module Maven {
           $scope.dependencyActivations = [];
           addChildren($scope.dependencyTree, json);
           $scope.dependencyActivations.reverse();
+          $scope.rootDependency = $scope.dependencyTree.children[0];
         }
       }
       Core.$apply($scope);
     }
 
     function addChildren(folder, dependency) {
-      var title = Maven.getName(dependency);
-      var node = new Folder(title);
-      node.key = title;
-      $scope.dependencyActivations.push(title);
+      var name = Maven.getName(dependency);
+      var node = new Folder(name);
+      node.key = name.replace(/\//g, '_');
+      node["dependency"] = dependency;
+      $scope.dependencyActivations.push(node.key);
 
 /*
       var imageUrl = Camel.getRouteNodeIcon(value);
