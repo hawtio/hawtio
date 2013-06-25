0550d47c3479dfe85feec0e88f322ae2e87ffdd5
Stan Lewis <slewis@fusesource.com>
2013-06-25 11:23:58 -0400

    Add jquery form plugin, use ajaxSubmit to submit the form when files are selected

--- hawtio-web/src/main/webapp/app/ui/js/fileUpload.ts
+++ hawtio-web/src/main/webapp/app/ui/js/fileUpload.ts
@@ -9,9 +9,15 @@ module UI {
 
     public templateUrl = UI.templatePath + "fileUpload.html";
 
+    public scope = {
+      target: '@hawtioFilePlugin'
+    };
+
+
     public controller = ($scope, $element, $attrs, jolokia) => {
 
       $scope.files = [];
+      $scope.target = '';
 
       $scope.update = (response) => {
         $scope.files = response.value;
@@ -23,6 +29,40 @@ module UI {
         operation: 'list(java.lang.String)',
         arguments: [null]
       }, onSuccess($scope.update));
+
+      $scope.onFileChange = () => {
+        console.log("File changed!");
+        var form:any = $('form[name=file-upload]');
+
+        form.ajaxSubmit({});
+
+        return false;
+      }
+
+    };
+
+
+    public link = ($scope, $element, $attrs) => {
+
+      var fileInput = $element.find('input[type=file]');
+      var form = $element.find('form[name=file-upload]');
+
+      form.submit(() => {
+        return false;
+      });
+
+      if ($.browser.msie) {
+        fileInput.click((event) => {
+          setTimeout(() => {
+            if (fileInput.val().length > 0) {
+              $scope.onFileChange();
+            }
+          }, 0);
+        })
+      } else {
+        fileInput.change($scope.onFileChange);
+      }
+
 
     };
 
