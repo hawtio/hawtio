c164a0584702dd1891fba06eb29703655fbb2b0f
James Strachan <james.strachan@gmail.com>
2013-07-04 12:20:07 +0100

    avoid the apollo plugin taking over the world & causing trouble :) fixes glitches in the hawtio UI

--- hawtio-web/src/main/webapp/app/apollo/js/apolloPlugin.ts
+++ hawtio-web/src/main/webapp/app/apollo/js/apolloPlugin.ts
@@ -4,8 +4,8 @@ module Apollo {
     module(pluginName, ['bootstrap', 'ngResource', 'hawtioCore']).
     config(($routeProvider) => {
       $routeProvider.
-        // when('/apollo', {templateUrl: 'app/apollo/html/layout-apollo.html'}).
-        otherwise({templateUrl: 'app/apollo/html/layout-apollo.html'})
+        when('/apollo', {templateUrl: 'app/apollo/html/layout-apollo.html'})
+        //otherwise({templateUrl: 'app/apollo/html/layout-apollo.html'})
     }).
     run(($location: ng.ILocationService, workspace:Workspace, viewRegistry) => {
       viewRegistry['apollo'] = "app/apollo/html/layout-apollo.html";
