8e61610e5245c8b802b7bfa37f1fa1995212d483
James Strachan <james.strachan@gmail.com>
2013-09-20 10:09:25 +0100

    tidied imports

--- hawtio-git/src/test/java/io/hawt/git/GitCloneTest.java
+++ hawtio-git/src/test/java/io/hawt/git/GitCloneTest.java
@@ -1,24 +1,12 @@
 package io.hawt.git;
 
-import org.eclipse.jgit.api.Status;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.management.ObjectName;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
 import static io.hawt.git.GitFacadeTest.assertConfigDirectoryExists;
 import static io.hawt.git.GitFacadeTest.assertFileContents;
 import static io.hawt.git.GitFacadeTest.createTestGitFacade;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Tests we can clone a remote repo
