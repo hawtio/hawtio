fe55f187d7853b98bb533e77cf5082034550a581
Prashant Gupta <prashant.gupta@middleware360.com>
2013-10-30 14:51:37 +0530

    refactored lobs list controller. Ref# HAWT-4

--- hawtio-web/src/main/webapp/app/springbatch/js/springbatch.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/springbatch.ts
@@ -1,13 +1,11 @@
 module SpringBatch {
-    var springBatchServerPath ='localhost:8181/jobs.json';
+    var springBatchServerPath ='localhost\\:8181/jobs.json';
     var proxyUrl = '/hawtio/proxy/';
-    export function JobListController($scope, $location, workspace:Workspace, jolokia, $http, $resource) {
+    export function JobListController($scope, $location, workspace:Workspace, jolokia, $resource) {
 
-        $http.get((proxyUrl+springBatchServerPath),{
-            cache:false
-        }).success(function(data){
-                if(data.jobs && data.jobs.registrations) $scope.jobList = data.jobs.registrations;
-            });
-
+        var jobList = $resource(proxyUrl+springBatchServerPath);
+        jobList.get(function(data){
+            if(data.jobs && data.jobs.registrations) $scope.jobList = data.jobs.registrations;
+        });
     }
 }
