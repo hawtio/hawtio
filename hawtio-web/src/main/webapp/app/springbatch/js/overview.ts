fb68702ec9078b97fc441a61106fd405fbc73b79
Prashant Gupta <prashant.gupta@middleware360.com>
2013-10-31 17:24:31 +0530

    implemented next and previous scroll functionality and fixed refresh functionality. Ref# HAWT-7

--- hawtio-web/src/main/webapp/app/springbatch/js/overview.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/overview.ts
@@ -29,8 +29,62 @@ module SpringBatch {
                 for(var job in data.job.jobInstances){
                     data.job.jobInstances[job].id=job;
                 }
-                $scope.jobInstance = data.job.jobInstances[jobInstance.id];
+                var jobInstanceId=null;
+                if(jobInstance && jobInstance.id){
+                    jobInstanceId = jobInstance.id;
+                    $scope.jobInstance = data.job.jobInstances[jobInstanceId];
+                }else{
+                    for(var job in data.job.jobInstances){
+                        $scope.jobInstance = jobInstances[job];
+                        break;
+                    }
+                }
             });
-        }
+        };
+        $scope.fetchNextJobInstance = function(jobInstance){
+            var tempId=null;
+            var jobList = $resource(proxyUrl+springBatchServerPath);
+            jobList.get({'jobName':jobName+'.json'},function(data){
+                for(var job in data.job.jobInstances){
+                    data.job.jobInstances[job].id=job;
+                    if(jobInstance && jobInstance.id && (job>jobInstance.id)){
+                        tempId=job;
+                        break;
+                    }else{tempId = job;}
+                }
+                if(jobInstance){
+                    $scope.jobInstance = data.job.jobInstances[tempId];
+                }
+                else{
+                    for(var job in data.job.jobInstances){
+                        $scope.jobInstance = jobInstances[job];
+                        break;
+                    }
+                }
+            });
+        };
+        $scope.fetchPrevJobInstance = function(jobInstance){
+            var tempId=null;
+            var jobList = $resource(proxyUrl+springBatchServerPath);
+            jobList.get({'jobName':jobName+'.json'},function(data){
+                for(var job in data.job.jobInstances){
+                    data.job.jobInstances[job].id=job;
+                    if(jobInstance && jobInstance.id && (job<jobInstance.id)){
+                        tempId=job;
+                    }
+                }
+                if(jobInstance){
+                    if((tempId == null) && jobInstance.id){tempId = jobInstance.id;}
+                    $scope.jobInstance = data.job.jobInstances[tempId];
+                }
+                else{
+                    for(var job in data.job.jobInstances){
+                        $scope.jobInstance = jobInstances[job];
+                        break;
+                    }
+                }
+            });
+        };
+
     }
 }
