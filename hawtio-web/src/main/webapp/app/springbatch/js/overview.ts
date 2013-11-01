376b5a7444a27173ec059c69324a2bb8b02e2b6e
Amit Pandey <amit.pandey@middleware360.com>
2013-11-01 14:54:57 +0530

    Commit for sbio executions by job - executions. Ref#HAWT-9

--- hawtio-web/src/main/webapp/app/springbatch/js/overview.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/overview.ts
@@ -1,11 +1,26 @@
 module SpringBatch {
     var springBatchServerPath ='localhost\\:8080/spring-batch-admin-sample/jobs/:jobName';
     var proxyUrl = '/hawtio/proxy/';
+    var executionsListPath='/:jobInstanceId/executions.json';
+
     export function JobOverviewExecListController($scope,$routeParams, $location, workspace:Workspace, jolokia, $resource) {
 
         var jobName = $routeParams.jobName;
         var jobInstances = null;
         var jobList = $resource(proxyUrl+springBatchServerPath);
+
+        $scope.fetchAllExecutions = function(jobInstance){
+            if(jobInstance != undefined){
+                var jobList = $resource(proxyUrl+springBatchServerPath+executionsListPath);
+                jobList.get({'jobName':jobName,jobInstanceId:jobInstance.id},function(data){
+                    for(var execution in data.jobInstance.jobExecutions){
+                        data.jobInstance.jobExecutions[execution].id=execution;
+                    }
+                    $scope.jobExecutionList = data.jobInstance.jobExecutions;
+                });
+            }
+        };
+
         jobList.get({'jobName':jobName+'.json'},function(data){
             for(var job in data.job.jobInstances){
                 data.job.jobInstances[job].id=job;
@@ -22,7 +37,11 @@ module SpringBatch {
 
                 }
             }
+            if($scope.jobInstance){
+                $scope.fetchAllExecutions($scope.jobInstance);
+            }
         });
+
         $scope.refreshJobInstance = function(jobInstance){
             var jobList = $resource(proxyUrl+springBatchServerPath);
             jobList.get({'jobName':jobName+'.json'},function(data){
@@ -33,12 +52,15 @@ module SpringBatch {
                 if(jobInstance && jobInstance.id){
                     jobInstanceId = jobInstance.id;
                     $scope.jobInstance = data.job.jobInstances[jobInstanceId];
+                    $scope.fetchAllExecutions(data.job.jobInstances[jobInstanceId]);
                 }else{
                     for(var job in data.job.jobInstances){
                         $scope.jobInstance = data.job.jobInstances[job];
+                        $scope.fetchAllExecutions(data.job.jobInstances[job]);
                         break;
                     }
                 }
+
             });
         };
         $scope.fetchNextJobInstance = function(jobInstance){
@@ -54,10 +76,12 @@ module SpringBatch {
                 }
                 if(jobInstance){
                     $scope.jobInstance = data.job.jobInstances[tempId];
+                    $scope.fetchAllExecutions(data.job.jobInstances[tempId]);
                 }
                 else{
                     for(var job in data.job.jobInstances){
                         $scope.jobInstance = data.job.jobInstances[job];
+                        $scope.fetchAllExecutions(data.job.jobInstances[job]);
                         break;
                     }
                 }
@@ -76,15 +100,18 @@ module SpringBatch {
                 if(jobInstance){
                     if((tempId == null) && jobInstance.id){tempId = jobInstance.id;}
                     $scope.jobInstance = data.job.jobInstances[tempId];
+                    $scope.fetchAllExecutions(data.job.jobInstances[tempId]);
                 }
                 else{
                     for(var job in data.job.jobInstances){
                         $scope.jobInstance = data.job.jobInstances[job];
+                        $scope.fetchAllExecutions(data.job.jobInstances[job]);
                         break;
                     }
                 }
             });
         };
+
 
     }
 }
