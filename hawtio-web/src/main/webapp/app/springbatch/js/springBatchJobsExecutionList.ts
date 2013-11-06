5aa5219eaeb80b07274852acfb3fe58612cf171d
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-06 18:39:07 +0530

    replaced $http with $resource. Ref# HAWT-8

--- hawtio-web/src/main/webapp/app/springbatch/js/springBatchJobsExecutionList.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/springBatchJobsExecutionList.ts
@@ -1,22 +1,17 @@
 module SpringBatch{
-    var springBatchServerOrigin = 'localhost:8080/spring-batch-admin-sample/';
+    var springBatchServerOrigin = 'localhost\\:8080/spring-batch-admin-sample/';
     var springBatchServerPath =springBatchServerOrigin+'jobs';
     var proxyUrl = '/hawtio/proxy/';
     var executionsListPath='/executions.json';
-    export function SpringBatchJobExecutionListController($scope, $http) {
+    export function SpringBatchJobExecutionListController($scope, $http, $resource) {
 
-        $http.get(proxyUrl+springBatchServerPath+executionsListPath,{
-            cache:false
-        }).success(function(data){
-                for(var execution in data.jobExecutions){
-                    data.jobExecutions[execution].id=execution;
-                }
-                console.info(data.jobExecutions)
-                $scope.jobExecutions=data.jobExecutions
-            }).error(function(data){
-
-            });
-
+        var executionListRes = $resource(proxyUrl+springBatchServerPath+executionsListPath);
+        executionListRes.get(function(data){
+            for(var execution in data.jobExecutions){
+                data.jobExecutions[execution].id=execution;
+            }
+            $scope.jobExecutions=data.jobExecutions
+        });
     }
 
 }
