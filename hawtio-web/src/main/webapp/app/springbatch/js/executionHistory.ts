57ee404b631cefd21b973ab6e173090cb70529d4
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-06 15:02:49 +0530

    implemented logic for viewing execution history of a given spring-batch job. Ref# HAWT-14

--- hawtio-web/src/main/webapp/app/springbatch/js/executionHistory.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/executionHistory.ts
@@ -1,6 +1,14 @@
 module SpringBatch {
+    var springBatchServerOrigin = 'localhost\\:8080/spring-batch-admin-sample/';
+    var proxyUrl = '/hawtio/proxy/';
+    var executionHistoryPath = 'jobs/:jobName/executions.json';
     export function ExecutionHistoryController($scope, $routeParams, $location, workspace:Workspace, $resource) {
-
-        console.info('------ hello wrold');
+        var jobExecutionRes = $resource(proxyUrl+springBatchServerOrigin+executionHistoryPath);
+        jobExecutionRes.get({'jobName':$routeParams.jobName},function(data){
+            for(var execution in data.jobExecutions){
+                data.jobExecutions[execution].id=execution;
+            }
+            $scope.executionHistory = data.jobExecutions;
+        })
     }
 }
