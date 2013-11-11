59e3939db96fa4c0365c1f5670eea5257e63630f
James Strachan <james.strachan@gmail.com>
2013-11-11 16:52:31 +0000

    initial spike of #740 for browsing WSDL APIs

---
+++ hawtio-web/src/main/webapp/app/wsdl/js/view.ts
@@ -0,0 +1,68 @@
+module Wsdl {
+
+  export function WsdlViewController($scope, $location, jolokia) {
+
+    $scope.url = $location.search()["wsdl"];
+    var log:Logging.Logger = Logger.get("Wsdl");
+    var wsdlNamespace = "http://schemas.xmlsoap.org/wsdl/";
+
+    if ($scope.url) {
+      log.info("Loading wsdl: " + $scope.url);
+
+      $.ajax({
+        type: "GET",
+        url: $scope.url,
+        dataType: "xml",
+        success: onWsdl
+      });
+    }
+
+    function onWsdl(response) {
+      log.info("Got WSDL: " + response);
+      $scope.services = [];
+      var root = response.documentElement;
+      var targetNamespace = root ? root.getAttribute("targetNamespace") : null;
+      var name = root ? root.getAttribute("name") : null;
+      var portTypes = response.getElementsByTagNameNS(wsdlNamespace, "portType");
+      var services = response.getElementsByTagNameNS(wsdlNamespace, "service");
+      var bindings = response.getElementsByTagNameNS(wsdlNamespace, "binding");
+
+      angular.forEach(portTypes, (portType) => {
+        var service = {
+          name: name,
+          targetNamespace: targetNamespace,
+          portName: portType.getAttribute("name") || "Unknown",
+          operations: []
+        };
+        $scope.services.push(service);
+        var operations = portType.getElementsByTagNameNS(wsdlNamespace, "operation");
+        angular.forEach(operations, (operation) => {
+          var input = operation.getElementsByTagNameNS(wsdlNamespace, "input");
+          var output = operation.getElementsByTagNameNS(wsdlNamespace, "output");
+
+          function createMessageData(data) {
+            var answer = [];
+            angular.forEach(data, (item) => {
+              var name = item.getAttribute("name");
+              if (name) {
+                answer.push({
+                  name: name
+                });
+              }
+            });
+            return answer;
+          }
+
+          var opData = {
+            name: operation.getAttribute("name") || "Unknown",
+            inputs: createMessageData(input),
+            outputs: createMessageData(output)
+          };
+          service.operations.push(opData);
+        });
+      });
+      Core.$apply($scope);
+    }
+
+  }
+}
