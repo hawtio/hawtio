43ca653b3db345ce891990185b9afa8739bb1eca
Stan Lewis <slewis@fusesource.com>
2013-11-12 16:30:56 -0500

    Fix #746, needs more testing with multiple containers when fabric is working better.

--- hawtio-web/src/main/webapp/app/fabric/js/assignProfile.ts
+++ hawtio-web/src/main/webapp/app/fabric/js/assignProfile.ts
@@ -1,9 +1,13 @@
 module Fabric {
 
-  export function AssignProfileController($scope, jolokia, $location, $routeParams) {
+  export function AssignProfileController($scope, jolokia, $location, $routeParams, workspace) {
 
     $scope.profileId = $routeParams['pid'];
     $scope.versionId = $routeParams['vid'];
+
+    Fabric.initScope($scope, $location, jolokia, workspace);
+
+    $scope.containerIdFilter = '';
 
     var valid = true;
 
@@ -21,14 +25,48 @@ module Fabric {
       $location.path("/fabric/view");
     }
 
+    $scope.gotoCreate = () => {
+      $location.path('/fabric/containers/createContainer').search({
+        versionId: $scope.versionId,
+        profileIds: $scope.profileId
+      });
+    };
+
     $scope.$on('$routeChangeSuccess', () => {
       log.debug("RouteParams: ", $routeParams);
       log.debug("Scope: ", $scope);
+
     });
 
-    $scope.$watch('selectedContainerIds', (newValue, oldValue) => {
-      log.debug("selected containers: ", newValue);
+    $scope.$watch('containers', (newValue, oldValue) => {
+      if (newValue !== oldValue && newValue) {
+        $scope.selected = newValue.filter((c) => { return c['selected']});
+      }
     }, true);
+
+    $scope.assignProfiles = () => {
+      var requests = [];
+      $scope.selected.forEach((c) => {
+        requests.push({
+          type: 'exec', mbean: Fabric.managerMBean,
+          operation: 'addProfilesToContainer',
+          arguments: [c.id, [$scope.profileId]]
+        });
+      });
+      notification('info', "Applying " + $scope.profileId + " to the selected containers");
+      var outstanding = requests.length;
+      jolokia.request(requests, onSuccess(() => {
+        outstanding = outstanding - 1;
+        if (outstanding === 0) {
+          notification('success', "Applied " + $scope.profileId);
+          Core.$apply($scope);
+        }
+      }));
+      setTimeout(() => {
+        $location.path("/fabric/activeProfiles");
+        Core.$apply($scope);
+      }, 30);
+    }
 
   }
 
