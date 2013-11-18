c906e3765bd5b49f3736c2756b1b19925da45952
Stan Lewis <slewis@fusesource.com>
2013-11-18 14:45:57 -0500

    Make jetty use src/main/webapp and target root, disable watch tasks and watch generated app.ts, move grunt to a profile

--- hawtio-web/GruntFile.js
+++ hawtio-web/GruntFile.js
@@ -11,53 +11,8 @@ module.exports = function (grunt) {
 
   if (!webapp_outdir) {
     webapp_outdir = 'target/hawtio-web-1.2-SNAPSHOT'
+    grunt.option('webapp_outdir', webapp_outdir);
   }
-
-  /*
-  no idea why this wouldn't work :-/
-  var fullBuild = ['clean-appjs', 'type', 'concat', 'copy:dist'];
-  var partialBuild = ['copy:dist'];
-
-  function endsWith(str, suffix) {
-    if (!str || !suffix || suffix.length > str.length) {
-      return false;
-    }
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-  }
-
-  var changedFiles = Object.create(null);
-
-  var onChange = grunt.util._.debounce(function() {
-    grunt.config.set('currentTasks', partialBuild);
-    grunt.log.writeln('tasks before checking files: ', currentTasks);
-    Object.keys(changedFiles).forEach(function(filename) {
-      grunt.log.writeln('Checking file: ', filename);
-      if (endsWith(filename, '.ts') ||
-         (endsWith(filename, '.js') && !endsWith(filename, 'GruntFile.js'))) {
-        grunt.log.writeln('need a full build for ', filename);
-        grunt.config.set('currentTasks', fullBuild);
-      }
-    });
-    grunt.log.writeln('tasks after checking files: ', currentTasks);
-    changedFiles = Object.create(null);
-  }, 200);
-
-  grunt.event.on('watch', function(action, filename) {
-    changedFiles[filename] = action;
-    onChange();
-  });
-
-  grunt.task.registerTask('conditionalBuild', 'Maybe build everything, maybe not', function() {
-    var done = this.async();
-    grunt.log.writeln('Checking build type...');
-    setTimeout(function() {
-      var tasks = grunt.config.get('currentTasks');
-      grunt.log.writeln("Running tasks: ", tasks);
-      grunt.task.run(tasks);
-      done();
-    }, 1000);
-  });
-  */
 
   grunt.task.registerTask('clean-appjs', 'Clean up generated app.js file', function() {
     var file = webapp_outdir + "/app/app.js";
@@ -126,8 +81,12 @@ module.exports = function (grunt) {
     },
     watch: {
       app: {
-        files: ["src/main/webapp/**", "target/schema/js/*.js"],
-        tasks: ['clean-appjs', 'type', 'concat', 'copy:dist'],
+        files: ['src/main/webapp/**',
+                '!src/main/webapp/**/*.ts',
+                'target/schema/js/*.js',
+                'GruntFile.js',
+                "<%= grunt.option('webapp_outdir') %>/app/app.js"],
+        tasks: [],
         options: {
           livereload: true,
           spawn: false
@@ -135,9 +94,6 @@ module.exports = function (grunt) {
       }
     }
   });
-
-   //files: ['src/main/webapp/**', '!src/main/webapp/**/*.ts', 'target/schema/js/*.js', 'GruntFile.js', "<%= grunt.option('webapp_outdir') %>/app/app.js"],
-   //tasks: ['copy'],
 
   //grunt.config.set('currentTasks', fullBuild);
 
