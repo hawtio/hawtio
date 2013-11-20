ababc5c915c1b3416627c70867d9c599969276a1
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-20 13:01:37 +0530

    implemented sortable headers on execution history screen. Ref# HAWT-17

--- hawtio-web/src/main/webapp/app/springbatch/js/executionHistory.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/executionHistory.ts
@@ -3,12 +3,18 @@ module SpringBatch {
     var proxyUrl = '/hawtio/proxy/';
     var executionHistoryPath = 'jobs/:jobName/executions.json';
     export function ExecutionHistoryController($scope, $routeParams, $location, workspace:Workspace, $resource) {
+
+        $scope.predicate='id';
+        $scope.reverse=false;
+
         var jobExecutionRes = $resource(proxyUrl+springBatchServerOrigin+executionHistoryPath);
         jobExecutionRes.get({'jobName':$routeParams.jobName},function(data){
+            var executionList = new Array();
             for(var execution in data.jobExecutions){
                 data.jobExecutions[execution].id=execution;
+                executionList.add(data.jobExecutions[execution]);
             }
-            $scope.executionHistory = data.jobExecutions;
+            $scope.executionHistory = executionList;
         })
     }
 }
