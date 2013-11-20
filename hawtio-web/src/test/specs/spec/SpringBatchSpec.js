9665db05ec3a351462c9c56c78673277037dc5f9
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-20 18:56:14 +0530

    added sample spring-batch test script. Ref# HAWT-18

---
+++ hawtio-web/src/test/specs/spec/SpringBatchSpec.js
@@ -0,0 +1,14 @@
+/**
+ * User: prashant
+ * Date: 20/11/13
+ * Time: 6:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+describe("SpringBatch", function () {
+    beforeEach(function () {
+    });
+
+    it("sample spring batch test", function () {
+        expect(1).toEqual(1);
+    });
+});
