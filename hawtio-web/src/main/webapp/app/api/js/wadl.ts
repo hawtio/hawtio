84c46b547ef316f09b5152fdf9045061757f73e9
James Strachan <james.strachan@gmail.com>
2013-11-21 21:03:53 +0000

    #736 reuse more code across both WSDL and WADL

--- hawtio-web/src/main/webapp/app/api/js/wadl.ts
+++ hawtio-web/src/main/webapp/app/api/js/wadl.ts
@@ -2,32 +2,7 @@ module API {
 
   export function WadlViewController($scope, $location, jolokia) {
 
-    var search = $location.search();
-    $scope.url = search["wadl"];
-    $scope.container = search["container"];
-    $scope.objectName = search["objectName"];
-
-    if ($scope.container && $scope.objectName) {
-      Fabric.containerJolokia(jolokia, $scope.container, (remoteJolokia) => {
-        $scope.remoteJolokia = remoteJolokia;
-        if (remoteJolokia) {
-          API.loadJsonSchema(remoteJolokia, $scope.objectName, (jsonSchema) => {
-            log.info("Got JSON Schema: " + JSON.stringify(jsonSchema, null, "  "));
-            $scope.jsonSchema = jsonSchema;
-          })
-        } else {
-          log.info("No Remote Jolokia!");
-        }
-      });
-    } else {
-      log.info("No container or objectName");
-    }
-
-    var log:Logging.Logger = Logger.get("API3");
-
-    log.info("container: " + $scope.container + " objectName: " + $scope.objectName + " url: " + $scope.url);
-
-    var wadlNamespace = "http://schemas.xmlsoap.org/wsdl/";
+    API.initScope($scope, $location, jolokia);
 
     loadXml($scope.url, onWsdl);
 
