a472e962f8bae9bf45f2728adc684d7e262079cc
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-22 20:05:44 +0530

    test script spring batch. Ref# HAWT-18

--- hawtio-web/src/test/specs/spec/SpringBatchSpec.js
+++ hawtio-web/src/test/specs/spec/SpringBatchSpec.js
@@ -4,22 +4,35 @@
  * Time: 6:04 PM
  * To change this template use File | Settings | File Templates.
  */
-describe("SpringBatch", function () {
+describe("NavBarController", function () {
+
+    beforeEach(module('SpringBatch'));
+
     var  $httpBackend;
+    var ctrl;
+    var scope;
+    var location;
+
+
 
     beforeEach(function () {
-        angular.mock.inject(function ($injector) {
-            $httpBackend = $injector.get('$httpBackend');
+        console.log(' ------ mock ------- '+angular.mock);
+        angular.mock.inject(function (_$httpBackend_, _$rootScope_, _$location_, $controller) {
+            $httpBackend = _$httpBackend_;
+            scope = _$rootScope_.$new();
+            location = _$location_;
+            Cntrl = $controller('SpringBatch.NavBarController',{
+                $scope:scope,
+                $location:location
+            });
+//            console.log(' ------ mock 2 ------- '+Cntrl);
         })
     });
 
     it("sample spring batch test", function () {
-//        $httpBackend.expectGET('/hawtio/proxy/localhost:8080/spring-batch-admin-sample/jobs.json')
-//            .respond('{"jobs":{"resource":"http://localhost:8080/spring-batch-admin-sample/jobs.json","registrations":{"job2":{"name":"job2","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job2.json","description":"No description","executionCount":0,"launchable":true,"incrementable":false},"job1":{"name":"job1","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job1.json","description":"No description","executionCount":0,"launchable":true,"incrementable":true}}},"$resolved":true}');
-
-//        browser().navigateTo('#/springbatch/jobs');
-        console.log(' ------------- '+browser());
-//        console.log(' ------------- '+$httpBackend);
+//        var jsonResponse = '{"jobs":{"resource":"http://localhost:8080/spring-batch-admin-sample/jobs.json","registrations":{"job2":{"name":"job2","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job2.json","description":"No description","executionCount":0,"launchable":true,"incrementable":false},"job1":{"name":"job1","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job1.json","description":"No description","executionCount":0,"launchable":true,"incrementable":true}}},"$resolved":true}';
+//        $httpBackend.expectGET('/hawtio/proxy/localhost:8080/spring-batch-admin-sample/jobs.json').respond(jsonResponse);
+        console.log(' ------ log 2 ------- ' + scope.subLevelTabs);
         expect(1).toEqual(1);
     });
 });
