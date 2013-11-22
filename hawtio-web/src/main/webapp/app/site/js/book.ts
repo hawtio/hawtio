1184202084a2ee6bf2342590d3d16075532e6fdd
James Strachan <james.strachan@gmail.com>
2013-11-22 12:05:10 +0000

    first spike of #771 so we can view a basic view of the hawtio website documentation at http://localhost:8080/hawtio/#/site which then fixes up any links

---
+++ hawtio-web/src/main/webapp/app/site/js/book.ts
@@ -0,0 +1,39 @@
+module Site {
+
+  export function BookController($scope, $routeParams, $location, $compile, $http, fileExtensionTypeRegistry) {
+
+    var log:Logging.Logger = Logger.get("Site");
+    var pageId = $routeParams["page"];
+    if (!pageId) {
+      $location.path("/site/doc/index.md");
+      return;
+    }
+
+    if (!pageId.startsWith("/") && pageId.indexOf(":/") < 0 && pageId.indexOf("app/site/") < 0) {
+      // lets assume the page is relative to app/site/
+      pageId = "app/site/" + pageId;
+    }
+    $scope.pageId = pageId;
+
+    log.info("Loading page '" + $scope.pageId + "'");
+
+    $http.get($scope.pageId).success(onResults);
+
+    function onResults(contents, status, headers, config) {
+      $scope.contents = contents;
+      $scope.html = contents;
+
+      var format = Wiki.fileFormat($scope.pageId, fileExtensionTypeRegistry) || "markdown";
+      if ("markdown" === format) {
+        // lets convert it to HTML
+        $scope.html = contents ? marked(contents) : "";
+      } else if (format && format.startsWith("html")) {
+        $scope.html = contents;
+      } else {
+        // TODO?
+      }
+      $compile($scope.html)($scope);
+      Core.$apply($scope);
+    }
+  }
+}
