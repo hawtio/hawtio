da03439cf4d3a61627c24a4cc8674ec00126e6da
Prashant Gupta <prashant.gupta@middleware360.com>
2013-11-25 19:46:07 +0530

    implemented test script for  navbar controller. Ref# HAWT-18

--- hawtio-web/src/test/e2e/scenarios.js
+++ hawtio-web/src/test/e2e/scenarios.js
@@ -174,25 +174,4 @@ describe("hawtio", function() {
         expect(element("table#grid tbody tr", "Number of messages on queue " + queueName).count()).toBeGreaterThan(0);
       });
     });
-
-    describe("SpringBatch", function () {
-        var  $httpBackend;
-
-        beforeEach(function () {
-            angular.mock.inject(function ($injector) {
-                $httpBackend = $injector.get('$httpBackend');
-            })
-        });
-
-        it("sample spring batch test", function () {
-//        $httpBackend.expectGET('/hawtio/proxy/localhost:8080/spring-batch-admin-sample/jobs.json')
-//            .respond('{"jobs":{"resource":"http://localhost:8080/spring-batch-admin-sample/jobs.json","registrations":{"job2":{"name":"job2","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job2.json","description":"No description","executionCount":0,"launchable":true,"incrementable":false},"job1":{"name":"job1","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job1.json","description":"No description","executionCount":0,"launchable":true,"incrementable":true}}},"$resolved":true}');
-
-            browser().navigateTo('#/springbatch/jobs');
-            console.log(' ------------- '+browser().location().url());
-
-//        console.log(' ------------- '+$httpBackend);
-            expect(1).toEqual(1);
-        });
-    });
 });
--- hawtio-web/src/test/specs/spec/SpringBatchSpec.js
+++ hawtio-web/src/test/specs/spec/SpringBatchSpec.js
@@ -1,38 +1,27 @@
-/**
- * User: prashant
- * Date: 20/11/13
- * Time: 6:04 PM
- * To change this template use File | Settings | File Templates.
- */
-describe("NavBarController", function () {
+describe("SpringBatch", function () {
 
-    beforeEach(module('SpringBatch'));
-
-    var  $httpBackend;
-    var ctrl;
-    var scope;
-    var location;
-
-
+    var $rootScope;
+    var $location;
+    var $routeParams;
 
     beforeEach(function () {
-        console.log(' ------ mock ------- '+angular.mock);
-        angular.mock.inject(function (_$httpBackend_, _$rootScope_, _$location_, $controller) {
-            $httpBackend = _$httpBackend_;
-            scope = _$rootScope_.$new();
-            location = _$location_;
-            Cntrl = $controller('SpringBatch.NavBarController',{
-                $scope:scope,
-                $location:location
-            });
-//            console.log(' ------ mock 2 ------- '+Cntrl);
+
+        angular.mock.inject(function ($injector) {
+            $rootScope = $injector.get('$rootScope');
+            $location = $injector.get('$location');
+            $routeParams = $injector.get('$routeParams');
         })
+
     });
 
-    it("sample spring batch test", function () {
-//        var jsonResponse = '{"jobs":{"resource":"http://localhost:8080/spring-batch-admin-sample/jobs.json","registrations":{"job2":{"name":"job2","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job2.json","description":"No description","executionCount":0,"launchable":true,"incrementable":false},"job1":{"name":"job1","resource":"http://localhost:8080/spring-batch-admin-sample/jobs/job1.json","description":"No description","executionCount":0,"launchable":true,"incrementable":true}}},"$resolved":true}';
-//        $httpBackend.expectGET('/hawtio/proxy/localhost:8080/spring-batch-admin-sample/jobs.json').respond(jsonResponse);
-        console.log(' ------ log 2 ------- ' + scope.subLevelTabs);
-        expect(1).toEqual(1);
+    it("spring batch sub level tabs", function () {
+
+        var scope = $rootScope.$new();
+        SpringBatch.NavBarController(scope, $routeParams, $location);
+
+        expect(scope.subLevelTabs[0].uri).toEqual('jobs');
+        expect(scope.subLevelTabs[1].uri).toEqual('jobs/executions');
+        expect(scope.isActive({uri:'jobs',name:'No tab'})).toEqual(false);
     });
 });
+
