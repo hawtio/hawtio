458338996cd0f51ea8d1718c98c7a958d093da86
James Strachan <james.strachan@gmail.com>
2013-11-27 14:56:10 +0000

    #778 added a better commit detail page so you can view the commit message; who made the change, when and what files were changed; with links to view the files or compare the changes

--- hawtio-web/src/main/webapp/app/wiki/js/commit.ts
+++ hawtio-web/src/main/webapp/app/wiki/js/commit.ts
@@ -3,8 +3,7 @@
  */
 module Wiki {
 
-  export function CommitController($scope, $location, $routeParams, $templateCache,
-                                   workspace:Workspace, marked, fileExtensionTypeRegistry, wikiRepository:GitWikiRepository) {
+  export function CommitController($scope, $location, $routeParams, $templateCache, workspace:Workspace, marked, fileExtensionTypeRegistry, wikiRepository:GitWikiRepository) {
 
     Wiki.initScope($scope, $routeParams, $location);
     $scope.commitId = $scope.pageId;
@@ -16,6 +15,10 @@ module Wiki {
     $scope.gridOptions = {
       data: 'commits',
       showFilter: false,
+      multiSelect: false,
+      selectWithCheckboxOnly: true,
+      showSelectionCheckbox: true,
+      displaySelectionCheckbox : true, // old pre 2.0 config!
       selectedItems: $scope.selectedItems,
       filterOptions: {
         filterText: ''
@@ -23,10 +26,11 @@ module Wiki {
       columnDefs: [
         {
           field: 'path',
-          displayName: 'Name',
+          displayName: 'File Name',
           cellTemplate: $templateCache.get('fileCellTemplate.html'),
+          width: "***",
           cellFilter: ""
-        }
+        },
       ]
     };
 
@@ -43,15 +47,84 @@ module Wiki {
       }
     });
 
+
+    $scope.canRevert = () => {
+      return $scope.selectedItems.length === 1;
+    };
+
+    $scope.revert = () => {
+      if ($scope.selectedItems.length > 0) {
+        var path = commitPath($scope.selectedItems[0]);
+        var objectId = $scope.commitId;
+        if (path && objectId) {
+          var commitMessage = "Reverting file " + $scope.pageId + " to previous version " + objectId;
+          wikiRepository.revertTo(objectId, $scope.pageId, commitMessage, (result) => {
+            Wiki.onComplete(result);
+            // now lets update the view
+            updateView();
+          });
+        }
+      }
+    };
+
+    function commitPath(commit) {
+      return commit.path || commit.name;
+    }
+
+    $scope.diff = () => {
+      if ($scope.selectedItems.length > 0) {
+        var commit = $scope.selectedItems[0];
+        /*
+         var commit = row;
+         var entity = row.entity;
+         if (entity) {
+         commit = entity;
+         }
+         */
+        var link = startLink($scope.branch) + "/diff/" + commitPath(commit) + "/" + $scope.commitId + "/";
+        var path = Core.trimLeading(link, "#");
+        $location.path(path);
+      }
+    };
+
     updateView();
 
     function updateView() {
-     wikiRepository.commitTree($scope.commitId, (commits) => {
-        $scope.commits = commits;
+      var commitId = $scope.commitId;
 
+      wikiRepository.commitInfo(commitId, (commitInfo) => {
+        $scope.commitInfo = commitInfo;
+        Core.$apply($scope);
+      });
+
+      wikiRepository.commitTree(commitId, (commits) => {
+        $scope.commits = commits;
         angular.forEach(commits, (commit) => {
           commit.fileIconHtml = Wiki.fileIconHtml(commit);
           commit.fileClass = commit.name.endsWith(".profile") ? "green" : "";
+          var changeType = commit.changeType;
+          var path = commitPath(commit);
+          if (path) {
+            commit.fileLink = startLink($scope.branch) + '/version/' + path + '/' + commitId;
+          }
+          if (changeType) {
+            changeType = changeType.toLowerCase();
+            if (changeType.startsWith("a")) {
+              commit.changeClass = "change-add";
+              commit.change = "add";
+              commit.title = "added";
+            } else if (changeType.startsWith("d")) {
+              commit.changeClass = "change-delete";
+              commit.change = "delete";
+              commit.title = "deleted";
+              commit.fileLink = null;
+            } else {
+              commit.changeClass = "change-modify";
+              commit.change = "modify";
+              commit.title = "modified";
+            }
+            commit.changeTypeHtml = '<span class="' + commit.changeClass + '">' + commit.title + '</span>';
+          }
         });
         Core.$apply($scope);
       });
