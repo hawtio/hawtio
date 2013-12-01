0deb30c8652475e73f34615159e055297979bfd8
Claus Ibsen <claus.ibsen@gmail.com>
2013-12-01 12:12:18 +0100

    #782: First spike of mvn hawtio:spring goal.

---
+++ hawtio-maven-plugin/src/main/java/io/hawt/maven/SpringMojo.java
@@ -0,0 +1,79 @@
+package io.hawt.maven;
+
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "spring", defaultPhase = LifecyclePhase.TEST_COMPILE, requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class SpringMojo extends RunMojo {
+
+    @Parameter(property = "spring.applicationContextUri")
+    private String applicationContextUri;
+
+    @Parameter(property = "spring.fileApplicationContextUri")
+    private String fileApplicationContextUri;
+
+    protected Artifact springCoreArtifact;
+
+    @Override
+    protected void addCustomArguments(List<String> args) throws Exception {
+        if (applicationContextUri != null) {
+            args.add("-ac");
+            args.add(applicationContextUri);
+        } else if (fileApplicationContextUri != null) {
+            args.add("-fa");
+            args.add(fileApplicationContextUri);
+        }
+
+        if (mainClass != null) {
+            getLog().info("Using custom " + mainClass + " to initiate Spring");
+        } else {
+            // use spring by default
+            getLog().info("Using io.hawt.maven.main.SpringMain to initiate Spring");
+            mainClass = "io.hawt.maven.main.SpringMain";
+        }
+    }
+
+    protected Artifact getSpringCoreArtifact(Set<Artifact> artifacts) throws MojoExecutionException {
+        for (Artifact artifact : artifacts) {
+            if (artifact.getGroupId().equals("org.springframework") && artifact.getArtifactId().equals("spring-core")) {
+                return artifact;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void resolvedArtifacts(Set<Artifact> artifacts) throws Exception {
+        // make sure we have spring-core
+        springCoreArtifact = getSpringCoreArtifact(artifacts);
+        if (springCoreArtifact == null) {
+            throw new IllegalAccessError("Cannot resolve camel-core dependency from the Maven pom.xml file");
+        }
+
+        super.resolvedArtifacts(artifacts);
+    }
+
+    @Override
+    protected boolean filterUnwantedArtifacts(Artifact artifact) {
+        // filter out unwanted OSGi related JARs as some projects like ActiveMQ includes these dependencies
+        // and you should use the blueprint goal for running as OSGi
+        if (artifact.getGroupId().equals("org.apache.aries.blueprint")) {
+            return true;
+        } else if (artifact.getGroupId().startsWith("org.ops4j")) {
+            return true;
+        } else if (artifact.getGroupId().equals("org.osgi")) {
+            return true;
+        } else if (artifact.getGroupId().equals("org.apache.felix")) {
+            return true;
+        }
+
+        return super.filterUnwantedArtifacts(artifact);
+    }
+}
