22d05b459a4c059e1d761f1b795b8fb4f942ca01
Claus Ibsen <claus.ibsen@gmail.com>
2013-12-05 18:12:45 +0100

    #782: Polished

--- hawtio-maven-plugin/src/main/java/io/hawt/maven/junit/DefaultJUnitService.java
+++ hawtio-maven-plugin/src/main/java/io/hawt/maven/junit/DefaultJUnitService.java
@@ -1,19 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.hawt.maven.junit;
 
 import io.hawt.util.ReflectionHelper;
@@ -86,7 +70,7 @@ public class DefaultJUnitService implements JUnitService {
 
         // if no annotations then assume setUp
         List<Method> names = findMethodsWithName(clazz, "setUp");
-        if (names.isEmpty()) {
+        if (!names.isEmpty()) {
             return names.get(0);
         }
 
@@ -117,7 +101,7 @@ public class DefaultJUnitService implements JUnitService {
 
         // if no annotations then assume setUp
         List<Method> names = findMethodsWithName(clazz, "tearDown");
-        if (names.isEmpty()) {
+        if (!names.isEmpty()) {
             return names.get(0);
         }
 
