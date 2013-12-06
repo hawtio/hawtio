eefdd7ea65046a9aa17d7998208160ac1850e455
James Strachan <james.strachan@gmail.com>
2013-12-06 13:59:14 +0000

    #817 add an IDE plugin for finding files in the file system and opening them in IDEA

---
+++ hawtio-ide/src/main/java/io/hawt/ide/IdeFacadeMBean.java
@@ -0,0 +1,25 @@
+package io.hawt.ide;
+
+import java.util.List;
+
+/**
+ * The JMX MBean interface for working with IDEs
+ */
+public interface IdeFacadeMBean {
+
+    /**
+     * Given a class name and a file name, try to find the absolute file name of the
+     * source file on the users machine or null if it cannot be found
+     */
+    String findClassAbsoluteFileName(String className, String fileName, List<String> sourceRoots);
+
+    /**
+     * Open an absolute file name in IDEA and navigate to the position in the file
+     */
+    String ideaOpenAndNavigate(String fileName, int line, int column) throws Exception;
+
+    /**
+     * Open an absolute file name in IDEA
+     */
+    String ideaOpen(String fileName) throws Exception;
+}
