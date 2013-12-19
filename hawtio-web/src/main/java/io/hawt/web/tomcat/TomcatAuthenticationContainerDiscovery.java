dced81ddfbd3baea79fea58898ab768921d6156e
Claus Ibsen <claus.ibsen@gmail.com>
2013-12-19 16:37:54 +0100

    #857: Tomcat security support for its conf/tomcat-users.xml. Work in progress.

--- hawtio-web/src/main/java/io/hawt/web/tomcat/TomcatAuthenticationContainerDiscovery.java
+++ hawtio-web/src/main/java/io/hawt/web/tomcat/TomcatAuthenticationContainerDiscovery.java
@@ -1,12 +1,21 @@
 package io.hawt.web.tomcat;
 
+import java.lang.management.ManagementFactory;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
 import io.hawt.web.AuthenticationConfiguration;
 import io.hawt.web.AuthenticationContainerDiscovery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * To use Apache Tomcat using its conf/tomcat-users.xml for authentication.
  */
 public class TomcatAuthenticationContainerDiscovery implements AuthenticationContainerDiscovery {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(TomcatAuthenticationContainerDiscovery.class);
 
     @Override
     public String getContainerName() {
@@ -15,15 +24,26 @@ public class TomcatAuthenticationContainerDiscovery implements AuthenticationCon
 
     @Override
     public boolean canAuthenticate(AuthenticationConfiguration configuration) {
-        // TODO: are we running in Tomcat
-        // lookup in JMX or env check?
+        try {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            boolean isTomcat = server.isRegistered(new ObjectName("Catalina:type=Server"));
+            if (!isTomcat) {
+                isTomcat = server.isRegistered(new ObjectName("Tomcat:type=Server"));
+            }
+            LOG.debug("Checked for {} in JMX for {} -> {}", getContainerName(), isTomcat);
 
-        boolean isTomcat = true;
-        if (isTomcat) {
-            configuration.setConfiguration(new TomcatLoginContextConfiguration());
-            configuration.setRolePrincipalClasses(TomcatPrincipal.class.getName());
+            if (isTomcat) {
+                configuration.setConfiguration(new TomcatLoginContextConfiguration());
+                configuration.setRolePrincipalClasses(TomcatPrincipal.class.getName());
+            }
+            return isTomcat;
+
+        } catch (MalformedObjectNameException e) {
+            // ignore
+            LOG.warn("Error checking in JMX for " + getContainerName() + ". This exception is ignored.", e);
         }
 
-        return isTomcat;
+        return false;
     }
+
 }
