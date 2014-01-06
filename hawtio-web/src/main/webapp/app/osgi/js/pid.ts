2dc1e05671ad6afeddd46922432dca681cbe70d7
James Strachan <james.strachan@gmail.com>
2014-01-06 11:51:22 +0000

    pretty print the code a little

--- hawtio-web/src/main/webapp/app/osgi/js/pid.ts
+++ hawtio-web/src/main/webapp/app/osgi/js/pid.ts
@@ -2,118 +2,115 @@
  * @module Osgi
  */
 module Osgi {
-    export function PidController($scope, $filter:ng.IFilterService, workspace:Workspace, $routeParams) {
-        $scope.deletePropDialog = new Core.Dialog();
-        $scope.deletePidDialog = new Core.Dialog();
-        $scope.addPropertyDialog = new Core.Dialog();
-        $scope.pid = $routeParams.pid;
+  export function PidController($scope, $filter:ng.IFilterService, workspace:Workspace, $routeParams, jolokia) {
+    $scope.deletePropDialog = new Core.Dialog();
+    $scope.deletePidDialog = new Core.Dialog();
+    $scope.addPropertyDialog = new Core.Dialog();
+    $scope.pid = $routeParams.pid;
 
-        updateTableContents();
+    updateTableContents();
 
-        $scope.pidSave = () => {
-            var table = document.getElementById("configValues");
+    $scope.pidSave = () => {
+      var table = document.getElementById("configValues");
 
-            var els : any = table.getElementsByClassName("pid-value");
-            var props = "";
-            var td = {};
-            for (var i = 0; i < els.length; i++) {
-                props += "\n " + els[i].previousElementSibling.textContent + " " + els[i].textContent;
-                td[els[i].previousElementSibling.textContent] = els[i].textContent;
-            }
+      var els:any = table.getElementsByClassName("pid-value");
+      var props = "";
+      var td = {};
+      for (var i = 0; i < els.length; i++) {
+        props += "\n " + els[i].previousElementSibling.textContent + " " + els[i].textContent;
+        td[els[i].previousElementSibling.textContent] = els[i].textContent;
+      }
 
-            var mbean = getHawtioConfigAdminMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request({
-                        type: "exec",
-                        mbean: mbean,
-                        operation: "configAdminUpdate",
-                        arguments: [$scope.pid, JSON.stringify(td)]
-                    }, {
-                        error: function(response) {
-                            notification("error", response.error);
-                        },
-                        success: function(response) {
-                            enableSave(false);
-                            notification("success", "Successfully updated pid: " + $scope.pid);
-                        }
-                    });
-            }
-        }
-
-        $scope.addPropertyConfirmed = (key, value) => {
-            $scope.addPropertyDialog.close();
-            $scope.row[key] = {
-                Key: key,
-                Value: value,
-                Type: "String"
-            };
-            enableSave(true);
-        }
-
-        $scope.deletePidProp = (e) => {
-            $scope.deleteKey = e.Key;
-            $scope.deletePropDialog.open();
-        }
-
-        $scope.deletePidPropConfirmed = () => {
-            $scope.deletePropDialog.close();
-            var cell : any = document.getElementById("pid." + $scope.deleteKey);
-            cell.parentElement.remove();
-            enableSave(true);
-        }
-
-        $scope.deletePidConfirmed = () => {
-            $scope.deletePidDialog.close();
-
-            var mbean = getSelectionConfigAdminMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request({
-                        type: "exec",
-                        mbean: mbean,
-                        operation: 'delete',
-                        arguments: [$scope.pid]
-                    }, {
-                        error: function(response) {
-                            notification("error", response.error);
-                        },
-                        success: function(response) {
-                            notification("success", "Successfully deleted pid: " + $scope.pid);
-                            // Move back to the overview page
-                            window.location.href = "#/osgi/configurations";
-                        }
-                    });
-            }
-        }
-
-        function jmxError(response) {
-            notification("error", "Oops: " + response);
-        }
-
-        function populateTable(response) {
-            $scope.row = response.value
-            Core.$apply($scope);
-        };
-
-        function updateTableContents() {
-            var mbean = getSelectionConfigAdminMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request(
-                    {type: 'exec', mbean: mbean, operation: 'getProperties', arguments: [$scope.pid]},
-                    onSuccess(populateTable));
-            }
-        }
+      var mbean = getHawtioConfigAdminMBean(workspace);
+      if (mbean) {
+        jolokia.request({
+          type: "exec",
+          mbean: mbean,
+          operation: "configAdminUpdate",
+          arguments: [$scope.pid, JSON.stringify(td)]
+        }, {
+          error: function (response) {
+            notification("error", response.error);
+          },
+          success: function (response) {
+            enableSave(false);
+            notification("success", "Successfully updated pid: " + $scope.pid);
+          }
+        });
+      }
     };
 
-    export function editPidValueCell(e) {
-        e.contentEditable = true;
-        enableSave(true);
+    $scope.addPropertyConfirmed = (key, value) => {
+      $scope.addPropertyDialog.close();
+      $scope.row[key] = {
+        Key: key,
+        Value: value,
+        Type: "String"
+      };
+      enableSave(true);
+    };
+
+    $scope.deletePidProp = (e) => {
+      $scope.deleteKey = e.Key;
+      $scope.deletePropDialog.open();
+    };
+
+    $scope.deletePidPropConfirmed = () => {
+      $scope.deletePropDialog.close();
+      var cell:any = document.getElementById("pid." + $scope.deleteKey);
+      cell.parentElement.remove();
+      enableSave(true);
+    };
+
+    $scope.deletePidConfirmed = () => {
+      $scope.deletePidDialog.close();
+
+      var mbean = getSelectionConfigAdminMBean(workspace);
+      if (mbean) {
+        jolokia.request({
+          type: "exec",
+          mbean: mbean,
+          operation: 'delete',
+          arguments: [$scope.pid]
+        }, {
+          error: function (response) {
+            notification("error", response.error);
+          },
+          success: function (response) {
+            notification("success", "Successfully deleted pid: " + $scope.pid);
+            // Move back to the overview page
+            window.location.href = "#/osgi/configurations";
+          }
+        });
+      }
+    };
+
+    function jmxError(response) {
+      notification("error", "Oops: " + response);
     }
 
-    function enableSave(enablement : boolean) {
-        var saveBtn = document.getElementById("saveButton");
-        saveBtn.disabled = !enablement;
+    function populateTable(response) {
+      $scope.row = response.value;
+      Core.$apply($scope);
     }
+
+    function updateTableContents() {
+      var mbean = getSelectionConfigAdminMBean(workspace);
+      if (mbean) {
+        jolokia.request(
+          {type: 'exec', mbean: mbean, operation: 'getProperties', arguments: [$scope.pid]},
+          onSuccess(populateTable));
+      }
+    }
+  }
+
+  export function editPidValueCell(e) {
+    e.contentEditable = true;
+    enableSave(true);
+  }
+
+  function enableSave(enablement:boolean) {
+    var saveBtn = document.getElementById("saveButton");
+    saveBtn.disabled = !enablement;
+  }
 }
