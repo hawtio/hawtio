65e3d115e345602a1db4de92b32ca96637da152d
James Strachan <james.strachan@gmail.com>
2014-01-06 11:39:31 +0000

    pretty print the code a little

--- hawtio-web/src/main/webapp/app/osgi/js/configurations.ts
+++ hawtio-web/src/main/webapp/app/osgi/js/configurations.ts
@@ -3,65 +3,63 @@
  */
 module Osgi {
 
-    export function ConfigurationsController($scope, $filter:ng.IFilterService, workspace:Workspace, $templateCache:ng.ITemplateCacheService, $compile:ng.IAttributes) {
-        var dateFilter = $filter('date');
+  export function ConfigurationsController($scope, $filter:ng.IFilterService, workspace:Workspace, $templateCache:ng.ITemplateCacheService, $compile:ng.IAttributes, jolokia) {
+    var dateFilter = $filter('date');
 
-        $scope.addPidDialog = new Core.Dialog();
+    $scope.addPidDialog = new Core.Dialog();
 
-        $scope.widget = new TableWidget($scope, workspace, [
-            { "mDataProp": "PidLink" }
+    $scope.widget = new TableWidget($scope, workspace, [
+      { "mDataProp": "PidLink" }
 
-        ], {
-            rowDetailTemplateId: 'configAdminPidTemplate',
-            disableAddColumns: true
-        });
+    ], {
+      rowDetailTemplateId: 'configAdminPidTemplate',
+      disableAddColumns: true
+    });
 
-        $scope.addPid = (newPid) => {
-            $scope.addPidDialog.close();
+    $scope.addPid = (newPid) => {
+      $scope.addPidDialog.close();
 
-            var mbean = getHawtioConfigAdminMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request({
-                        type: "exec",
-                        mbean: mbean,
-                        operation: "configAdminUpdate",
-                        arguments: [newPid, JSON.stringify({})]
-                    }, {
-                        error: function(response) {
-                            notification("error", response.error);
-                        },
-                        success: function(response) {
-                            notification("success", "Successfully created pid: " + newPid);
-                            updateTableContents();
-                        }
-                    });
-            }
-        }
-
-        $scope.$on("$routeChangeSuccess", function (event, current, previous) {
-            // lets do this asynchronously to avoid Error: $digest already in progress
-            setTimeout(updateTableContents, 50);
-        });
-
-        $scope.$watch('workspace.selection', function () {
+      var mbean = getHawtioConfigAdminMBean(workspace);
+      if (mbean) {
+        jolokia.request({
+          type: "exec",
+          mbean: mbean,
+          operation: "configAdminUpdate",
+          arguments: [newPid, JSON.stringify({})]
+        }, {
+          error: function (response) {
+            notification("error", response.error);
+          },
+          success: function (response) {
+            notification("success", "Successfully created pid: " + newPid);
             updateTableContents();
+          }
         });
+      }
+    };
 
-        function populateTable(response) {
-            var configurations = Osgi.defaultConfigurationValues(workspace, $scope, response.value);
-            $scope.widget.populateTable(configurations);
-            Core.$apply($scope);
-        }
+    $scope.$on("$routeChangeSuccess", function (event, current, previous) {
+      // lets do this asynchronously to avoid Error: $digest already in progress
+      setTimeout(updateTableContents, 50);
+    });
 
-        function updateTableContents() {
-            var mbean = getSelectionConfigAdminMBean(workspace);
-            if (mbean) {
-                var jolokia = workspace.jolokia;
-                jolokia.request(
-                    {type: 'exec', mbean: mbean, operation: 'getConfigurations', arguments: ['(service.pid=*)']},
-                    onSuccess(populateTable));
-            }
-        }
+    $scope.$watch('workspace.selection', function () {
+      updateTableContents();
+    });
+
+    function populateTable(response) {
+      var configurations = Osgi.defaultConfigurationValues(workspace, $scope, response.value);
+      $scope.widget.populateTable(configurations);
+      Core.$apply($scope);
     }
+
+    function updateTableContents() {
+      var mbean = getSelectionConfigAdminMBean(workspace);
+      if (mbean) {
+        jolokia.request(
+          {type: 'exec', mbean: mbean, operation: 'getConfigurations', arguments: ['(service.pid=*)']},
+          onSuccess(populateTable));
+      }
+    }
+  }
 }
