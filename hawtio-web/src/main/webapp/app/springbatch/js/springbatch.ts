b0926f2e9a4a563f783af3e9b0a4777f629dafa4
Prashant Gupta <prashant.gupta@middleware360.com>
2014-01-07 18:32:47 +0530

    implemented controller logic for launching and refreshing jobs. Ref# HAWT-28

--- hawtio-web/src/main/webapp/app/springbatch/js/springbatch.ts
+++ hawtio-web/src/main/webapp/app/springbatch/js/springbatch.ts
@@ -1,31 +1,63 @@
 module SpringBatch {
 
-    export function JobListController($scope, $location, workspace:Workspace, jolokia, $resource, $rootScope) {
+    export function JobListController($scope, $location, workspace:Workspace, jolokia, $resource, $rootScope, $http) {
         var springBatchServerPath = $rootScope.springBatchServer+'jobs.json';
         var proxyUrl = $rootScope.proxyUrl ;
 
         $scope.predicate = 'name';
         $scope.reverse = false;
-        var jobList = $resource(proxyUrl+springBatchServerPath);
-        jobList.get(function(data){
-            if(data.jobs && data.jobs.registrations){
-                var jobList = new Array();
-                for(var job in data.jobs.registrations){
-                    data.jobs.registrations[job].showLaunchForm=false;
-                    data.jobs.registrations[job].launchParams='';
-                    jobList.add(data.jobs.registrations[job]);
-                    console.info(' ---------------- '+JSON.stringify(data.jobs.registrations[job]));
+
+
+        $scope.getJobList = function(){
+            var jobList = $resource(proxyUrl+springBatchServerPath);
+            jobList.get(function(data){
+                if(data.jobs && data.jobs.registrations){
+                    var jobList = new Array();
+                    for(var job in data.jobs.registrations){
+                        data.jobs.registrations[job].showLaunchForm=false;
+                        data.jobs.registrations[job].launchParams='';
+                        jobList.add(data.jobs.registrations[job]);
+                    }
+                    $scope.jobList = jobList;
                 }
-                $scope.jobList = jobList;
-            }
-        });
+            });
+        };
+
+        $scope.getJobList();
 
         $scope.launchJob = function(jobName){
 
             var job ;
             for(var idx in $scope.jobList){ if( $scope.jobList[idx].name == jobName) job=$scope.jobList[idx]; }
+            var params = job.launchParams;
 
-            console.info(' --------------- '+ job.launchParams );
+            if(jobName && params){
+                var springServerOrigin=$rootScope.springBatchServer.replace('\\','');
+                var postUrl = proxyUrl+springServerOrigin+'jobs/'+jobName+'.json';
+                $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+                params = encodeURIComponent(params);
+                $http.post(postUrl,'jobParameters='+params)
+                    .success(function(data){
+                        if(data.jobExecution){
+                            $rootScope.alert.content='Job started successfully.';
+                            $rootScope.alert.type = 'alert-success';
+                            $rootScope.alert.show();
+                            $scope.getJobList();
+                        }else if(data.errors){
+                            $rootScope.alert.content='';
+                            for(var message in data.errors){
+                                $rootScope.alert.content+=data.errors[message]+'\n';
+                                $rootScope.alert.type = 'alert-error';
+                                $rootScope.alert.show();
+                            }
+                        }
+                    })
+                    .error(function(data){
+                        $rootScope.alert.content='Count not start the job';
+                        $rootScope.alert.type = 'alert-error';
+                        $rootScope.alert.show();
+                    });
+            }
         }
     }
 
