845adfb8647e0c88ab5b095a77dfd543f21362ad
James Strachan <james.strachan@gmail.com>
2014-01-10 09:21:07 +0000

    used more consistent plugin name and paths (and comment out unimplemented URIs)

--- hawtio-web/src/main/webapp/app/elasticsearch/js/esPlugin.ts
+++ hawtio-web/src/main/webapp/app/elasticsearch/js/esPlugin.ts
@@ -4,42 +4,43 @@
  */
 module ES {
 
-    var pluginEsName = 'elasticjs';
+    var pluginName = 'elasticsearch';
     var base_url = 'app/elasticsearch/html';
 
     /* Application level module which depends on filters, controllers, and services */
-    angular.module(pluginEsName, ['bootstrap', 'ngResource', 'elasticjs.service', 'dangle'])
+    angular.module(pluginName, ['bootstrap', 'ngResource', 'elasticjs.service', 'dangle'])
 
             .config(['$routeProvider', function ($routeProvider) {
                 $routeProvider
-                        .when('/search', {templateUrl: base_url + '/search.html'})
-                        .when('/results', {templateUrl: base_url + '/results.html'})
-                        .when('/elasticjs', {templateUrl: base_url + '/es.html'})
+/*
+                        .when('/elasticsearch/search', {templateUrl: base_url + '/search.html'})
+                        .when('/elasticsearch/results', {templateUrl: base_url + '/results.html'})
+*/
+                        .when('/elasticsearch', {templateUrl: base_url + '/es.html'})
             }])
 
             .run(($location:ng.ILocationService, workspace:Workspace, viewRegistry, helpRegistry) => {
 
                 // Use Full Layout of Hawtio
-                viewRegistry['elasticjs'] = 'app/elasticsearch/html/es.html';
+                viewRegistry[pluginName] = 'app/elasticsearch/html/es.html';
 
-                helpRegistry.addUserDoc('elasticjs', 'app/elasticsearch/doc/help.md', () => {
+                helpRegistry.addUserDoc(pluginName, 'app/elasticsearch/doc/help.md', () => {
                   // TODO not sure how this plugin actually shows up in the toolbar
                   return false;
                 });
 
-
                 /*
                  // Set up top-level link to our plugin
                  workspace.topLevelTabs.push({
-                 content: "ElasticJs",
-                 title: "ElasticJs plugin loaded dynamically",
+                 content: "ElasticSearch",
+                 title: "ElasticSearch",
                  isValid: (workspace) => true,
-                 href: () => '#/elasticjs',
-                 isActive: (workspace:Workspace) => workspace.isLinkActive("elasticjs")
+                 href: () => '#/elasticsearch',
+                 isActive: (workspace:Workspace) => workspace.isLinkActive("elasticsearch")
                  });
                  */
 
             });
 
-    hawtioPluginLoader.addModule(pluginEsName);
+    hawtioPluginLoader.addModule(pluginName);
 }
