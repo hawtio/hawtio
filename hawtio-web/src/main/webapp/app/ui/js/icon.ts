9936505ebb01041b9adb0169827700c726e7afef
Stan Lewis <slewis@fusesource.com>
2014-05-16 14:33:53 -0400

    Fix #2185, switch container view to use the new hawtio-icon directive

---
+++ hawtio-web/src/main/webapp/app/ui/js/icon.ts
@@ -0,0 +1,51 @@
+/**
+ * @module UI
+ */
+module UI {
+
+  /**
+   * Test controller for the icon help page
+   * @param $scope
+   * @param $templateCache
+   * @constructor
+   */
+  export function IconTestController($scope, $templateCache) {
+    $scope.exampleHtml = $templateCache.get('example-html');
+    $scope.exampleConfigJson = $templateCache.get('example-config-json');
+
+    $scope.$watch('exampleConfigJson', (newValue, oldValue) => {
+      $scope.icons = angular.fromJson($scope.exampleConfigJson);
+      log.debug("Icons: ", $scope.icons);
+    });
+  }
+
+  /**
+   * The hawtio-icon directive
+   * @returns {{}}
+   */
+  export function hawtioIcon() {
+    log.debug("Creating icon directive");
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: UI.templatePath + 'icon.html',
+      scope: {
+        icon: '=config'
+      },
+      link: ($scope, $element, $attrs) => {
+        if (!$scope.icon) {
+          return;
+        }
+        if (!('type' in $scope.icon) && !Core.isBlank($scope.icon.src)) {
+          if ($scope.icon.src.startsWith("icon-")) {
+            $scope.icon.type = "icon";
+          } else {
+            $scope.icon.type = "img";
+          }
+        }
+        log.debug("Created icon: ", $scope.icon);
+      }
+    };
+  }
+
+}
