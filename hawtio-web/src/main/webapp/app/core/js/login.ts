895ac3fd9507826beb4e7ec3497d801647687863
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-05-30 08:21:57 +0200

    [ENTESB-1542] Do not disable Log In button on login screen

--- hawtio-web/src/main/webapp/app/core/js/login.ts
+++ hawtio-web/src/main/webapp/app/core/js/login.ts
@@ -60,49 +60,51 @@ module Core {
       if (jolokiaUrl) {
         var url = jolokiaUrl.replace("jolokia", "auth/login/");
 
-        $.ajax(url, {
-          type: "POST",
-          success: (response) => {
-            userDetails.username = $scope.entity.username;
-            userDetails.password = $scope.entity.password;
-            userDetails.rememberMe = $scope.rememberMe;
-            userDetails.loginDetails = response;
+        if ($scope.entity.username.trim() != '') {
+          $.ajax(url, {
+            type: "POST",
+            success: (response) => {
+              userDetails.username = $scope.entity.username;
+              userDetails.password = $scope.entity.password;
+              userDetails.rememberMe = $scope.rememberMe;
+              userDetails.loginDetails = response;
 
-            Core.username = $scope.entity.username;
-            Core.password = $scope.entity.password;
-            if ($scope.rememberMe) {
-              localStorage[jolokiaUrl] = angular.toJson(userDetails);
-            } else {
-              delete localStorage[jolokiaUrl];
+              Core.username = $scope.entity.username;
+              Core.password = $scope.entity.password;
+              if ($scope.rememberMe) {
+                localStorage[jolokiaUrl] = angular.toJson(userDetails);
+              } else {
+                delete localStorage[jolokiaUrl];
+              }
+
+              jolokia.start();
+              workspace.loadTree();
+              Core.executePostLoginTasks();
+              Core.$apply($scope);
+            },
+            error: (xhr, textStatus, error) => {
+              // TODO placeholder for more feedback
+              switch (xhr.status) {
+                case 401:
+                  notification('error', 'Failed to log in, ' + error);
+                  break;
+                case 403:
+                  notification('error', 'Failed to log in, ' + error);
+                  break;
+                default:
+                  notification('error', 'Failed to log in, ' + error);
+                  break;
+              }
+              Core.$apply($scope);
+            },
+            beforeSend: (xhr) => {
+              xhr.setRequestHeader('Authorization', Core.getBasicAuthHeader($scope.entity.username, $scope.entity.password));
             }
 
-            jolokia.start();
-            workspace.loadTree();
-            Core.executePostLoginTasks();
-            Core.$apply($scope);
-          },
-          error: (xhr, textStatus, error) => {
-            // TODO placeholder for more feedback
-            switch (xhr.status) {
-              case 401:
-                notification('error', 'Failed to log in, ' + error);
-                break;
-              case 403:
-                notification('error', 'Failed to log in, ' + error);
-                break;
-              default:
-                notification('error', 'Failed to log in, ' + error);
-                break;
-            }
-            Core.$apply($scope);
-          },
-          beforeSend: (xhr) => {
-            xhr.setRequestHeader('Authorization', Core.getBasicAuthHeader($scope.entity.username, $scope.entity.password));
-          }
-
-          //username: $scope.entity.username,
-          //password: $scope.entity.password
-        });
+            //username: $scope.entity.username,
+            //password: $scope.entity.password
+          });
+        }
       }
     }
   }
