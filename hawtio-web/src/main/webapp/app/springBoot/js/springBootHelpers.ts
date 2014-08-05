3658d4dd42f03759866328583f54832336a680c8
Henryk Konsek <hekonsek@gmail.com>
2014-08-05 22:20:10 +0200

    [Spring Boot plugin] Fixed deprecated URL decoding.

--- hawtio-web/src/main/webapp/app/springBoot/js/springBootHelpers.ts
+++ hawtio-web/src/main/webapp/app/springBoot/js/springBootHelpers.ts
@@ -1,13 +1,16 @@
 module SpringBoot {
 
+    /**
+     * Resolves Jolokia URL of the Spring Boot to which Hawt.io is connected.
+     **/
     export function springBootAppUrl() {
-        var urls = decodeURI(document.URL).match(/\/http.*/);
-        if (!urls) {
+        var urlFromParameter = document.URL.match(/.http.*/);
+        if (!urlFromParameter) {
             return '';
         }
-        var url = urls[0].substring(1);
-        var hashIndex = url.lastIndexOf('#');
-        return url.substring(0, hashIndex)
+        var decodedUrl = decodeURIComponent(urlFromParameter[0].substring(1));
+        var hashIndex = decodedUrl.lastIndexOf('#');
+        return decodedUrl.substring(0, hashIndex)
     }
 
     export function callIfSpringBootAppAvailable(http, url, callbackFunc) {
