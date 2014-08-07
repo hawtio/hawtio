cc076d6d5b1d762c3956ddead26852fb5ed34161
Henryk Konsek <hekonsek@gmail.com>
2014-08-07 19:58:52 +0200

    [Spring Boot plugin] Added 'beans' tab.

--- hawtio-web/src/main/webapp/app/springBoot/js/springBootPlugin.ts
+++ hawtio-web/src/main/webapp/app/springBoot/js/springBootPlugin.ts
@@ -4,18 +4,17 @@
  */
 module SpringBoot {
 
-
-
     var pluginName = 'springBoot';
     export var _module = angular.module(pluginName, ['bootstrap', 'ngResource', 'ui.bootstrap.dialog', 'hawtioCore']);
     _module.config(["$routeProvider", ($routeProvider) => {
         $routeProvider.
-            when('/springBoot', {templateUrl: 'app/springBoot/html/springBoot.html'});
+            when('/springBoot/metrics', {templateUrl: 'app/springBoot/html/metrics.html'}).
+            when('/springBoot/beans', {templateUrl: 'app/springBoot/html/beans.html'});
     }]);
 
     _module.run(["$location", "$http", "workspace", "viewRegistry", "helpRegistry", "jolokia", ($location:ng.ILocationService, $http, workspace:Workspace, viewRegistry, helpRegistry, jolokia) => {
 
-        viewRegistry['springBoot'] = "app/springBoot/html/springBoot.html";
+        viewRegistry['springBoot'] = "app/springBoot/html/layoutSpringBootTabs.html";
 
         callIfSpringBootAppAvailable(jolokia, function () {
             workspace.topLevelTabs.push({
@@ -26,7 +25,7 @@ module SpringBoot {
                     return true;
                 },
                 href: function () {
-                    return "#/springBoot";
+                    return "#/springBoot/metrics";
                 },
                 isActive: function (workspace) {
                     return workspace.isTopTabActive("springBoot");
@@ -36,11 +35,21 @@ module SpringBoot {
 
     }]);
 
-    _module.controller("SpringBoot.MainController", ["$scope", "jolokia", ($scope, jolokia) => {
+    _module.controller("SpringBoot.MetricsController", ["$scope", "jolokia", ($scope, jolokia) => {
         jolokia.execute(metricsMBean, metricsMBeanOperation, onSuccess(function (data) {
             convertRawMetricsToUserFriendlyFormat($scope, data)
         }, {error: function(){
             $scope.loadingError = 'Cannot read metrics data.';
+            $scope.$apply();
+        }}));
+    }]);
+
+    _module.controller("SpringBoot.BeansController", ["$scope", "jolokia", ($scope, jolokia) => {
+        jolokia.execute('org.springframework.boot:type=Endpoint,name=beansEndpoint', metricsMBeanOperation, onSuccess(function (data) {
+            $scope.beans = data[0]['beans'];
+            $scope.$apply();
+        }, {error: function(){
+            $scope.loadingError = 'Cannot read beans data.';
             $scope.$apply();
         }}));
     }]);
