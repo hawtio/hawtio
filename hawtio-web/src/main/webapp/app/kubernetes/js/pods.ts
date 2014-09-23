cc0f22181059d172dc6c29475f5a59fc66a92066
Stan Lewis <slewis@fusesource.com>
2014-09-23 12:27:34 -0400

    Fix #1583 and add a simple helper function to create a confirmation dialog to perform some action on a collection of things, beats hawtioConfirmDialog :-)

--- hawtio-web/src/main/webapp/app/kubernetes/js/pods.ts
+++ hawtio-web/src/main/webapp/app/kubernetes/js/pods.ts
@@ -1,8 +1,12 @@
 /// <reference path="kubernetesPlugin.ts"/>
-
+/// <reference path="../../ui/js/dialog.ts"/>
 module Kubernetes {
+
+  interface KubePod {
+    id:string;
+  }
   
-  export var Pods = controller("Pods", ["$scope", "KubernetesPods", ($scope, KubernetesPods:ng.IPromise<ng.resource.IResourceClass>) => {
+  export var Pods = controller("Pods", ["$scope", "KubernetesPods", "$dialog", ($scope, KubernetesPods:ng.IPromise<ng.resource.IResourceClass>, $dialog) => {
 
     $scope.pods = []
     $scope.fetched = false;
@@ -33,16 +37,54 @@ module Kubernetes {
       ]
     };
 
-    KubernetesPods.then((KubernetesPods:ng.resource.IResourceClass) => {
-      // Define all our functions that need KubernetesPods
-      $scope.deletePrompt = (selected) => {
-        log.debug("delete: ", selected);
-      };
+    function fetch(KubernetesPods:ng.resource.IResourceClass) {
       // Fetch the list of pods
       KubernetesPods.query((response) => {
         $scope.fetched = true;
         $scope.pods = response['items'];
       });
+    }
+
+    KubernetesPods.then((KubernetesPods:ng.resource.IResourceClass) => {
+
+      $scope.deletePrompt = (selected:Array<KubePod>) => {
+        UI.multiItemConfirmActionDialog(<UI.MultiItemConfirmActionOptions>{
+          collection: selected,
+          index: 'id',
+          onClose: (result:boolean) => {
+            if (result) {
+              function deleteSelected(selected:Array<KubePod>, next:KubePod) {
+                if (!next) {
+                  $scope.fetched = false;
+                  // update the table
+                  fetch(KubernetesPods);
+                } else {
+                  log.debug("deleting: ", next.id);
+                  KubernetesPods.delete({
+                    id: next.id
+                  }, undefined, () => {
+                    log.debug("deleted: ", next.id);
+                    deleteSelected(selected, selected.shift());
+                  }, (error) => {
+                    log.debug("Error deleting: ", error);
+                    deleteSelected(selected, selected.shift());
+                  });
+                }
+              }
+              deleteSelected(selected, selected.pop());
+            }
+          },
+          title: 'Delete pods?',
+          action: 'The following pods will be deleted:',
+          okText: 'Delete',
+          okClass: 'btn-danger',
+          custom: "This operation is permanent once completed!",
+          customClass: "alert alert-warning"
+        }).open();
+      };
+
+      // initial fetch
+      fetch(KubernetesPods);
     });
 
   }]);
