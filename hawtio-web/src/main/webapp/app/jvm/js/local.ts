3d8e82ee8437b5fdd8a0f1dc6ae4cb626fdf6b42
Claus Ibsen <claus.ibsen@gmail.com>
2014-09-30 10:27:56 +0200

    JMV plugin. Removed the notification noise when using the local connector. Styled the table a bit better so it dont change column size when start/stop agents.

--- hawtio-web/src/main/webapp/app/jvm/js/local.ts
+++ hawtio-web/src/main/webapp/app/jvm/js/local.ts
@@ -10,9 +10,11 @@ module JVM {
     $scope.data = [];
     $scope.deploying = false;
     $scope.status = '';
+    $scope.discovering = true;
 
     $scope.fetch = () => {
-      Core.notification('info', 'Discovering local JVM processes, please wait...');
+      $scope.discovering = true;
+
       jolokia.request({
         type: 'exec', mbean: mbeanName,
         operation: 'listLocalJVMs()',
@@ -21,6 +23,7 @@ module JVM {
         success: render,
         error: (response) => {
           $scope.data = [];
+          $scope.discovering = false;
           $scope.status = 'Could not discover local JVM processes: ' + response.error;
           Core.$apply($scope);
         }
@@ -28,25 +31,21 @@ module JVM {
     };
 
     $scope.stopAgent = (pid) => {
-      Core.notification('info', "Attempting to detach agent from PID " + pid);
       jolokia.request({
         type: 'exec', mbean: mbeanName,
         operation: 'stopAgent(java.lang.String)',
         arguments: [pid]
       }, onSuccess(function() {
-        Core.notification('success', "Detached agent from PID " + pid);
         $scope.fetch()
       }));
     };
 
     $scope.startAgent = (pid) => {
-      Core.notification('info', "Attempting to attach agent to PID " + pid);
       jolokia.request({
         type: 'exec', mbean: mbeanName,
         operation: 'startAgent(java.lang.String)',
         arguments: [pid]
       }, onSuccess(function() {
-        Core.notification('success', "Attached agent to PID " + pid);
         $scope.fetch()
       }));
     };
@@ -71,6 +70,7 @@ module JVM {
     };
 
     function render(response) {
+      $scope.discovering = false;
       $scope.data = response.value;
       if ($scope.data.length === 0) {
         $scope.status = 'Could not discover local JVM processes';
