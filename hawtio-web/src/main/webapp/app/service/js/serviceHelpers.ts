dd9dbae429db84da7e55eb37772cdaf70b0ba03f
James Strachan <james.strachan@gmail.com>
2014-11-24 13:24:00 +0000

    add a couple of helper methods to so its easier to show/hide services based on the remote presence of kube services

--- hawtio-web/src/main/webapp/app/service/js/serviceHelpers.ts
+++ hawtio-web/src/main/webapp/app/service/js/serviceHelpers.ts
@@ -5,4 +5,38 @@ module Service {
   export var pluginName = 'Service';
   export var log:Logging.Logger = Logger.get(pluginName);
 
+  /**
+   * Returns true if there is a service available for the given ID or false
+   */
+  export function hasService(ServiceRegistry, serviceName: string) {
+    if (!ServiceRegistry || !serviceName) {
+      return false;
+    }
+    var answer = false;
+    angular.forEach(ServiceRegistry.services, (service) => {
+      if (serviceName === service.id) {
+        answer = true;
+      }
+    });
+    return answer;
+  }
+
+  /**
+   * Returns the service for the given service name (ID) or null if it cannot be found
+   *
+   * @param ServiceRegistry
+   * @param serviceName
+   * @return {null}
+   */
+  export function findService(ServiceRegistry, serviceName: string) {
+    var answer = null;
+    if (ServiceRegistry && serviceName) {
+      angular.forEach(ServiceRegistry.services, (service) => {
+        if (serviceName === service.id) {
+          answer = service;
+        }
+      });
+    }
+    return answer;
+  }
 }
