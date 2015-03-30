0584d558345e8bcff6140443bc1fa0f19c3d3d73
Stan Lewis <slewis@fusesource.com>
2015-03-30 09:42:28 -0400

    Use gulp-gh-pages to deploy site - test run

--- ci-website.sh
+++ ci-website.sh
@@ -5,9 +5,14 @@ export MAVEN_OPTS="-Xmx986m -XX:MaxPermSize=350m"
 echo ============================================================================
 echo Deploying hawtio website
 echo ============================================================================
-rm -rf hawtio
-git clone git@github.com:hawtio/hawtio.git hawtio && \
-cd hawtio && \
-mvn clean install && \
+#rm -rf hawtio
+#git clone git@github.com:hawtio/hawtio.git hawtio && \
+#cd hawtio && \
+#mvn clean install && \
+#cd website && \
+#mvn clean scalate:sitegen scalate:deploy
+
 cd website && \
-mvn clean scalate:sitegen scalate:deploy
+mvn clean scalate:sitegen && \
+npm install && \
+gulp
---
+++ website/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins');
+
+var plugins = gulpLoadPlugins({});
+var pkg = require('./package.json');
+
+gulp.task('deploy', function() {
+  return gulp.src(['target/sitegen/**', 'target/sitegen/**/*.*', 'target/sitegen/*.*'], { base: 'target/sitegen' })
+    .pipe(plugins.debug({title: 'deploy'}))
+    .pipe(plugins.ghPages({
+      push: false,
+      message: "[ci skip] Update site"                     
+    }));
+});
+
+gulp.task('default', ['deploy']);
+
+
+
