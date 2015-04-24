130ddd438f8bc8c41ef6d7c5ee5639ac5fdd60ee
CircleCI <circleci@mail.com>
2015-04-24 12:20:35 +0000

    [ci skip] Update site

--- app/app/app.js
+++ app/app/app.js
@@ -49121,7 +49121,7 @@ var Wiki;
                     var wikiLinks = $(".logbar");
                     var top = (wikiLinks.height() + 8) + "px";
                     $element.css({ "margin-top": top });
-                }, 0, false);
+                }, 1000, false);
             }
         };
     }]);
