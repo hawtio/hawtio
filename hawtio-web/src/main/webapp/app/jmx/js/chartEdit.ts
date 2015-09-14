1ffc610c5cb62f64f878d9c8bae84e22edb40d4d
Grzegorz Grzybek <gr.grzybek@gmail.com>
2015-09-14 12:21:38 +0200

    [ENTESB-3390] Remove duplicate chart elements/attributes

--- hawtio-web/src/main/webapp/app/jmx/js/chartEdit.ts
+++ hawtio-web/src/main/webapp/app/jmx/js/chartEdit.ts
@@ -122,14 +122,14 @@ module Jmx {
                   var elementNames = toSearchArgumentArray(search["el"]);
                   if (attributeNames && attributeNames.length) {
                     attributeNames.forEach((name) => {
-                      if ($scope.metrics[name]) {
+                      if ($scope.metrics[name] && $scope.selectedAttributes.none(el => el === name)) {
                         $scope.selectedAttributes.push(name);
                       }
                     });
                   }
                   if (elementNames && elementNames.length) {
                     elementNames.forEach((name) => {
-                      if ($scope.mbeans[name]) {
+                      if ($scope.mbeans[name] && $scope.selectedMBeans.none(el => el === name)) {
                         $scope.selectedMBeans.push(name);
                       }
                     });
