36ae06ab18f0886a5bf4a42150db35f13fb0b4c9
Claus Ibsen <claus.ibsen@gmail.com>
2015-10-21 10:45:47 +0200

    #1920: Port insight-log module from fabric8 v1 to hawtio and make it work for Karaf 3 onwards.

--- hawtio-insight-log-osgi/src/main/java/io/hawt/log/osgi/LogQuery.java
+++ hawtio-insight-log-osgi/src/main/java/io/hawt/log/osgi/LogQuery.java
@@ -1,65 +1,28 @@
 package io.hawt.log.osgi;
 
 import java.io.IOException;
-import javax.management.MBeanServer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.hawt.log.LogFilter;
 import io.hawt.log.LogResults;
 import io.hawt.log.support.LogQuerySupport;
 import io.hawt.log.support.Predicate;
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.ReferenceCardinality;
-import org.apache.felix.scr.annotations.ReferencePolicy;
+import org.apache.karaf.log.core.LogService;
 import org.ops4j.pax.logging.spi.PaxLoggingEvent;
-import org.osgi.service.log.LogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * An implementation of {@link LogQueryMBean} using the embedded pax appender used by karaf
  */
-@Component
 public class LogQuery extends LogQuerySupport implements LogQueryMBean {
     private transient Logger LOG = LoggerFactory.getLogger(LogQuery.class);
 
-    @Reference
-    private MBeanServer mbeanServer;
+    private final LogService logService;
 
-    @Reference(referenceInterface = LogService.class, policy = ReferencePolicy.DYNAMIC, cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE, bind = "bindLogService", unbind = "unbindLogService")
-    private LogService logService;
-
-    private volatile org.apache.karaf.log.core.LogService karafLogService;
-
-    public LogQuery() {
+    public LogQuery(LogService logService) {
+        this.logService = logService;
         mapper.getSerializationConfig().withSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-    }
-
-    @Activate
-    public void activate() {
-        registerMBeanServer(mbeanServer);
-    }
-
-    @Deactivate
-    public void deactivate() {
-        unregisterMBeanServer(mbeanServer);
-    }
-
-    public void bindLogService(LogService service) {
-        this.logService = service;
-        if (service instanceof org.apache.karaf.log.core.LogService) {
-            karafLogService = (org.apache.karaf.log.core.LogService) service;
-        }
-    }
-
-    public void unbindLogService(LogService service) {
-        if (logService != null && logService.equals(service)) {
-            logService = null;
-            karafLogService = null;
-        }
     }
 
     @Override
@@ -89,8 +52,8 @@ public class LogQuery extends LogQuerySupport implements LogQueryMBean {
 
         long from = Long.MAX_VALUE;
         long to = Long.MIN_VALUE;
-        if (karafLogService != null) {
-            Iterable<PaxLoggingEvent> iterable = karafLogService.getEvents();
+        if (logService != null) {
+            Iterable<PaxLoggingEvent> iterable = logService.getEvents();
             if (iterable != null) {
                 int matched = 0;
                 for (PaxLoggingEvent event : iterable) {
