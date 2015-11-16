8208abbc225b5db72cf52e70cf20fd2451b5f456
Claus Ibsen <claus.ibsen@gmail.com>
2015-11-16 17:50:08 +0100

    #2031: camel goal should pickup mainClass from camel-maven-plugin if none explict configured.

--- hawtio-maven-plugin/src/main/java/io/hawt/maven/CamelMojo.java
+++ hawtio-maven-plugin/src/main/java/io/hawt/maven/CamelMojo.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 @Mojo(name = "camel", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES, requiresDependencyResolution = ResolutionScope.RUNTIME)
 @Execute(phase = LifecyclePhase.PROCESS_TEST_CLASSES)
@@ -31,6 +33,24 @@ public class CamelMojo extends RunMojo {
         } else if (fileApplicationContextUri != null) {
             args.add("-fa");
             args.add(fileApplicationContextUri);
+        }
+
+        // if no mainClass configured then try to find it from camel-maven-plugin
+        if (mainClass == null && project.getBuildPlugins() != null) {
+            for (Object obj : project.getBuildPlugins()) {
+                Plugin plugin = (Plugin) obj;
+                if ("org.apache.camel".equals(plugin.getGroupId()) && "camel-maven-plugin".equals(plugin.getArtifactId())) {
+                    Object config = plugin.getConfiguration();
+                    System.out.println(config);
+                    if (config instanceof Xpp3Dom) {
+                        Xpp3Dom dom = (Xpp3Dom) config;
+                        Xpp3Dom child = dom.getChild("mainClass");
+                        if (child != null) {
+                            mainClass = child.getValue();
+                        }
+                    }
+                }
+            }
         }
 
         if (mainClass != null) {
