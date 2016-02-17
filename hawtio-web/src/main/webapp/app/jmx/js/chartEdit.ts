fdb2893cbdee6a2f33296e0922f6211592172703
Kevin Earls <kevin@kevinearls.com>
2016-02-17 17:34:17 +0100

    ENTESB-4165 fix redirect to edit chart behavior

--- hawtio-web/src/main/webapp/app/jmx/js/chartEdit.ts
+++ hawtio-web/src/main/webapp/app/jmx/js/chartEdit.ts
@@ -23,6 +23,23 @@ module Jmx {
               $scope.size($scope.mbeans) > 0 && $scope.size($scope.metrics) > 0;
     };
 
+
+    $scope.canEditChart = () => {
+      // Similar to can view chart, although rules are slightly different for parents
+      var result;
+      if (workspace.selection.isFolder()) {
+        // For ENTESB-4165.  This is a bit hacky but needed to deal with special conditions like
+        // where there is only a single queue or topic
+        result =  $scope.selectedAttributes.length && $scope.selectedMBeans.length &&
+            ($scope.size($scope.mbeans) + $scope.size($scope.metrics) > 2);
+      } else {
+        result =  $scope.selectedAttributes.length && $scope.selectedMBeans.length &&
+            $scope.size($scope.mbeans) > 0 && $scope.size($scope.metrics) > 0;
+      }
+      console.info("canEditChart Returning " + result);
+      return result;
+    }
+
     $scope.showAttributes = () => {
       return $scope.canViewChart() && $scope.size($scope.metrics) > 1;
     };
@@ -41,7 +58,7 @@ module Jmx {
         search["att"] = $scope.selectedAttributes;
       }
       // if we are on an mbean with no children lets discard an unnecessary parameter
-      if ($scope.selectedMBeans.length === $scope.size($scope.mbeans) && $scope.size($scope.mbeans) === 1) {
+      if (!workspace.selection.isFolder() && $scope.selectedMBeans.length === $scope.size($scope.mbeans) && $scope.size($scope.mbeans) === 1) {
         delete search["el"];
       } else {
         search["el"] = $scope.selectedMBeans;
