990fe1949546f63cbd89cf3c9a9c32bb717d0664
Kevin Earls <kevin@kevinearls.com>
2016-03-31 15:31:04 +0200

    ENTESB-5199 fix tag behavior for container/profiles view

--- hawtio-web/src/main/webapp/app/fabric/js/containerView.ts
+++ hawtio-web/src/main/webapp/app/fabric/js/containerView.ts
@@ -24,6 +24,7 @@ module Fabric {
     $scope.filterProfilesText = 'Filter Profiles...';
     $scope.filterLocationsText = 'Filter Locations...';
     $scope.filterBoxText = $scope.filterContainersText;
+    $scope.selectedTags = [];
 
     $scope.createLocationDialog = ContainerHelpers.getCreateLocationDialog($scope, $dialog);
 
