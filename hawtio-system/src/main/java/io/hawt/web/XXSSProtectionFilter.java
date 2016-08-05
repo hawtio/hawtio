1361fb2d25c2d13ba7de0c3b52b07c43367e31fd
Kevin Earls <kevin@kevinearls.com>
2016-08-05 10:45:24 +0200

    ENTESB-5859 Add X-XSS-Protection header

---
+++ hawtio-system/src/main/java/io/hawt/web/XXSSProtectionFilter.java
@@ -0,0 +1,30 @@
+package io.hawt.web;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class XXSSProtectionFilter implements Filter {
+
+    public XXSSProtectionFilter() {
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.addHeader("X-XSS-Protection", "1");
+        }
+        chain.doFilter(request, response);
+    }
+}
