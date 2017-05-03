3b5aec39aa3d40ba9c6181ae454c4643ffcee5cf
Paolo Antinori <pantinor@redhat.com>
2017-05-03 12:36:31 +0200

    Keycloak - support URLHandlers

--- hawtio-system/src/main/java/io/hawt/web/keycloak/KeycloakServlet.java
+++ hawtio-system/src/main/java/io/hawt/web/keycloak/KeycloakServlet.java
@@ -1,12 +1,11 @@
 package io.hawt.web.keycloak;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -124,8 +123,8 @@ public class KeycloakServlet extends HttpServlet {
             return getClass().getClassLoader().getResourceAsStream(classPathLocation);
         } else {
             try {
-                return new FileInputStream(keycloakConfigFile);
-            } catch (FileNotFoundException fnfe) {
+                return new URL(keycloakConfigFile).openStream();
+            } catch (Exception e) {
                 LOG.warn("Couldn't find keycloak config file on location: " + keycloakConfigFile);
                 return null;
             }
