c33d8a966e2363887de1536fca8e6aae573b7e18
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2017-06-20 16:21:55 +0900

    ENTESB-6823 - Route diagram in Hawtio has inconsistent behavior for ID attributes

--- hawtio-web/src/main/webapp/app/camel/js/camelHelpers.ts
+++ hawtio-web/src/main/webapp/app/camel/js/camelHelpers.ts
@@ -1316,9 +1316,9 @@ module Camel {
           var text = route.textContent;
           if (text) {
             parentNode["tooltip"] = parentNode["label"] + " " + name + " " + text;
-            parentNode["label"] = text;
+            parentNode["label"] += ": " + appendLabel(route, text, true);
           } else {
-            parentNode["label"] = parentNode["label"] + " " + name;
+            parentNode["label"] += ": " + appendLabel(route, name, false);
           }
         }
       }
@@ -1335,6 +1335,27 @@ module Camel {
       }
     });
     return siblingNodes;
+  }
+
+  function appendLabel(route: Element, label: string, text: boolean): string {
+    switch (route.localName) {
+      case "method":
+        if (!text) {
+          if (route.getAttribute("bean")) {
+            label += " " + route.getAttribute("bean");
+          } else if (route.getAttribute("ref")) {
+            label += " " + route.getAttribute("ref");
+          } else if (route.getAttribute("beanType")) {
+            label += " " + route.getAttribute("beanType");
+          }
+        }
+        if (route.getAttribute("method")) {
+          label += " " + route.getAttribute("method");
+        }
+        break;
+      default:
+    }
+    return label;
   }
 
   export function getCanvasHeight(canvasDiv) {
