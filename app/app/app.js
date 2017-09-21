fc65514311fc82bcc5c2ff073d1fd323effa0047
CircleCI <circleci@mail.com>
2017-09-21 09:40:42 +0000

    [ci skip] Update site

--- app/app/app.js
+++ app/app/app.js
@@ -34390,68 +34390,6 @@ var Jmx;
 })(Jmx || (Jmx = {}));
 var Jmx;
 (function (Jmx) {
-    Jmx._module.controller("Jmx.AttributeController", ["$scope", "jolokia", function ($scope, jolokia) {
-        $scope.init = function (mbean, attribute) {
-            $scope.mbean = mbean;
-            $scope.attribute = attribute;
-            if (angular.isDefined($scope.mbean) && angular.isDefined($scope.attribute)) {
-                Core.register(jolokia, $scope, {
-                    type: 'read',
-                    mbean: $scope.mbean,
-                    attribute: $scope.attribute
-                }, onSuccess(render));
-            }
-        };
-        function render(response) {
-            if (!Object.equal($scope.data, response.value)) {
-                $scope.data = safeNull(response.value);
-                Core.$apply($scope);
-            }
-        }
-    }]);
-    Jmx._module.controller("Jmx.AttributeChartController", ["$scope", "jolokia", "$document", function ($scope, jolokia, $document) {
-        $scope.init = function (mbean, attribute) {
-            $scope.mbean = mbean;
-            $scope.attribute = attribute;
-            if (angular.isDefined($scope.mbean) && angular.isDefined($scope.attribute)) {
-                Core.register(jolokia, $scope, {
-                    type: 'read',
-                    mbean: $scope.mbean,
-                    attribute: $scope.attribute
-                }, onSuccess(render));
-            }
-        };
-        function render(response) {
-            if (!angular.isDefined($scope.chart)) {
-                $scope.chart = $($document.find("#" + $scope.attribute)[0]);
-                if ($scope.chart) {
-                    $scope.width = $scope.chart.width();
-                }
-            }
-            if (!angular.isDefined($scope.context)) {
-                console.log("Got: ", response);
-                $scope.context = cubism.context().serverDelay(0).clientDelay(0).step(1000).size($scope.width);
-                $scope.jcontext = $scope.context.jolokia(jolokia);
-                $scope.metrics = [];
-                Object.extended(response.value).keys(function (key, value) {
-                    $scope.metrics.push($scope.jcontext.metric({
-                        type: 'read',
-                        mbean: $scope.mbean,
-                        attribute: $scope.attribute,
-                        path: key
-                    }, $scope.attribute));
-                });
-                d3.select("#" + $scope.attribute).call(function (div) {
-                    div.append("div").data($scope.metrics).call($scope.context.horizon());
-                });
-                Core.unregister(jolokia, $scope);
-                Core.$apply($scope);
-            }
-        }
-    }]);
-})(Jmx || (Jmx = {}));
-var Jmx;
-(function (Jmx) {
     Jmx.propertiesColumnDefs = [
         {
             field: 'name',
