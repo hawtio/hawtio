18fe5cf3411ea9da03133b12b177a638279b7df1
Alexandre Kieling <abrianik@redhat.com>
2017-11-17 17:03:09 -0200

    fix bug on AuthenticationFilter.java

--- hawtio-system/src/main/java/io/hawt/web/auth/AuthenticationFilter.java
+++ hawtio-system/src/main/java/io/hawt/web/auth/AuthenticationFilter.java
@@ -1,28 +1,20 @@
 package io.hawt.web.auth;
 
-import java.io.IOException;
-import java.security.PrivilegedActionException;
-import java.security.PrivilegedExceptionAction;
-import java.util.ArrayList;
-import java.util.List;
-import javax.security.auth.Subject;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import io.hawt.system.AuthInfo;
 import io.hawt.system.AuthenticateResult;
 import io.hawt.system.Authenticator;
-import io.hawt.system.ConfigManager;
 import io.hawt.web.ServletHelpers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.security.auth.Subject;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 
 /**
  * Filter for authentication. If the filter is enabled, then the login screen is shown.
@@ -43,6 +35,7 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         String path = httpRequest.getServletPath();
+
         LOG.debug("Handling request for path {}", path);
 
         if (configuration.getRealm() == null || configuration.getRealm().equals("") || !configuration.isEnabled()) {
@@ -62,52 +55,50 @@ public class AuthenticationFilter implements Filter {
             }
         }
 
-        LoginRedirectFilter.redirect(httpRequest, httpResponse);
+        LOG.debug("Doing authentication and authorization for path {}", path);
 
-//        LOG.debug("Doing authentication and authorization for path {}", path);
-//        AuthenticateResult result = Authenticator.authenticate(
-//            configuration.getRealm(),
-//            configuration.getRole(),
-//            configuration.getRolePrincipalClasses(),
-//            configuration.getConfiguration(),
-//            httpRequest,
-//            subject -> executeAs(request, response, chain, subject));
-//        HttpServletResponse httpResponse = (HttpServletResponse) response;
-//        switch (result) {
-//            case AUTHORIZED:
-//                // request was executed using the authenticated subject, nothing more to do
-//                break;
-//            case NOT_AUTHORIZED:
-//                ServletHelpers.doForbidden(httpResponse);
-//                break;
-//            case NO_CREDENTIALS:
-//                if (configuration.isNoCredentials401()) {
-//                    // return auth prompt 401
-//                    ServletHelpers.doAuthPrompt(configuration.getRealm(), httpResponse);
-//                } else {
-//                    // return forbidden 403 so the browser login does not popup
-//                    ServletHelpers.doForbidden(httpResponse);
-//                }
-//                break;
-//        }
+        AuthenticateResult result = Authenticator.authenticate(
+            configuration.getRealm(),
+            configuration.getRole(),
+            configuration.getRolePrincipalClasses(),
+            configuration.getConfiguration(),
+            httpRequest,
+            subject -> executeAs(request, response, chain, subject));
+
+        switch (result) {
+            case AUTHORIZED:
+                // request was executed using the authenticated subject, nothing more to do
+                break;
+            case NOT_AUTHORIZED:
+                ServletHelpers.doForbidden(httpResponse);
+                break;
+            case NO_CREDENTIALS:
+                if (configuration.isNoCredentials401()) {
+                    // return auth prompt 401
+                    ServletHelpers.doAuthPrompt(configuration.getRealm(), httpResponse);
+                } else {
+                    // return forbidden 403 so the browser login does not popup
+                    ServletHelpers.doForbidden(httpResponse);
+                }
+                break;
+        }
     }
 
     private boolean validateSession(HttpServletRequest request, HttpSession session, Subject subject) {
-//        String authHeader = request.getHeader(Authenticator.HEADER_AUTHORIZATION);
-//        AuthInfo info = new AuthInfo();
-//        if (authHeader != null && !authHeader.equals("")) {
-//            Authenticator.extractAuthInfo(authHeader, (userName, password) -> info.username = userName);
-//        }
-//        String sessionUser = (String) session.getAttribute("user");
-//        if (info.username == null || info.username.equals(sessionUser)) {
-//            LOG.debug("Session subject - {}", subject);
-//            return true;
-//        } else {
-//            LOG.debug("User differs, re-authenticating: {} (request) != {} (session)", info.username, sessionUser);
-//            session.invalidate();
-//            return false;
-//        }
-        return true;
+        String authHeader = request.getHeader(Authenticator.HEADER_AUTHORIZATION);
+        AuthInfo info = new AuthInfo();
+        if (authHeader != null && !authHeader.equals("")) {
+            Authenticator.extractAuthInfo(authHeader, (userName, password) -> info.username = userName);
+        }
+        String sessionUser = (String) session.getAttribute("user");
+        if (info.username == null || info.username.equals(sessionUser)) {
+            LOG.debug("Session subject - {}", subject);
+            return true;
+        } else {
+            LOG.debug("User differs, re-authenticating: {} (request) != {} (session)", info.username, sessionUser);
+            session.invalidate();
+            return false;
+        }
     }
 
     private static void executeAs(final ServletRequest request, final ServletResponse response, final FilterChain chain, Subject subject) {
