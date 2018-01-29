73dd83e32db0c13e5b03f44696a3f4661f07454e
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-01-29 14:15:33 +0900

    #2413: Update hawtio-website dependencies

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -8,11 +8,8 @@ gulp.task('deploy', function() {
   return gulp.src(['target/sitegen/**', 'target/sitegen/**/*.*', 'target/sitegen/*.*'], { base: 'target/sitegen' })
     .pipe(plugins.debug({title: 'deploy'}))
     .pipe(plugins.ghPages({
-      message: "[ci skip] Update site"                     
+      message: "[ci skip] Update site"
     }));
 });
 
 gulp.task('default', ['deploy']);
-
-
-
