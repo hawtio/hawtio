698ea6683b7de113b00049a17e554f7bae662a8f
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-03-30 19:03:59 +0900

    Update run-hawtio script

--- examples/keycloak-integration/run-hawtio.sh
+++ examples/keycloak-integration/run-hawtio.sh
@@ -9,12 +9,12 @@ fi
 HOME=`cd $(dirname $0); pwd`
 
 JAVA_OPTS="$JAVA_OPTS -Dhawtio.authenticationEnabled=true"
-JAVA_OPTS="$JAVA_OPTS -Dhawtio.realm=hawtio-demo"
+JAVA_OPTS="$JAVA_OPTS -Dhawtio.realm=hawtio"
 JAVA_OPTS="$JAVA_OPTS -Dhawtio.keycloakEnabled=true"
 JAVA_OPTS="$JAVA_OPTS -Dhawtio.roles=admin,manager,viewer"
 JAVA_OPTS="$JAVA_OPTS -Dhawtio.rolePrincipalClasses=org.keycloak.adapters.jaas.RolePrincipal"
-JAVA_OPTS="$JAVA_OPTS -Dhawtio.keycloakClientConfig=$HOME/keycloak-hawtio-client.json"
-JAVA_OPTS="$JAVA_OPTS -Dhawtio.keycloakServerConfig=$HOME/keycloak-hawtio.json"
+JAVA_OPTS="$JAVA_OPTS -Dhawtio.keycloakClientConfig=$HOME/keycloak-hawtio.json"
+JAVA_OPTS="$JAVA_OPTS -Dhawtio.keycloakServerConfig=$HOME/keycloak-bearer.json"
 JAVA_OPTS="$JAVA_OPTS -Djava.security.auth.login.config=$HOME/login.conf"
 
 echo =========================================================================
