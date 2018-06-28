b2204e4d9143b32876e82b754252110a60c50302
jwalker <jwalker@aamc.org>
2018-06-28 10:52:11 -0400

    added changes for ssl

--- hawtio-embedded/src/main/java/io/hawt/embedded/Options.java
+++ hawtio-embedded/src/main/java/io/hawt/embedded/Options.java
@@ -35,6 +35,8 @@ public class Options {
     private boolean help;
     private boolean jointServerThread;
     private boolean openUrl = true;
+    private String keyStore = null;
+    private String keyStorePass = null;
 
     private abstract class Option {
         private String abbreviation;
@@ -166,6 +168,16 @@ public class Options {
                 openUrl = Boolean.valueOf(parameter);
             }
         });
+        addOption(new ParameterOption("ks", "keyStore", "JKS keyStore with the keys for https") {
+            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+                keyStore = parameter;
+            }
+        });
+        addOption(new ParameterOption("kp", "keyStorePass", "password for the JKS keyStore with the keys for https") {
+            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+                keyStorePass = parameter;
+            }
+        });
 
     }
 
@@ -206,6 +218,12 @@ public class Options {
         }
         if (plugins != null) {
             sb.append("\n\tplugins=").append(plugins);
+        }
+        if (keyStore != null) {
+            sb.append("\n\tkeyStore=").append(keyStore);
+        }
+        if (keyStorePass != null) {
+            sb.append("\n\tkeyStore=").append(keyStorePass);
         }
         sb.append("\n\topenUrl=").append(openUrl);
         sb.append("\n\tjointServerThread=").append(jointServerThread);
@@ -312,5 +330,16 @@ public class Options {
     public boolean isHelp() {
         return help;
     }
-
+    public String getKeyStore(){
+        return keyStore;
+    }
+    public void setKeyStore(String keyStore){
+        this.keyStore=keyStore;
+    }
+    public String getKeyStorePass(){
+        return keyStorePass;
+    }
+    public void setKeyStorePass(String keyStorePass){
+        this.keyStorePass=keyStorePass;
+    }
 }
