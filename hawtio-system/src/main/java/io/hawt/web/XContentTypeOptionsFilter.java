5fcc8f5f155773d66d51882724d380ab12b80660
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2018-08-01 14:46:06 +0900

    feat(hawtio-system): ENTESB-7876 - Implement the latest HTTP security features
    (cherry picked from commit b43baba6eed29863b0aaeca20a875aa2fb0030c0)

--- hawtio-system/src/main/java/io/hawt/web/XContentTypeOptionsFilter.java
+++ hawtio-system/src/main/java/io/hawt/web/XContentTypeOptionsFilter.java
@@ -4,12 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
  */
-public class XFrameOptionsFilter extends HttpHeaderFilter {
+public class XContentTypeOptionsFilter extends HttpHeaderFilter {
 
     @Override
     protected void addHeaders(HttpServletRequest request, HttpServletResponse response) {
-        response.addHeader("X-Frame-Options", "SAMEORIGIN");
+        response.addHeader("X-Content-Type-Options", "nosniff");
     }
 }
