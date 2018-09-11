d7b32d54b20cbd5e49a058805a73c76ba6dd2ef7
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-09-11 15:06:34 +0200

    [ENTESB-9242] Add discovered Keycloak host:port to CSP header

--- hawtio-system/src/main/java/io/hawt/web/filters/ContentSecurityPolicyFilter.java
+++ hawtio-system/src/main/java/io/hawt/web/filters/ContentSecurityPolicyFilter.java
@@ -1,18 +1,66 @@
 package io.hawt.web.filters;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import io.hawt.web.ServletHelpers;
+import io.hawt.web.auth.keycloak.KeycloakServlet;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
  */
 public class ContentSecurityPolicyFilter extends HttpHeaderFilter {
 
-    private static final String POLICY =
+    private static final transient Logger LOG = LoggerFactory.getLogger(ContentSecurityPolicyFilter.class);
+
+    private static String POLICY = "";
+    private static final String POLICY_TEMPLATE =
         "default-src 'self'; " +
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+        "script-src 'self'%s 'unsafe-inline' 'unsafe-eval'; " +
         "style-src 'self' 'unsafe-inline'; " +
-        "font-src 'self' data:";
+        "font-src 'self' data:; " +
+        "connect-src 'self'%s";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
+
+        String keycloakConfigFile = getConfigParameter(KeycloakServlet.KEYCLOAK_CLIENT_CONFIG);
+        if (keycloakConfigFile == null && System.getProperty(KeycloakServlet.HAWTIO_KEYCLOAK_CLIENT_CONFIG) != null) {
+            keycloakConfigFile = System.getProperty(KeycloakServlet.HAWTIO_KEYCLOAK_CLIENT_CONFIG);
+        }
+
+        boolean addedKeycloakUrl = false;
+        if (keycloakConfigFile != null) {
+            try (FileReader reader = new FileReader(keycloakConfigFile)) {
+                JSONObject json = ServletHelpers.readObject(new BufferedReader(reader));
+                String url = (String) json.get("url");
+                URI uri = URI.create(url);
+                LOG.info("Found Keycloak URL: {}", uri);
+                // mind the initial whitespace
+                String hostPort = String.format(" %s:%d", uri.getHost(), uri.getPort() > 0 ? uri.getPort()
+                        : "https".equals(uri.getScheme()) ? 443 : 80);
+                POLICY = String.format(POLICY_TEMPLATE, hostPort, hostPort);
+                addedKeycloakUrl = true;
+            } catch (IOException e) {
+                LOG.error("Can't read keycloak configuration file", e);
+            }
+        }
+        if (!addedKeycloakUrl) {
+            POLICY = String.format(POLICY_TEMPLATE, "", "");
+        }
+
+        super.init(filterConfig);
+    }
 
     @Override
     protected void addHeaders(HttpServletRequest request, HttpServletResponse response) {
