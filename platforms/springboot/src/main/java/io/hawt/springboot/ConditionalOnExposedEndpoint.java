1e06831fd9658a611b2cc580517e27a10c83e8b7
James Netherton <jamesnetherton@gmail.com>
2019-01-04 09:47:23 +0000

    fix(spring-boot): Ensure ExposedEndpoint condition can handle all variants of defining Spring properties
    relates #2559

--- platforms/springboot/src/main/java/io/hawt/springboot/ConditionalOnExposedEndpoint.java
+++ platforms/springboot/src/main/java/io/hawt/springboot/ConditionalOnExposedEndpoint.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.context.annotation.Conditional;
 
-@Target({ ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Conditional(ExposedEndpoint.class)
 public @interface ConditionalOnExposedEndpoint {
--- platforms/springboot/src/main/java/io/hawt/springboot/ExposedEndpoint.java
+++ platforms/springboot/src/main/java/io/hawt/springboot/ExposedEndpoint.java
@@ -1,13 +1,21 @@
 package io.hawt.springboot;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class ExposedEndpoint implements Condition {
+
+    private static final String WEB_EXPOSURE_INCLUDE = "management.endpoints.web.exposure.include";
+    private static final Bindable<List<String>> STRING_LIST = Bindable.listOf(String.class);
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
@@ -16,14 +24,9 @@ public class ExposedEndpoint implements Condition {
         if (attributes != null) {
             String endpointName = (String) attributes.get("name");
             Environment environment = context.getEnvironment();
-            String property = environment.getProperty("management.endpoints.web.exposure.include");
-            if (property != null && !property.isEmpty()) {
-                for (String exposure : property.split(",")) {
-                    if (exposure.equals(endpointName)) {
-                        return true;
-                    }
-                }
-            }
+            BindResult<List<String>> property = Binder.get(environment).bind(WEB_EXPOSURE_INCLUDE, STRING_LIST);
+            List<String> exposedEndpoints = property.orElse(Collections.emptyList());
+            return exposedEndpoints.contains(endpointName);
         }
 
         return false;
