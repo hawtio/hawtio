f4fabee6fabcf92e8614effafdd833acd959640c
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2020-01-31 10:32:12 +0900

    fix release script

--- release.sh
+++ release.sh
@@ -18,6 +18,6 @@ mvn -s $M2_SETTINGS --batch-mode \
   -DdevelopmentVersion=1.5-SNAPSHOT && \
 mvn -s $M2_SETTINGS --batch-mode org.apache.maven.plugins:maven-release-plugin:2.5:perform -Prelease,watcher && \
 git push --tags && \
-REPO_ID=$(mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:rc-list -DserverId=oss-sonatype-staging -DnexusUrl=https://oss.sonatype.org | grep OPEN | grep -Eo 'iohawt-[[:digit:]]+') && \
+REPO_ID=$(mvn -s $M2_SETTINGS org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:rc-list -DserverId=oss-sonatype-staging -DnexusUrl=https://oss.sonatype.org | grep OPEN | grep -Eo 'iohawt-[[:digit:]]+') && \
 mvn -s $M2_SETTINGS org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:rc-close -DserverId=oss-sonatype-staging -DnexusUrl=https://oss.sonatype.org -DstagingRepositoryId=${REPO_ID} -Ddescription="${release_version} is ready" -DstagingProgressTimeoutMinutes=60 && \
 mvn -s $M2_SETTINGS org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:rc-release -DserverId=oss-sonatype-staging -DnexusUrl=https://oss.sonatype.org -DstagingRepositoryId=${REPO_ID} -Ddescription="${release_version} is ready" -DstagingProgressTimeoutMinutes=60
