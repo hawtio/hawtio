3165215dfa1c921d03d4449f6b13b0aa7197485e
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2020-10-08 13:45:51 +0900

    feat(hawtio-console-assembly): add "remember me" checkbox to Login page #2652
    Fixes #2652

--- hawtio-console-assembly/app/src/login/login.component.ts
+++ hawtio-console-assembly/app/src/login/login.component.ts
@@ -2,18 +2,30 @@ namespace Login {
 
   const LOGIN_URL: string = 'auth/login';
 
+  const LOCAL_STORAGE_KEY_LOGIN: string = 'login';
+
   export class LoginController {
     branding: Core.Branding = { appName: '', appLogoUrl: '' };
     login: Core.Login = {};
     entity = { username: '', password: '' };
     wrongPassword: boolean = false;
+    rememberMe: boolean = false;
 
     constructor(
       private configManager: Core.ConfigManager,
       private $http: ng.IHttpService,
       private $window: ng.IWindowService,
+      private localStorage: Storage,
       private documentBase: string) {
       'ngInject';
+
+      if (LOCAL_STORAGE_KEY_LOGIN in localStorage) {
+        const login = angular.fromJson(localStorage[LOCAL_STORAGE_KEY_LOGIN]);
+        if (login.username) {
+          this.entity.username = login.username;
+          this.rememberMe = true;
+        }
+      }
     }
 
     $onInit(): void {
@@ -28,6 +40,11 @@ namespace Login {
       this.$http.post(LOGIN_URL, this.entity).then(
         (response: ng.IHttpResponse<any>) => {
           log.debug("Server login success:", response.data);
+          if (this.rememberMe) {
+            this.localStorage[LOCAL_STORAGE_KEY_LOGIN] = angular.toJson({ username: this.entity.username });
+          } else {
+            delete this.localStorage[LOCAL_STORAGE_KEY_LOGIN];
+          }
           this.$window.location.href = this.documentBase;
         },
         (response) => {
