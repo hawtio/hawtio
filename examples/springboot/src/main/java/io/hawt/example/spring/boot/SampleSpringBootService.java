adf5d0addd0c926f0d739728c2cc15e1296939e6
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-04-01 14:30:53 +0900

    chore(examples): add configs for Spring Boot Info endpoint to springboot example

--- examples/springboot/src/main/java/io/hawt/example/spring/boot/SampleSpringBootService.java
+++ examples/springboot/src/main/java/io/hawt/example/spring/boot/SampleSpringBootService.java
@@ -3,6 +3,8 @@ package io.hawt.example.spring.boot;
 import io.hawt.config.ConfigFacade;
 import io.hawt.springboot.HawtioPlugin;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -30,5 +32,13 @@ public class SampleSpringBootService {
     @Bean
     public ConfigFacade configFacade() {
         return ConfigFacade.getSingleton();
+    }
+
+    /**
+     * Enable HTTP tracing for Spring Boot
+     */
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 }
