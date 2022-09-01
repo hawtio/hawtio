cc8f10465fe1b9ee7b6178e7c8882d6f179745db
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-09-01 20:38:59 +0900

    fix(log): ClassNotFoundException when using Log4J2 with Spring Boot

---
+++ platforms/springboot/src/main/java/io/hawt/springboot/HawtioLogAutoConfiguration.java
@@ -0,0 +1,26 @@
+package io.hawt.springboot;
+
+import io.hawt.log.logback.LogbackLogQuery;
+import io.hawt.log.support.LogQuerySupport;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Auto configuration for Hawtio Log.
+ */
+@AutoConfiguration
+@ConditionalOnClass(LogQuerySupport.class)
+public class HawtioLogAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnClass(LogbackLogQuery.class)
+    public LogbackLogQuery logbackLogQuery() {
+        LogbackLogQuery logQuery = new LogbackLogQuery();
+        logQuery.start();
+        return logQuery;
+    }
+
+}
--- platforms/springboot/src/main/java/io/hawt/springboot/SpringHawtioContextListener.java
+++ platforms/springboot/src/main/java/io/hawt/springboot/SpringHawtioContextListener.java
@@ -12,19 +12,16 @@ import io.hawt.system.HawtioProperty;
 public class SpringHawtioContextListener extends HawtioContextListener {
 
     public final String servletPath;
-    private final LogbackLogQuery logQuery;
 
     public SpringHawtioContextListener(final ConfigManager configManager,
                                        final String servletPath) {
         super(configManager);
         this.servletPath = Objects.requireNonNull(servletPath);
-        this.logQuery = new LogbackLogQuery();
     }
 
     @Override
     public void contextInitialized(final ServletContextEvent servletContextEvent) {
         super.contextInitialized(servletContextEvent);
-        logQuery.start();
         servletContextEvent.getServletContext()
             .setAttribute(HawtioProperty.SERVLET_PATH, this.servletPath);
     }
