7982115d183a779660f9c4547539701f27dc3fe9
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-09-08 16:39:21 +0900

    fix(system): ENTESB-19675 make Access-Control-Allow-Origin header configurable
    By default Hawtio sets the following HTTP header:
    
      Access-Control-Allow-Origin: *
    
    But optionally now you can customise the header by setting the following system
    property:
    
      hawtio.http.accessControlAllowOrigin=https://example.com

--- hawtio-system/src/main/java/io/hawt/web/filters/CORSFilter.java
+++ hawtio-system/src/main/java/io/hawt/web/filters/CORSFilter.java
@@ -1,16 +1,39 @@
 package io.hawt.web.filters;
 
 import java.util.concurrent.TimeUnit;
+
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+ */
 public class CORSFilter extends HttpHeaderFilter {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(CORSFilter.class);
+
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "http.accessControlAllowOrigin";
+    public static final String HAWTIO_ACCESS_CONTROL_ALLOW_ORIGIN = "hawtio." + ACCESS_CONTROL_ALLOW_ORIGIN;
+
+    private String headerValue = "*";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
+        String origin = getConfigParameter(ACCESS_CONTROL_ALLOW_ORIGIN);
+        if (origin != null) {
+            headerValue = origin;
+        }
+        LOG.debug("Access-Control-Allow-Origin is configured: {}", headerValue);
+    }
 
     @Override
     protected void addHeaders(HttpServletRequest request, HttpServletResponse response) {
-        if (!allowAny()) {
-            return;
-        }
         if ("OPTIONS".equals(request.getMethod())) {
             response.addHeader("Access-Control-Request-Method", "GET, POST, PUT, DELETE");
             String headers = request.getHeader("Access-Control-Request-Headers");
@@ -19,11 +42,6 @@ public class CORSFilter extends HttpHeaderFilter {
             }
             response.addHeader("Access-Control-Max-Age", "" + TimeUnit.DAYS.toSeconds(1));
         }
-        response.addHeader("Access-Control-Allow-Origin", "*");
-    }
-
-    protected boolean allowAny() {
-        // TODO allow configuration...
-        return true;
+        response.addHeader("Access-Control-Allow-Origin", headerValue);
     }
 }
