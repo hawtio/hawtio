9bc34e0719a9e4ae38f7e53d0c107cfaa3d2d071
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-02-17 16:36:24 +0900

    chore: import prettier settings for js/ts formatting

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+}
