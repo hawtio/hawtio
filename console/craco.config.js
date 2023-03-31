55b47791f521726f504e743213a3b4386087dd1e
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-03-31 15:54:16 +0900

    chore(console): update @hawtio/react to 0.2.0-dev.5

--- console/craco.config.js
+++ console/craco.config.js
@@ -7,14 +7,16 @@ module.exports = {
       // Required when doing `yarn link`
       react: path.resolve(__dirname, 'node_modules/react'),
     },
-    configure: (webpackConfig) => {
+    configure: webpackConfig => {
       webpackConfig.ignoreWarnings = [
         // For suppressing sourcemap warnings coming from superstruct
         function ignoreSourcemapsloaderWarnings(warning) {
-          return warning.module
-            && warning.module.resource.includes('node_modules')
-            && warning.details
-            && warning.details.includes('source-map-loader')
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          )
         },
       ]
 
@@ -28,17 +30,32 @@ module.exports = {
     },
   },
   devServer: {
-    setupMiddlewares: (middlewares) => {
+    setupMiddlewares: (middlewares, devServer) => {
+      // Redirect / to /hawtio/
+      devServer.app.get('/', (req, res) => res.redirect('/hawtio/'))
+
+      const username = 'developer'
+      const login = true
+      const proxyEnabled = true
+      const plugin = []
+
+      // Hawtio backend API mock
+      devServer.app.get('/hawtio/user', (req, res) => res.send(`"${username}"`))
+      devServer.app.post('/hawtio/auth/login', (req, res) => res.send(String(login)))
+      devServer.app.get('/hawtio/auth/logout', (req, res) => res.redirect('/hawtio/login'))
+      devServer.app.get('/hawtio/proxy/enabled', (req, res) => res.send(String(proxyEnabled)))
+      devServer.app.get('/hawtio/plugin', (req, res) => res.send(JSON.stringify(plugin)))
+
       middlewares.push({
         name: 'hawtio-backend',
         path: '/proxy',
         middleware: hawtioBackend({
           // Uncomment it if you want to see debug log for Hawtio backend
           logLevel: 'debug',
-        })
+        }),
       })
 
       return middlewares
-    }
-  }
+    },
+  },
 }
