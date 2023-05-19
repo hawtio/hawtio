d09b01587a2667e2281683ab4d9fdd189c65f094
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-05-19 19:12:29 +0900

    refactor(tests): refine how to run e2e tests

--- tests/sample-plugin/src/declare.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '*.md'
-declare module '*.svg'
-declare module '*.jpg'
--- tests/sample-plugin/src/sample-plugin/globals.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '@hawtio/react'
-
-export const pluginName = 'sample-plugin'
-export const pluginTitle = 'Sample Plugin'
-export const pluginPath = '/sample-plugin'
-
-export const log = Logger.get(pluginName)
