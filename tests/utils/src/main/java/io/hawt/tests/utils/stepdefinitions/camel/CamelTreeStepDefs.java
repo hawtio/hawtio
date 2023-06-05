55dded5a2157139789444d9e3de0d3fab3b42a87
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-06-05 18:33:55 +0900

    fix(e2e): Camel view tests failures caused by @hawtio/react upgrade
    Following changes need to be adapted by the Camel e2e tests:
    hawtio/hawtio-next#301
    hawtio/hawtio-next#309
    hawtio/hawtio-next#313

--- tests/utils/src/main/java/io/hawt/tests/utils/stepdefinitions/camel/CamelTreeStepDefs.java
+++ tests/utils/src/main/java/io/hawt/tests/utils/stepdefinitions/camel/CamelTreeStepDefs.java
@@ -9,26 +9,30 @@ public class CamelTreeStepDefs {
 
     @And("^User is on Camel Contexts$")
     public void userIsOnCamelContexts() {
-        camelPage.camelTree().selectSpecificItemByExactId("org.apache.camel-CamelContexts");
+        camelPage.camelTree().selectSpecificItemByExactId(folder("org.apache.camel") + "-" + folder("CamelContexts"));
     }
 
     @And("^User is on Camel \"([^\"]*)\" context$")
     public void userIsOnCamelContext(String context) {
-        camelPage.camelTree().selectSpecificItem("CamelContexts-" + context);
+        camelPage.camelTree().selectSpecificItem(folder("CamelContexts") + "-" + folder(context));
     }
 
     @And("^User is on Camel \"([^\"]*)\" folder of \"([^\"]*)\" context$")
     public void userIsOnCamelEndpointsPage(String folder, String context) {
         camelPage.camelTree()
-            .expandSpecificFolder(CamelTree.class, context)
-            .selectSpecificItem(context + "-" + folder);
+            .expandSpecificFolder(CamelTree.class, folder(context))
+            .selectSpecificItem(folder(context) + "-" + folder(folder));
     }
 
     @And("^User is on Camel \"([^\"]*)\" item of \"([^\"]*)\" folder of \"([^\"]*)\" context$")
     public void userIsOnCamelItemOfFolderOfContext(String item, String folder, String context) {
         camelPage.camelTree()
-            .expandSpecificFolder(CamelTree.class, context)
-            .expandSpecificFolder(CamelTree.class, context + "-" + folder)
-            .selectSpecificItem(context + "-" + folder + "-" + item);
+            .expandSpecificFolder(CamelTree.class, folder(context))
+            .expandSpecificFolder(CamelTree.class, folder(context) + "-" + folder(folder))
+            .selectSpecificItem(folder(context) + "-" + folder(folder) + "-" + item);
+    }
+
+    private static String folder(String id) {
+        return id + "-folder";
     }
 }
