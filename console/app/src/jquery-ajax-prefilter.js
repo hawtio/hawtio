d5cb8afcf7dd946cc9dcae007ad9783bed0b6b0e
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-06-21 14:58:27 +0900

    chore(console): remove leftover from hawtio v2

--- console/app/src/jquery-ajax-prefilter.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/jquery/jquery/issues/2432#issuecomment-403761229
-// Prevent auto-execution of scripts when no explicit dataType was provided (See gh-2432)
-jQuery.ajaxPrefilter(function (s) {
-  if (s.crossDomain) {
-    s.contents.script = false;
-  }
-});
-console.debug("jQuery ajax prefilter applied");
