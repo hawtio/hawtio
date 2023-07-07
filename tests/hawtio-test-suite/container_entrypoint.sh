96b75de5cc5e74badeed47193623ca09c0f98624
Martin Muzikar <mmuzikar@redhat.com>
2023-07-07 09:25:30 +0200

    fix(e2e-workflow): Fix reporting on E2E workflows

--- tests/hawtio-test-suite/container_entrypoint.sh
+++ tests/hawtio-test-suite/container_entrypoint.sh
@@ -3,6 +3,6 @@ export VNC_NO_PASSWORD=true
 Xvfb :99 -screen 0 1920x1080x24 &
 /opt/bin/start-vnc.sh &
 /opt/bin/start-novnc.sh &
-CMD="mvn verify -ntp -Dselenide.browserSize=1920x1080 -Pe2e -pl :hawtio-test-suite -am -Dio.hawt.test.ci -Dhawtio-container -P!local-test-app-dependency $@"
+CMD="mvn verify -ntp -Dselenide.browserSize=1920x1080 -Dchromeoptions.args='--disable-dev-shm-usage' -Pe2e -pl :hawtio-test-suite -am -Dio.hawt.test.ci -Dhawtio-container -P!local-test-app-dependency $@"
 echo running: $CMD
 eval $CMD
