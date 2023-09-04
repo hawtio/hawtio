4fd0cd215fca557d4cbbc5ef6348e841a15b2278
mmelko <mmelko@redhat.com>
2023-09-04 16:14:16 +0200

    feat(quarkus3-support): migrate platform/quarkus and quarkus examples to quarkus 3.3.1

--- platforms/quarkus/runtime/src/main/java/io/hawt/quarkus/filters/HawtioQuarkusLoginRedirectFilter.java
+++ platforms/quarkus/runtime/src/main/java/io/hawt/quarkus/filters/HawtioQuarkusLoginRedirectFilter.java
@@ -2,8 +2,8 @@ package io.hawt.quarkus.filters;
 
 import java.util.Arrays;
 
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
 
 import io.hawt.quarkus.HawtioConfig;
 import io.hawt.web.auth.AuthenticationConfiguration;
--- platforms/quarkus/runtime/src/main/java/io/hawt/quarkus/servlets/HawtioQuakusLogoutServlet.java
+++ platforms/quarkus/runtime/src/main/java/io/hawt/quarkus/servlets/HawtioQuakusLogoutServlet.java
@@ -2,9 +2,9 @@ package io.hawt.quarkus.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import io.hawt.web.auth.AuthSessionHelpers;
 import io.hawt.web.auth.AuthenticationConfiguration;
