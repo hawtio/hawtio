3ff4b05ca326acfd29e091f6366749fb6457f44d
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-10-28 22:35:27 +0900

    feat(system): return partial list of MBeans based on path from RBACRegistry#list()
    Fix #2964

--- hawtio-system/src/test/java/io/hawt/jmx/RBACRegistryTest.java
+++ hawtio-system/src/test/java/io/hawt/jmx/RBACRegistryTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -23,17 +24,42 @@ public class RBACRegistryTest {
         rbacRegistry.destroy();
     }
 
-    @Test
-    @SuppressWarnings("unchecked")
-    public void list() throws Exception {
-        Map<String, Object> result = rbacRegistry.list();
-
+    private static void assertResult(Map<String, Object> result) {
         assertThat(result.get("cache"), notNullValue());
+        @SuppressWarnings("unchecked")
         Map<String, Map<String, Object>> domains = (Map<String, Map<String, Object>>) result.get("domains");
         assertThat(domains, notNullValue());
         Map<String, Object> hawtioDomain = domains.get("hawtio");
         assertThat(hawtioDomain, notNullValue());
         assertThat(hawtioDomain.get("type=security,name=RBACRegistry"), notNullValue());
+    }
+
+    @Test
+    public void list() throws Exception {
+        Map<String, Object> result = rbacRegistry.list();
+        assertResult(result);
+    }
+
+    @Test
+    public void listWithExistingDomainPath() throws Exception {
+        Map<String, Object> result = rbacRegistry.list("hawtio");
+        assertResult(result);
+    }
+
+    @Test
+    public void listWithExistingMBeanPath() throws Exception {
+        Map<String, Object> result = rbacRegistry.list("hawtio/type=security,name=RBACRegistry");
+        assertResult(result);
+    }
+
+    @Test
+    public void listWithNonExistingPath() throws Exception {
+        Map<String, Object> result = rbacRegistry.list("hawtio/name=NoSuchMBean");
+        assertThat(result.get("cache"), notNullValue());
+        @SuppressWarnings("unchecked")
+        Map<String, Map<String, Object>> domains = (Map<String, Map<String, Object>>) result.get("domains");
+        assertThat(domains, notNullValue());
+        assertThat(domains.isEmpty(), is(true));
     }
 
 }
