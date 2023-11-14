55588ec7908b2a6a28112185035f9ac5f7ce723b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-11-14 15:13:01 +0900

    feat(deploy): enable JVMList MBean in hawtio-default for Connect discovery support

---
+++ deploy/hawtio-default/src/main/java/io/hawt/HawtioDefaultContextListener.java
@@ -0,0 +1,33 @@
+package io.hawt;
+
+import javax.servlet.ServletContextEvent;
+
+import io.hawt.HawtioContextListener;
+import io.hawt.jvm.local.JVMList;
+
+public class HawtioDefaultContextListener extends HawtioContextListener {
+    private final JVMList jvmList;
+
+    public HawtioDefaultContextListener() {
+        super();
+        this.jvmList = new JVMList();
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        super.contextInitialized(servletContextEvent);
+        try {
+            jvmList.init();
+        } catch (Exception e) {
+            throw createServletException(e);
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        super.contextDestroyed(servletContextEvent);
+        try {
+            jvmList.destroy();
+        } catch (Exception e) {
+            throw createServletException(e);
+        }
+    }
+}
