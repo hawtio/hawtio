52f4deab4436c6372c32241b22659cc70ec2fbc7
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-12-14 15:58:55 +0900

    fix(examples): improve run-keycloak.sh

--- examples/keycloak-integration/run-keycloak.sh
+++ examples/keycloak-integration/run-keycloak.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname "$0")"
+
 podman run --rm -p 18080:8080 --name keycloak \
   -e KEYCLOAK_ADMIN=admin \
   -e KEYCLOAK_ADMIN_PASSWORD=admin \
-  -v "$PWD/hawtio-demo-realm.json":/opt/keycloak/data/import/hawtio-demo-realm.json:Z \
+  -v "$SCRIPT_DIR/hawtio-demo-realm.json":/opt/keycloak/data/import/hawtio-demo-realm.json:Z \
   quay.io/keycloak/keycloak \
   -v start-dev --import-realm
