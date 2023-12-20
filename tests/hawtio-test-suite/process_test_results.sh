9ed57a5c0a2bc56e38758610c6c077a2d62d1a78
Martin Muzikar <mmuzikar@redhat.com>
2023-12-20 11:06:37 +0100

    Fix report merging in E2E tests

--- tests/hawtio-test-suite/process_test_results.sh
+++ tests/hawtio-test-suite/process_test_results.sh
@@ -1,14 +1,18 @@
+#!/usr/bin/env bash
 echo "# Test results "
 echo "Run attempt: $GITHUB_RUN_NUMBER"
 echo "[Detailed summary]($CHECK_URL)"
+
+#Enable ** glob pattern
+shopt -s globstar
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "| NAME | TESTS | PASSED ✅ | SKIPPED 💤 | FAILED ❌ | ERRORS 🚫 | TIME 🕖 |"
 echo "| --- | --- | --- | --- | --- | --- | --- |"
 for i in $1/*; do 
-    if [ -f "$i/Cucumber.xml" ]; then
+    if [ -f $i/**/Cucumber.xml ]; then
         name=$(basename $i)
         echo -n "| $name | "
-        xmlstarlet tr $SCRIPT_DIR/pr_results_template.xsl $i/Cucumber.xml | xargs
+        xmlstarlet tr $SCRIPT_DIR/pr_results_template.xsl $i/**/Cucumber.xml | xargs
     fi
 done
