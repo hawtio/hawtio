481f85bcabe7704dd1684f1e5a3851868034a970
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-01-23 15:36:05 +0900

    feat(jbang): support red hat maven repositories for jbang

--- hawtio-jbang/src/main/jbang/main/HawtioJBang.java
+++ hawtio-jbang/src/main/jbang/main/HawtioJBang.java
@@ -19,7 +19,7 @@
  */
 
 //JAVA 11+
-//REPOS mavencentral
+//REPOS mavencentral,redhat-ga=https://maven.repository.redhat.com/ga,redhat-ea=https://maven.repository.redhat.com/earlyaccess/all
 //DEPS io.hawt:hawtio-embedded:${hawtio.jbang.version:3.0.1}
 
 package main;
