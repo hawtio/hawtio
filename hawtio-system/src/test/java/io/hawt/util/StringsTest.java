00e28f1ad9619bf8dbcd0b934dfffbd2f8b6b5a1
Grzegorz Grzybek <gr.grzybek@gmail.com>
2024-02-20 20:17:28 +0100

    feat(system): Add simple property placeholder resolver to Strings.java

--- hawtio-system/src/test/java/io/hawt/util/StringsTest.java
+++ hawtio-system/src/test/java/io/hawt/util/StringsTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -43,6 +44,56 @@ public class StringsTest {
         }
     }
 
+    @Nested
+    class PropertiesTest {
+
+        @Test
+        public void oneLevelSystemProperties() {
+            System.setProperty("hawtio.what", "world");
+            assertThat(Strings.resolvePlaceholders(null), nullValue());
+            assertThat(Strings.resolvePlaceholders("Hello"), is("Hello"));
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.what}!"), is("Hello world!"));
+            assertThat(Strings.resolvePlaceholders("${hawtio.what}"), is("world"));
+            assertThat(Strings.resolvePlaceholders("${hawtio.what}, hello ${hawtio.what}!"), is("world, hello world!"));
+        }
+
+        @Test
+        public void valuesWithPlaceholdersAreNotResolved() {
+            System.setProperty("hawtio.what", "${world}");
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.what}!"), is("Hello ${world}!"));
+        }
+
+        @Test
+        public void mismatchedPlaceholders() {
+            System.setProperty("nestedProperty", "what");
+            System.setProperty("hawtio.what", "world");
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.what!"), is("Hello ${hawtio.what!"));
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.${nestedProperty}!"), is("Hello ${hawtio.what!"));
+        }
+
+        @Test
+        public void nestedSystemProperties() {
+            System.setProperty("doubleProperty", "hawtio.what");
+            System.setProperty("nestedProperty", "what");
+            System.setProperty("hawtio.what", "world");
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.${nestedProperty}}!"), is("Hello world!"));
+            assertThat(Strings.resolvePlaceholders("${hawtio.${nestedProperty}}"), is("world"));
+            assertThat(Strings.resolvePlaceholders("${hawtio.${nestedProperty}}, hello ${hawtio.${nestedProperty}}!"), is("world, hello world!"));
+            assertThat(Strings.resolvePlaceholders("${doubleProperty}"), is("hawtio.what"));
+            assertThat(Strings.resolvePlaceholders("${${doubleProperty}}"), is("world"));
+            assertThat(Strings.resolvePlaceholders("${${${doubleProperty}}}"), is("${world}"));
+        }
+
+        @Test
+        public void recursiveSystemProperties() {
+            System.setProperty("nestedProperty", "what");
+            System.setProperty("hawtio.what", "hawtio.${nestedProperty}");
+            assertThat(Strings.resolvePlaceholders("Hello ${hawtio.${nestedProperty}}!"), is("Hello hawtio.${nestedProperty}!"));
+            // no risk, because we don't resolve properties in system property values at all
+//            assertThrows(IllegalArgumentException.class,
+//                    () -> Strings.resolvePlaceholders("Hello ${hawtio.${nestedProperty}}!"));
+        }
+    }
 
     public static class CleanPathTest {
 
