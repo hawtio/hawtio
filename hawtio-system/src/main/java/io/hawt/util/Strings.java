53c12cb8be5a66ef1556521c146052e96aaec329
Grzegorz Grzybek <gr.grzybek@gmail.com>
2024-02-20 20:17:28 +0100

    feat(system): Add simple property placeholder resolver to Strings.java

--- hawtio-system/src/main/java/io/hawt/util/Strings.java
+++ hawtio-system/src/main/java/io/hawt/util/Strings.java
@@ -80,4 +80,74 @@ public class Strings {
         return cleanedPath.length() == 1 ? "" : cleanedPath;
     }
 
+    /**
+     * Simple, recursively-safe property placeholder resolver. Only system properties are used (for now). De-facto
+     * standard {@code ${...}} syntax is used. Unresolvable properties are not replaces and separators pass to
+     * resulting value.
+     *
+     * @param value
+     * @return
+     */
+    public static String resolvePlaceholders(String value) {
+        if (value == null || !value.contains("$")) {
+            return value;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int l = value.length();
+        for (int pos1 = 0; pos1 < l; pos1++) {
+            char c1 = value.charAt(pos1);
+            char c2 = pos1 == l - 1 ? '\0' : value.charAt(pos1 + 1);
+            if (c1 == '$' && c2 == '{') {
+                // find matching }
+                //  - if found, resolve and continue with the rest of the value
+                //  - if not found, just proceed (possibly until next "${")
+                int depth = 1;
+                int pos2 = pos1 + 2;
+                while (depth > 0 && pos2 < l) {
+                    if (value.charAt(pos2) == '$' && pos2 < l - 1 && value.charAt(pos2 + 1) == '{') {
+                        depth++;
+                        pos2 += 2;
+                    } else if (value.charAt(pos2) == '}') {
+                        depth--;
+                        pos2++;
+                    } else {
+                        pos2++;
+                    }
+                }
+                if (depth > 0) {
+                    // no matching '}'
+                    result.append('$');
+                } else {
+                    pos1 = resolve(value, result, pos1, pos2) - 1;
+                }
+            } else {
+                result.append(c1);
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Single iteration resolve method. {@code from} indicates <code>${</code> placeholder start
+     *
+     * @param value
+     * @param result
+     * @param from
+     * @param to
+     * @return
+     */
+    private static int resolve(String value, StringBuilder result, int from, int to) {
+        // "from" always points to "${" and "to" points to _matching_ "}"
+        String key = resolvePlaceholders(value.substring(from + 2, to - 1));
+        String v = System.getProperty(key);
+        if (v == null) {
+            result.append("${").append(key).append("}");
+        } else {
+            result.append(v);
+        }
+        return to;
+    }
+
 }
