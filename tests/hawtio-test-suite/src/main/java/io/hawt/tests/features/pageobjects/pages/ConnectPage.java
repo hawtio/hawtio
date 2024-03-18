8fb1c6af6a6ea004446dd16137643f2747893fcc
Martin Muzikar <mmuzikar@redhat.com>
2024-03-18 10:54:13 +0100

    fix(e2e): fix click on connect button

--- tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/pages/ConnectPage.java
+++ tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/pages/ConnectPage.java
@@ -1,15 +1,17 @@
 package io.hawt.tests.features.pageobjects.pages;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import io.hawt.tests.features.config.TestConfiguration;
-import io.hawt.tests.features.setup.LoginLogout;
+import static com.codeborne.selenide.Selenide.$;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.net.URL;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 
-import static com.codeborne.selenide.Selenide.$;
+import java.net.URL;
+import java.time.Duration;
+
+import io.hawt.tests.features.config.TestConfiguration;
 
 public class ConnectPage extends HawtioPage {
 
@@ -25,28 +27,28 @@ public class ConnectPage extends HawtioPage {
 
     public void addConnection(String name, URL connection) {
 
-    if ($(CONNECTION_LIST).isDisplayed()) {
-        /* I have added if-else construct due to the reason that on re-occurring error screenshots, it seemed like the test-connection already existed.
+        if ($(CONNECTION_LIST).isDisplayed()) {
+        /* I have added if-else construct due to the reason that on re-occurring error screenshots, it seemed like the test-connection already
+        existed.
         TO-DO: task for further examination and potential refinement */
-        return;
-    } else {
-        $(CONNECT_BUTTON).shouldBe(Condition.interactable).click();
+            return;
+        } else {
+            $(CONNECT_BUTTON).shouldBe(Condition.interactable).click();
 
-        $(CONNECTION_FORM).$(By.id("connection-form-name")).setValue(name);
-        $(CONNECTION_FORM).$(By.id("connection-form-host")).setValue(connection.getHost());
-        $(CONNECTION_FORM).$(By.id("connection-form-port")).setValue(String.valueOf(connection.getPort()));
-        $(CONNECTION_FORM).$(By.id("connection-form-path")).setValue(connection.getPath());
+            $(CONNECTION_FORM).$(By.id("connection-form-name")).setValue(name);
+            $(CONNECTION_FORM).$(By.id("connection-form-host")).setValue(connection.getHost());
+            $(CONNECTION_FORM).$(By.id("connection-form-port")).setValue(String.valueOf(connection.getPort()));
+            $(CONNECTION_FORM).$(By.id("connection-form-path")).setValue(connection.getPath());
 
-        if (!connection.getPath().endsWith("/jolokia")) {
-            $(CONNECTION_FORM).$(By.id("connection-form-path")).sendKeys("/jolokia");
-        }
+            if (!connection.getPath().endsWith("/jolokia")) {
+                $(CONNECTION_FORM).$(By.id("connection-form-path")).sendKeys("/jolokia");
+            }
 
-        if ("https".equals(connection.getProtocol())) {
-            $(CONNECTION_FORM).$(By.id("connection-form-scheme")).click();
-        }
+            if ("https".equals(connection.getProtocol())) {
+                $(CONNECTION_FORM).$(By.id("connection-form-scheme")).click();
+            }
 
-        $(MODAL).$(FOOTER_BUTTON).click();
-
+            $(MODAL).$(FOOTER_BUTTON).click();
         }
     }
 
@@ -56,14 +58,14 @@ public class ConnectPage extends HawtioPage {
         final String username = TestConfiguration.getConnectAppUsername();
         final String password = TestConfiguration.getConnectAppPassword();
 
-        $(CONNECTION_LIST).$(connectionSelector).click();
+        $(CONNECTION_LIST).$(connectionSelector).shouldBe(Condition.interactable, Duration.ofSeconds(5))
+            .click();
 
         Selenide.Wait().until(ExpectedConditions.numberOfWindowsToBe(2));
         Selenide.switchTo().window(1);
 
         $(CONNECTION_LOGIN_FORM).$(By.id("connect-login-form-username")).setValue(username);
         $(CONNECTION_LOGIN_FORM).$(By.id("connect-login-form-password")).setValue(password);
-        $(MODAL).$(FOOTER_BUTTON).click();
-
+        $(MODAL).$(FOOTER_BUTTON).shouldBe(Condition.interactable, Duration.ofSeconds(5)).click();
     }
 }
