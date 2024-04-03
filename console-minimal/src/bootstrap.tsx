faef74c04de89828ccb0e3ea224e4ad89a3840cc
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-04-03 18:42:31 +0900

    build(console): migrate from craco to webpack
    hawtio/hawtio-next#455

--- console-minimal/src/bootstrap.tsx
+++ console-minimal/src/bootstrap.tsx
@@ -1,7 +1,6 @@
 import { configManager, connect, hawtio, Hawtio, jmx, keycloak, Logger, oidc, rbac } from '@hawtio/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { reportWebVitals } from './reportWebVitals'
 
 const hawtioVersion = '__HAWTIO_VERSION_PLACEHOLDER__'
 
@@ -30,8 +29,3 @@ root.render(
     <Hawtio />
   </React.StrictMode>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
