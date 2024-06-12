ba896c16840f1bebdbacd5bdb8e2b2cc9cb63a9b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-06-12 14:46:42 +0900

    chore: update hawtio jbang version to 3.0.2

--- hawtio-jbang/src/main/jbang/main/HawtioJBang.java
+++ hawtio-jbang/src/main/jbang/main/HawtioJBang.java
@@ -20,7 +20,7 @@
 
 //JAVA 11+
 //REPOS mavencentral,redhat-ga=https://maven.repository.redhat.com/ga,redhat-ea=https://maven.repository.redhat.com/earlyaccess/all
-//DEPS io.hawt:hawtio-embedded:${hawtio.jbang.version:3.0.1}
+//DEPS io.hawt:hawtio-embedded:${hawtio.jbang.version:3.0.2}
 
 package main;
 
