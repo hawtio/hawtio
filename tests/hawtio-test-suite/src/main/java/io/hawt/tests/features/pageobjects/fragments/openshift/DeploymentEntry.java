3924e36a1d688cf14d59c8964e4b4a19f6a5ff70
Martin Muzikar <mmuzikar@redhat.com>
2024-08-02 10:29:49 +0200

    Update hawtio testsuite for Hawtio Online 4.1.x

--- tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/fragments/openshift/DeploymentEntry.java
+++ tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/fragments/openshift/DeploymentEntry.java
@@ -28,10 +28,10 @@ public class DeploymentEntry {
     }
 
     public void toggleExpand() {
-        $(root).$(By.className("pf-c-accordion__toggle")).click();
+        $(root).$(By.className("pf-v5-c-accordion__toggle")).click();
     }
 
     public List<PodEntry> getPods() {
-        return $(root).sibling(0).$(By.className("pf-c-list")).$$(By.cssSelector("li.pf-c-list__item")).asFixedIterable().stream().map(PodEntry::new).collect(Collectors.toList());
+        return $(root).sibling(0).$(By.className("pf-v5-c-list")).$$(By.cssSelector("li.pf-v5-c-list__item")).asFixedIterable().stream().map(PodEntry::new).collect(Collectors.toList());
     }
 }
