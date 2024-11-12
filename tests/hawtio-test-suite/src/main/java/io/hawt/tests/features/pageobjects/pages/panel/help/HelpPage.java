f81ee2cee94034576a6dfd307a5ce48df655b62c
Juri Solovjov <jsolovjo@redhat.com>
2024-11-12 13:57:43 +0200

    [E2E] Fix tests for PR 1201

--- tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/pages/panel/help/HelpPage.java
+++ tests/hawtio-test-suite/src/main/java/io/hawt/tests/features/pageobjects/pages/panel/help/HelpPage.java
@@ -3,14 +3,15 @@ package io.hawt.tests.features.pageobjects.pages.panel.help;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.empty;
 import static com.codeborne.selenide.Condition.interactable;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.hawt.tests.features.utils.ByUtils.byAttribute;
+import static io.hawt.tests.features.utils.ByUtils.byText;
 import io.hawt.tests.features.pageobjects.pages.HawtioPage;
 
 public class HelpPage extends HawtioPage {
     public void switchTab(String tab) {
-        $(byXpath("//div[@class='pf-v5-c-page__main-nav']//a[text()='" + tab + "']")).shouldBe(interactable).click();
+        $(byAttribute("nav", "aria-label", "Nav")).$(byText("a", tab)).shouldBe(interactable).click();
     }
 
     public void checkContent() {
