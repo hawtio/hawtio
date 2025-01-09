c9ba311a24e70e88f87c9b21a666a30efcce8195
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2025-01-09 16:28:47 +0900

    feat(jbang): add option to auto-connect to a remote Jolokia endpoint
    Fix #3731

--- deploy/hawtio-embedded/src/main/java/io/hawt/embedded/HawtioDefaultLocator.java
+++ deploy/hawtio-embedded/src/main/java/io/hawt/embedded/HawtioDefaultLocator.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
@@ -29,7 +28,7 @@ public class HawtioDefaultLocator {
 
                 // now lets try the JAVA_HOME environment variable just in case
                 javaHome = System.getenv("JAVA_HOME");
-                if (javaHome != null && javaHome.length() > 0) {
+                if (javaHome != null && !javaHome.isEmpty()) {
                     addPath(paths, javaHome);
                 }
 
