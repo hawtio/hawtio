eaba902abf01ec958cb71adfbd3058388539e65d
Grzegorz Grzybek <gr.grzybek@gmail.com>
2025-03-11 09:45:07 +0100

    fix(springboot): Polish Sprint Boot autoconfiguration (for #3806, #3808)

--- platforms/springboot/src/main/java/io/hawt/springboot/HawtioPluginController.java
+++ platforms/springboot/src/main/java/io/hawt/springboot/HawtioPluginController.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * Exposes Hawtio Plugins as a management (/actuator) RestController
  */
+@SuppressWarnings("ClassCanBeRecord")
 public class HawtioPluginController {
 
     private final List<HawtioPlugin> plugins;
