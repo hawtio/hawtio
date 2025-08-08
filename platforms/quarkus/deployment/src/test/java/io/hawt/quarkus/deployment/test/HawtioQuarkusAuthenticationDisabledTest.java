00f1ae767c64e505ce411bf24e6486c70d1a9179
Juri Solovjov <jsolovjo@redhat.com>
2025-08-08 12:17:43 +0200

    [E2E] Update snapshot versions.

--- platforms/quarkus/deployment/src/test/java/io/hawt/quarkus/deployment/test/HawtioQuarkusAuthenticationDisabledTest.java
+++ platforms/quarkus/deployment/src/test/java/io/hawt/quarkus/deployment/test/HawtioQuarkusAuthenticationDisabledTest.java
@@ -23,7 +23,7 @@ public class HawtioQuarkusAuthenticationDisabledTest {
 
     @RegisterExtension
     static final QuarkusUnitTest CONFIG = new QuarkusUnitTest()
-        .setForcedDependencies(List.of(new AppArtifact("io.hawt", "hawtio-quarkus-deployment", "4.2.0-SNAPSHOT")))
+        .setForcedDependencies(List.of(new AppArtifact("io.hawt", "hawtio-quarkus-deployment", "4.2.1-SNAPSHOT")))
         .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
             .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
             .addAsResource(applicationProperties(), "application.properties"));
