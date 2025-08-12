18977f0b6121ebdd2302e8c369c4210f4dbeac20
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2025-08-12 17:48:25 +0900

    fix: update bootstrap.tsx and tsconfig.json for @hawtio/react 1.10

--- console/src/bootstrap.tsx
+++ console/src/bootstrap.tsx
@@ -1,27 +1,33 @@
-import { configManager, hawtio, Hawtio, Logger, registerPlugins } from '@hawtio/react'
+import { configManager, hawtio, HawtioInitialization } from '@hawtio/react/init'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const hawtioVersion = '__HAWTIO_VERSION_PLACEHOLDER__'
 
-const log = Logger.get('hawtio-console')
-log.info('Hawtio console:', hawtioVersion)
-
-// Register builtin plugins
-registerPlugins()
-
-hawtio
-  // Set up plugin location
-  .addUrl('plugin')
-  // Bootstrap Hawtio
-  .bootstrap()
-
 // Configure the console version
 configManager.addProductInfo('Hawtio', hawtioVersion)
 
+// Set up plugin location
+hawtio.addUrl('plugin')
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <Hawtio />
-  </React.StrictMode>,
-)
+root.render(<HawtioInitialization verbose={false} />)
+
+import('@hawtio/react').then(async m => {
+  const log = m.Logger.get('hawtio-console')
+  log.info('Hawtio console:', hawtioVersion)
+
+  // Register builtin plugins
+  m.registerPlugins()
+
+  // Bootstrap Hawtio
+  m.hawtio.bootstrap().then(() => {
+    import('@hawtio/react/ui').then(ui => {
+      root.render(
+        <React.StrictMode>
+          <ui.Hawtio />
+        </React.StrictMode>,
+      )
+    })
+  })
+})
--- console/webpack.config.cjs
+++ console/webpack.config.cjs
@@ -112,6 +112,10 @@ module.exports = (_, args) => {
     },
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      symlinks: false,
+      alias: {
+        '@thumbmarkjs/thumbmarkjs': path.join(__dirname, './node_modules/@thumbmarkjs/thumbmarkjs/dist/thumbmark.esm.js'),
+      },
     },
     ignoreWarnings: [
       // For suppressing sourcemap warnings coming from some dependencies
