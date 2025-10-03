9e153b39a482a8847459746abce226e172ce77a0
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2025-10-03 15:12:01 +0900

    fix(tests): fix spring security configuration for test app

--- tests/springboot/src/main/java/io/hawt/tests/spring/boot/ApplicationNoSecurity.java
+++ tests/springboot/src/main/java/io/hawt/tests/spring/boot/ApplicationNoSecurity.java
@@ -3,14 +3,22 @@ package io.hawt.tests.spring.boot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("!keycloak")
 public class ApplicationNoSecurity {
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().anyRequest();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
