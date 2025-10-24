a0f8cb5468508d4f97131fb3b6ce2d3d617b6600
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2025-10-24 17:40:41 +0900

    fix(console-minimal): sync index.ts/bootstrap.tsx

--- console-minimal/src/bootstrap.tsx
+++ console-minimal/src/bootstrap.tsx
@@ -1,35 +1,40 @@
-import { configManager, hawtio, HawtioInitialization } from '@hawtio/react/init'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const hawtioVersion = '__HAWTIO_VERSION_PLACEHOLDER__'
+import { configManager, hawtio, HawtioInitialization, Logger, TaskState } from '@hawtio/react/init'
+
+configManager.initItem('Loading UI', TaskState.started, 'config')
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<HawtioInitialization verbose={configManager.globalLogLevel() < Logger.INFO.value} />)
 
 // Configure the console version
+const hawtioVersion = '__HAWTIO_VERSION_PLACEHOLDER__'
 configManager.addProductInfo('Hawtio', hawtioVersion)
 
 // Set up plugin location
 hawtio.addUrl('plugin')
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<HawtioInitialization verbose={false} />)
-
-import('@hawtio/react').then(async m => {
-  const log = m.Logger.get('hawtio-console')
+import('@hawtio/react').then(async ({ Logger, keycloak, oidc, connect, rbac, jmx, hawtio }) => {
+  const log = Logger.get('hawtio-console')
   log.info('Hawtio minimal console:', hawtioVersion)
 
   // Register minimum builtin plugins
-  m.keycloak()
-  m.oidc()
-  m.connect()
-  m.rbac()
-  m.jmx()
+  keycloak()
+  oidc()
+  connect()
+  rbac()
+  jmx()
+
+  configManager.initItem('Loading UI', TaskState.finished, 'config')
 
   // Bootstrap Hawtio
-  m.hawtio.bootstrap().then(() => {
-    import('@hawtio/react/ui').then(ui => {
+  hawtio.bootstrap().then(() => {
+    import('@hawtio/react/ui').then(({ Hawtio }) => {
       root.render(
         <React.StrictMode>
-          <ui.Hawtio />
+          <Hawtio />
         </React.StrictMode>,
       )
     })
