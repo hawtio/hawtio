c0469bfe6bb44b1b7e18ab49140f7c06ef7cb8bc
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2025-11-18 17:23:05 +0900

    chore(jbang): update hawtio jbang version to 4.6.2

--- hawtio-jbang/src/main/jbang/main/HawtioJBang.java
+++ hawtio-jbang/src/main/jbang/main/HawtioJBang.java
@@ -20,7 +20,7 @@
 
 //JAVA 17+
 //REPOS mavencentral,redhat-ga=https://maven.repository.redhat.com/ga,redhat-ea=https://maven.repository.redhat.com/earlyaccess/all
-//DEPS io.hawt:hawtio-embedded:${hawtio.jbang.version:4.6.1}
+//DEPS io.hawt:hawtio-embedded:${hawtio.jbang.version:4.6.2}
 
 package main;
 
