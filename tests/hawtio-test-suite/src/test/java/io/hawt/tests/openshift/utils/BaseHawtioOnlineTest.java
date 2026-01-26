2a18f4949ea01021eb546903045958cc6ed6084c
Juri Solovjov <jsolovjo@redhat.com>
2026-01-26 21:19:19 +0100

    [E2E] Align versions and refactor the test automations.

--- tests/hawtio-test-suite/src/test/java/io/hawt/tests/openshift/utils/BaseHawtioOnlineTest.java
+++ tests/hawtio-test-suite/src/test/java/io/hawt/tests/openshift/utils/BaseHawtioOnlineTest.java
@@ -14,6 +14,7 @@ import com.codeborne.selenide.WebDriverRunner;
 import io.hawt.tests.features.config.TestConfiguration;
 import io.hawt.tests.features.hooks.DeployAppHook;
 import io.hawt.tests.features.pageobjects.pages.openshift.HawtioOnlineLoginPage;
+import io.hawt.tests.features.setup.WebDriver;
 import io.hawt.tests.features.setup.deployment.OpenshiftDeployment;
 
 @OpenshiftTest
@@ -28,6 +29,7 @@ public class BaseHawtioOnlineTest {
         openshiftDeployment = (OpenshiftDeployment) TestConfiguration.getAppDeploymentMethod();
         DeployAppHook.appSetup();
         openshiftDeployment.restartApp();
+        WebDriver.setup();
         Selenide.open(DeployAppHook.getBaseURL(), HawtioOnlineLoginPage.class)
             .login(TestConfiguration.getOpenshiftUsername(), TestConfiguration.getOpenshiftPassword());
     }
