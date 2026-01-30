da3465b911a0ca405432e0c9eb39567cde563f58
Juri Solovjov <jsolovjo@redhat.com>
2026-01-30 17:33:16 +0100

    [E2E] Refactor fix.

--- tests/quarkus/start-keycloak.sh
+++ tests/quarkus/start-keycloak.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-cd /tmp
+# Perform the JAR update in the writable tmp directory
+cd /deployments/tmp
 jar xf /deployments/app/*.jar keycloak-hawtio.json
 sed -i s,http://localhost:18080,$KEYCLOAK_URL, keycloak-hawtio.json
 jar uf  /deployments/app/*.jar keycloak-hawtio.json
-echo "Running: java ${JAVA_OPTS} -Dquarkus.profile=keycloak -Dquarkus.oidc.auth-server-url=$KEYCLOAK_URL/realms/hawtio-demo -jar ${JAVA_APP_JAR}"
-java ${JAVA_OPTS} -Dquarkus.profile=keycloak -Dquarkus.oidc.auth-server-url=$KEYCLOAK_URL/realms/hawtio-demo -jar ${JAVA_APP_JAR}
+
+# Return to workdir and launch with the REBUILD flag
+cd /deployments
+echo "Launching with Reaugmentation (Mutable-Jar mode)..."
+
+java ${JAVA_OPTS} \
+     -Dquarkus.profile=keycloak \
+     -Dquarkus.launch.rebuild=true \
+     -Dquarkus.oidc.auth-server-url=$KEYCLOAK_URL/realms/hawtio-demo \
+     -jar ${JAVA_APP_JAR}
