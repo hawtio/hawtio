4f5649169013a408dfe1f3caff8eb46b3b009e7b
GChuf <gasper.cefarin@gmail.com>
2026-07-12 13:07:56 +0200

    tests: change expected status to 401

--- platforms/springboot/src/integration-test/java/io/hawt/springboot/HawtioSpringBootEndpointIT.java
+++ platforms/springboot/src/integration-test/java/io/hawt/springboot/HawtioSpringBootEndpointIT.java
@@ -71,7 +71,7 @@ public class HawtioSpringBootEndpointIT extends HawtioSpringBootTestCommon {
             WebTestClient client = getTestClient(context);
 
             client.get().uri(properties.getJolokiaPath(false)).exchange()
-                .expectStatus().isForbidden();
+                .expectStatus().isUnauthorized();
 
             client.get().uri(properties.getHawtioPath(false) + "/").exchange()
                 .expectStatus().isFound()
@@ -81,7 +81,7 @@ public class HawtioSpringBootEndpointIT extends HawtioSpringBootTestCommon {
                 .expectStatus().isOk();
 
             client.get().uri(properties.getHawtioJolokiaPath(false)).exchange()
-                .expectStatus().isForbidden();
+                .expectStatus().isUnauthorized();
         });
     }
 
